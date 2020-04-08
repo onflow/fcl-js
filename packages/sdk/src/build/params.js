a6635384131ef9fde568e1ef6c75cfbe4fabd5e0
James Hunter <github@qv.vg>
2020-04-08 10:59:30 -0700

    PKG -- [sdk] build param argument order

--- packages/sdk/src/build/params.js
+++ packages/sdk/src/build/params.js
@@ -1,13 +1,20 @@
 import {pipe, put} from "@onflow/interaction"
 
 function toMap(kv = []) {
-  return kv.reduce((acc, d) => ({...acc, [d.key]: d.value}), {})
+  return kv
+    .filter(d => d.key != null)
+    .reduce((acc, d) => ({...acc, [d.key]: d.value}), {})
 }
 
 export function params(px = []) {
   return pipe([put("ix.params", toMap(px))])
 }
 
-export function param(key, value, _type = null) {
-  return {key, value}
+const identity = {
+  asParam: v => v,
+  asInjection: v => v,
+}
+
+export function param(value, xform = identity, key = null) {
+  return {key, value, xform}
 }
