2ce1f222c329dc83a35feb055ab94451c5f865bf
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-04-18 14:43:34 -0700

    PKG -- [response] Removes unused fields

--- packages/response/src/response.js
+++ packages/response/src/response.js
@@ -1,4 +1,4 @@
 const DEFAULT_RESPONSE =
-'{"tag": 0, "transaction":null, "transactionHash":null, "encodedData":null, "decodeInstructions": null, "events": null, "account": null, "ping": null}'
+'{"tag": 0, "transaction":null, "transactionHash":null, "encodedData":null, "events": null, "account": null}'
 
 export const response = () => JSON.parse(DEFAULT_RESPONSE)
