e66cdfcef3afe6f0c7a5ce13dab8e821c1a4eb0f
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-04-29 13:28:42 -0700

    PKG -- [interaction] Updates interaction structure

--- packages/interaction/src/interaction.js
+++ packages/interaction/src/interaction.js
@@ -10,47 +10,80 @@ export const PING /*                    */ = 0b10000000
 export const BAD /* */ = 0b01
 export const OK /*  */ = 0b10
 
+export const ACCOUNT /* */ = 0b01
+export const PARAM /*   */ = 0b10
+
+const ACCT = `{
+  "kind":${ACCOUNT},
+  "tempId":null,
+  "addr":null,
+  "keyId":null,
+  "sequenceNum":null,
+  "signature":null,
+  "signingFunction":null,
+  "resolve":null,
+  "role": {
+    "proposer":false,
+    "authorizer":false,
+    "payer":false,
+    "param":false
+  }
+}`
+
+const PRM = `{
+  "kind":${PARAM},
+  "tempId":null,
+  "value":null
+}`
+
 const IX = `{
   "tag":${UNKNOWN},
   "assigns":{},
   "status":${OK},
   "reason":null,
-  "payload":{
-    "code":null,
-    "ref":null,
-    "nonce":null,
-    "limit":null
+  "accounts":{},
+  "params":{},
+  "message": {
+    "cadence":null,
+    "refBlock":null,
+    "computLimit":null,
+    "proposer":null,
+    "payer":null,
+    "authorizations":[],
+    "params":[]
   },
-  "bounds":{
+  "proposer":null,
+  "authorizations":[],
+  "payer":null,
+  "events": {
+    "eventType":null,
     "start":null,
     "end":null
   },
-  "acct":null,
-  "proposer":null,
-  "authz":[],
-  "payer":null,
-  "eventType":null,
-  "txId":null,
-  "isSealed":null
+  "latestBlock": {
+    "isSealed":null
+  },
+  "accountAddr":null,
+  "transactionId":null
 }`
 
 const KEYS = new Set(Object.keys(JSON.parse(IX)))
 
 export const interaction = () => JSON.parse(IX)
 
-const isArray = d => Array.isArray(d)
-const isObj = d => typeof d === "object"
-const isNull = d => d == null
-const isNumber = d => d === "number"
-const isFn = d => typeof d === "function"
+const isArray = (d) => Array.isArray(d)
+const isObj = (d) => typeof d === "object"
+const isNull = (d) => d == null
+const isNumber = (d) => d === "number"
+const isFn = (d) => typeof d === "function"
 
-export const isInteraction = ix => {
+export const isInteraction = (ix) => {
   if (!isObj(ix) || isNull(ix) || isNumber(ix)) return false
   for (let key of KEYS) if (!ix.hasOwnProperty(key)) return false
   return true
 }
 
-export const Ok = ix => {
+export const Ok = (ix) => {
   ix.status = OK
   return ix
 }
@@ -61,21 +94,35 @@ export const Bad = (ix, reason) => {
   return ix
 }
 
-const make = wat => ix => {
+const makeIx = (wat) => (ix) => {
   ix.tag = wat
   return Ok(ix)
 }
 
-export const makeUnknown /*               */ = make(UNKNOWN)
-export const makeScript /*                */ = make(SCRIPT)
-export const makeTransaction /*           */ = make(TRANSACTION)
-export const makeGetTransactionStatus /*  */ = make(GET_TRANSACTION_STATUS)
-export const makeGetAccount /*            */ = make(GET_ACCOUNT)
-export const makeGetEvents /*             */ = make(GET_EVENTS)
-export const makeGetLatestBlock /*        */ = make(GET_LATEST_BLOCK)
-export const makePing /*                  */ = make(PING)
+console.log('JSON.parse(ACCT)', ACCT)
 
-const is = wat => ix => Boolean(ix.tag & wat)
+export const makeAccount = (ix, uuid) => {
+  console.log('JSON.parse(ACCT)', ACCT)
+  ix.accounts[uuid] = JSON.parse(ACCT)
+  return Ok(ix)
+}
+
+export const makeParam = (ix, uuid) => {
+  console.log('JSON.parse(PRM)', JSON.parse(PRM))
+  ix.params[uuid] = JSON.parse(PRM)
+  return Ok(ix)
+}
+
+export const makeUnknown /*               */ = makeIx(UNKNOWN)
+export const makeScript /*                */ = makeIx(SCRIPT)
+export const makeTransaction /*           */ = makeIx(TRANSACTION)
+export const makeGetTransactionStatus /*  */ = makeIx(GET_TRANSACTION_STATUS)
+export const makeGetAccount /*            */ = makeIx(GET_ACCOUNT)
+export const makeGetEvents /*             */ = makeIx(GET_EVENTS)
+export const makeGetLatestBlock /*        */ = makeIx(GET_LATEST_BLOCK)
+export const makePing /*                  */ = makeIx(PING)
+
+const is = (wat) => (ix) => Boolean(ix.tag & wat)
 
 export const isUnknown /*               */ = is(UNKNOWN)
 export const isScript /*                */ = is(SCRIPT)
@@ -86,11 +133,14 @@ export const isGetEvents /*             */ = is(GET_EVENTS)
 export const isGetLatestBlock /*        */ = is(GET_LATEST_BLOCK)
 export const isPing /*                  */ = is(PING)
 
-export const isOk /*  */ = ix => Boolean(ix.status & OK)
-export const isBad /* */ = ix => Boolean(ix.status & BAD)
-export const why /*   */ = ix => ix.reason
+export const isOk /*  */ = (ix) => Boolean(ix.status & OK)
+export const isBad /* */ = (ix) => Boolean(ix.status & BAD)
+export const why /*   */ = (ix) => ix.reason
 
-const hardMode = ix => {
+export const isAccount /*  */ = (account) => Boolean(account.kind & ACCOUNT)
+export const isParam /*    */ = (param) => Boolean(param.kind & PARAM)
+
+const hardMode = (ix) => {
   for (let key of Object.keys(ix)) {
     if (!KEYS.has(key))
       throw new Error(`"${key}" is an invalid root level Interaction property.`)
@@ -111,27 +161,27 @@ const recPipe = async (ix, fns = []) => {
 
 export const pipe = (...args) => {
   const [arg1, arg2] = args
-  if (isArray(arg1) && arg2 == null) return d => pipe(d, arg1)
+  if (isArray(arg1) && arg2 == null) return (d) => pipe(d, arg1)
   return recPipe(arg1, arg2)
 }
 
-const identity = v => v
+const identity = (v) => v
 
 export const get = (ix, key, fallback) => {
   return ix.assigns[key] == null ? fallback : ix.assigns[key]
 }
 
-export const put = (key, value) => ix => {
+export const put = (key, value) => (ix) => {
   ix.assigns[key] = value
   return Ok(ix)
 }
 
-export const update = (key, fn = identity) => ix => {
+export const update = (key, fn = identity) => (ix) => {
   ix.assigns[key] = fn(ix.assigns[key], ix)
   return Ok(ix)
 }
 
-export const destroy = key => ix => {
+export const destroy = (key) => (ix) => {
   delete ix.assigns[key]
   return Ok(ix)
 }
