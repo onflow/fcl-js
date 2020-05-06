76d03cb138e28447ac74161d14eba3171f69f87c
James Hunter <github@qv.vg>
2020-05-06 08:46:23 -0700

    PKG -- [fcl] fix issue with custom decoders

--- packages/fcl/src/decode/index.js
+++ packages/fcl/src/decode/index.js
@@ -4,7 +4,7 @@ import {config} from "../config"
 export const decode = async response => {
   const decodersFromConfig = await config().where(/^decoder\./)
   const decoders = Object.fromEntries(
-    Object.entries(decodersFromConfig).map((pattern, xform) => {
+    Object.entries(decodersFromConfig).map(([pattern, xform]) => {
       pattern = `/${pattern.replace(/^decoder\./, "")}$/`
       return [pattern, xform]
     })
