6b41074cd83d75acb838a115ce91630d518ef034
James Hunter <github@qv.vg>
2020-05-26 09:26:20 -0700

    PKG -- [dev-wallet] use flow.json for config

--- packages/dev-wallet/server/config.js
+++ packages/dev-wallet/server/config.js
@@ -1,21 +1,49 @@
+import {flowConfig} from "@onflow/fcl-config"
 import * as fcl from "@onflow/fcl"
 
-export const ORIGIN = String(process.env.ORIGIN || "http://localhost")
-export const PORT = Number(process.env.PORT || 8701)
+const get = (scope, path, fallback) => {
+  if (typeof path === "string") return get(scope, path.split("/"), fallback)
+  if (!path.length) return scope
+  try {
+    const [head, ...rest] = path
+    return get(scope[head], rest, fallback)
+  } catch (_error) {
+    return fallback
+  }
+}
+
+export const PK = String(
+  process.env.PK ||
+    get(
+      flowConfig(),
+      "accounts/root/privateKey",
+      "bf9db4706c2fdb9011ee7e170ccac492f05427b96ab41d8bf2d8c58443704b76"
+    )
+)
+
+export const ROOT_ADDR = String(
+  process.env.ROOT_ADDR || get(flowConfig(), "accounts/root/address", "01")
+)
+
+export const PORT = Number(
+  process.env.PORT || get(flowConfig(), "devWallet/port", 8701)
+)
+
+export const ACCESS_NODE = String(
+  process.env.ACCESS_NODE ||
+    get(flowConfig(), "devWallet/accessNode/endpoint", "http://localhost:8080")
+)
+
 export const PID = String(process.env.PID || `asdf${PORT}`)
-export const NAME = String(process.env.NAME || `FCL Dev Wallet`)
+
 export const ICON = String(
   process.env.ICON || `https://avatars.onflow/avatar/${PID}.svg`
 )
+
+export const ORIGIN = "http://localhost"
 export const HOST = [ORIGIN, PORT].filter(Boolean).join(":")
-export const ACCESS_NODE = String(
-  process.env.ACCESS_NODE || "http://localhost:8080"
-)
-export const AUTHN = String(process.env.AUTHN || `${HOST}/flow/authenticate`)
-export const ROOT_ADDR = String(process.env.ROOT_ADDR || "01")
-export const PK = String(
-  process.env.PK ||
-    "bf9db4706c2fdb9011ee7e170ccac492f05427b96ab41d8bf2d8c58443704b76"
-)
+
+export const AUTHN = `${HOST}/flow/authenticate`
+export const NAME = `FCL Dev Wallet`
 
 fcl.config().put("accessNode.api", ACCESS_NODE)
