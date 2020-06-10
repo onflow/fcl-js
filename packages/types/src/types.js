ad139e261a528ca8d6c1706d642bc86f8473221d
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-06-10 12:58:33 -0700

    PKG -- [types] Adds all types from json-cdc

--- packages/types/src/types.js
+++ packages/types/src/types.js
@@ -9,3 +9,195 @@ export const Identity = type(
   v => v,
   v => v
 )
+
+export const UInt = type(
+  "UInt",
+  v => v,
+  v => v
+)
+
+export const Int = type(
+  "Int",
+  v => v,
+  v => v
+)
+
+export const UInt8 = type(
+  "UInt8",
+  v => v,
+  v => v
+)
+
+export const Int8 = type(
+  "Int8",
+  v => v,
+  v => v
+)
+
+export const UInt16 = type(
+  "UInt16",
+  v => v,
+  v => v
+)
+
+export const Int16 = type(
+  "Int16",
+  v => v,
+  v => v
+)
+
+export const UInt32 = type(
+  "UInt32",
+  v => v,
+  v => v
+)
+
+export const Int32 = type(
+  "Int32",
+  v => v,
+  v => v
+)
+
+export const UInt64 = type(
+  "UInt64",
+  v => v,
+  v => v
+)
+
+export const Int64 = type(
+  "Int64",
+  v => v,
+  v => v
+)
+
+export const UInt128 = type(
+  "UInt128",
+  v => v,
+  v => v
+)
+
+export const Int128 = type(
+  "Int128",
+  v => v,
+  v => v
+)
+
+export const UInt256 = type(
+  "UInt256",
+  v => v,
+  v => v
+)
+
+export const Int256 = type(
+  "Int256",
+  v => v,
+  v => v
+)
+
+export const Word8 = type(
+  "Word8",
+  v => v,
+  v => v
+)
+
+export const Word16 = type(
+  "Word16",
+  v => v,
+  v => v
+)
+
+export const Word32 = type(
+  "Word32",
+  v => v,
+  v => v
+)
+
+export const Word64 = type(
+  "Word64",
+  v => v,
+  v => v
+)
+
+export const UFix64 = type(
+  "UFix64",
+  v => v,
+  v => v
+)
+
+export const Fix64 = type(
+  "Fix64",
+  v => v,
+  v => v
+)
+
+export const String = type(
+  "String",
+  v => v,
+  v => v
+)
+
+export const Character = type(
+  "Character",
+  v => v,
+  v => v
+)
+
+export const Bool = type(
+  "Bool",
+  v => v,
+  v => v
+)
+
+export const Address = type(
+  "Address",
+  v => v,
+  v => v
+)
+
+export const Void = type(
+  "Void",
+  v => v,
+  v => v
+)
+
+export const Optional = type(
+  "Optional",
+  v => v,
+  v => v
+)
+
+export const Reference = type(
+  "Reference",
+  v => v,
+  v => v
+)
+
+export const Array = type(
+  "Array",
+  v => v,
+  v => v
+)
+
+export const Dictionary = type(
+  "Dictionary",
+  v => v,
+  v => v
+)
+
+export const Event = type(
+  "Event",
+  v => v,
+  v => v
+)
+
+export const Resource = type(
+  "Resource",
+  v => v,
+  v => v
+)
+
+export const Struct = type(
+  "Struct",
+  v => v,
+  v => v
+)
