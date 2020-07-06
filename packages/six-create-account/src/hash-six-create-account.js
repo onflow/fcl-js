2c1f05a3ee3494edcc4fd549f53c9cd8b7f824f0
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-07-06 09:59:24 -0700

    PKG -- [six] Update create account stored interaction

--- packages/six-create-account/src/hash-six-create-account.js
+++
@@ -1,8 +0,0 @@
-import crypto from 'crypto'
-import {CREATE_ACCOUNT_CADENCE} from "./six-create-account.js"
-
-const hashSixCreateAccount = () => {
-    console.log(crypto.createHash('sha256').update(CREATE_ACCOUNT_CADENCE, 'utf8').digest('hex'))
-}
-
-hashSixCreateAccount()
--- packages/six-create-account/src/six-create-account.js
+++ packages/six-create-account/src/six-create-account.js
@@ -1,7 +1,11 @@
 import * as sdk from "@onflow/sdk"
 import * as t from "@onflow/types"
 
-export const CREATE_ACCOUNT_CADENCE = 
+export const TITLE = "Create Account"
+export const DESCRIPTION = "Create an Account on Flow with given code and public keys."
+export const VERSION = "0.0.1"
+export const HASH = "b4b6397f2985d89662dbe4c2b8577545d49d64bce477935379c32cecaea63484"
+export const CODE = 
 `
 transaction(code: String, publicKeys: [String]) {
     prepare(signer: AuthAccount) {
@@ -14,9 +18,15 @@ transaction(code: String, publicKeys: [String]) {
 }
 `
 
-export const CREATE_ACCOUNT_CADENCE_sha256_AS_hex = "b4b6397f2985d89662dbe4c2b8577545d49d64bce477935379c32cecaea63484"
-
-export const createAccount = (code = "", publicKeys = []) => sdk.pipe([
-    sdk.transaction(CREATE_ACCOUNT_CADENCE),
-    sdk.args([sdk.arg(code, t.String), sdk.arg(publicKeys, t.Array(t.String))])
+export const template = ({ proposer, authorization, payer, code = "", publicKeys = [] }) => sdk.pipe([
+    sdk.invariant(publicKeys.length > 0, "template({publicKeys}) -- must include one public key when creating an account."),
+    sdk.transaction(CODE),
+    sdk.args([sdk.arg(code, t.String), sdk.arg(publicKeys, t.Array(t.String))]),
+    sdk.proposer(proposer),
+    sdk.authorizations([authorization]),
+    sdk.payer(payer),
+    sdk.validate((ix, {Ok, Bad}) => {
+        if (ix.authorizations.length > 1) return Bad(ix, "template only requires one authorization.")
+        return Ok(ix)
+    })
 ])
