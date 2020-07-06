d41fb890912a7243fe0f2492305c6613f11cfa17
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-07-06 13:43:38 -0700

    PKG -- [six] Fix version number

--- packages/six-create-account/src/six-create-account.js
+++ packages/six-create-account/src/six-create-account.js
@@ -3,7 +3,7 @@ import * as t from "@onflow/types"
 
 export const TITLE = "Create Account"
 export const DESCRIPTION = "Create an Account on Flow with given code and public keys."
-export const VERSION = "0.0.1"
+export const VERSION = "0.0.0"
 export const HASH = "b4b6397f2985d89662dbe4c2b8577545d49d64bce477935379c32cecaea63484"
 export const CODE = 
 `
