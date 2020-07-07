bce2137e21855fdee8e10ad7150169eb1dac210e
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-07-07 12:23:14 -0700

    PKG -- [six] Updates according to hex requirements

--- packages/six-create-account/src/six-create-account.js
+++ packages/six-create-account/src/six-create-account.js
@@ -4,14 +4,14 @@ import * as t from "@onflow/types"
 export const TITLE = "Create Account"
 export const DESCRIPTION = "Create an Account on Flow with given code and public keys."
 export const VERSION = "0.0.0"
-export const HASH = "b4b6397f2985d89662dbe4c2b8577545d49d64bce477935379c32cecaea63484"
+export const HASH = "655d4c6d55b2bb42a208ee9d0793f11499f3f2c3e714cf47dee660ff4185285a"
 export const CODE = 
 `
 transaction(code: String, publicKeys: [String]) {
     prepare(signer: AuthAccount) {
         let acct = AuthAccount(payer: signer)
         for key in publicKeys {
-            acct.addPublicKey(key)
+            acct.addPublicKey(key.decodeHex())
         }
         acct.setCode(code.decodeHex())
     }
@@ -21,7 +21,7 @@ transaction(code: String, publicKeys: [String]) {
 export const template = ({ proposer, authorization, payer, code = "", publicKeys = [] }) => sdk.pipe([
     sdk.invariant(publicKeys.length > 0, "template({publicKeys}) -- must include one public key when creating an account."),
     sdk.transaction(CODE),
-    sdk.args([sdk.arg(code, t.String), sdk.arg(publicKeys.map(k => Buffer.from(k).toString("hex")), t.Array(t.String))]),
+    sdk.args([sdk.arg(Buffer.from(code).toString("hex"), t.String), sdk.arg(publicKeys, t.Array(t.String))]),
     sdk.proposer(proposer),
     sdk.authorizations([authorization]),
     sdk.payer(payer),
