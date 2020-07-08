6bcc63d7a7672fbb6da52f7553a22623cefa3766
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-07-08 11:55:47 -0700

    PKG -- [send] Adds arguments for scripts

--- packages/send/src/send-execute-script.js
+++ packages/send/src/send-execute-script.js
@@ -2,9 +2,12 @@ import {ExecuteScriptAtLatestBlockRequest, AccessAPI} from "@onflow/protobuf"
 import {response} from "@onflow/response"
 import {unary} from "./unary"
 
+const argumentBuffer = arg => Buffer.from(JSON.stringify(arg), "utf8")
+
 export async function sendExecuteScript(ix, opts = {}) {
   const req = new ExecuteScriptAtLatestBlockRequest()
   const code = Buffer.from(ix.message.cadence, "utf8")
+  ix.message.arguments.forEach(arg => req.addArguments(argumentBuffer(ix.arguments[arg].asArgument)))
   req.setScript(code)
 
   const res = await unary(opts.node, AccessAPI.ExecuteScriptAtLatestBlock, req)
