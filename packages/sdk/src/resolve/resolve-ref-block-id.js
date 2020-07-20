3ce4801878b95e88fde41dbcd2e36dde5592a767
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-07-20 12:18:02 -0700

    PKG -- [sdk] Removes unnessary pipe

--- packages/sdk/src/resolve/resolve-ref-block-id.js
+++ packages/sdk/src/resolve/resolve-ref-block-id.js
@@ -5,9 +5,9 @@ export const resolveRefBlockId = ({ node }) => async (ix) => {
     if (!(isTransaction(ix))) return Ok(ix)
     if (ix.message.refBlock) return Ok(ix)
 
-    const response = await send(await pipe(await build([
+    const response = await send(await build([
         getLatestBlock()
-    ])), { node })
+    ]), { node })
     const decoded = await decodeResponse(response)
 
     ix.message.refBlock = decoded.id
