a7ebd1e2a3845bb952e7a4717dc259b2b7b281ad
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-07-21 14:43:18 -0700

    EXP -- Adds docs to script example

--- examples/react-simple/src/ix/Script.js
+++ examples/react-simple/src/ix/Script.js
@@ -1,27 +1,56 @@
 import React, {useState} from "react"
 import * as sdk from "@onflow/sdk"
+import * as t from "@onflow/types"
 
 export const Script = () => {
   const [result, setResult] = useState(null)
 
   const run = async () => {
+
+    /*
+
+      Script
+      ------
+      
+      Scripts are declared by building an interaction and specifying a script within it.
+      This is done by specifying Cadence code within the script builder.
+
+      Declaring a script that returns an integer may look like such:
+
+        sdk.script`
+          pub fun main(): Int {
+            return 1
+          }
+        `
+
+      Script Arguments
+      ----------------
+
+      Scripts can consume arguments. These arguments are populated into the main function.
+      Declaring arguments for a Script 
+
+      Resolvers
+      ---------
+
+      In order to resolve any script arguments specified in the interaction,
+      the interaction muse be passed through the resolveArguments resolver.
+
+    */
+
     const response = await sdk.send(await sdk.pipe(await sdk.build([
-      sdk.params([sdk.param("foo", "bar")]),
-      sdk.script`
-        pub fun main(): Int {
-          log("${p => p.foo}")
-          return 7
+      sdk.args([sdk.arg(1, t.Int), sdk.arg(2, t.Int)]),                   
+      sdk.script`                                                         
+        pub fun main(arg1: Int, arg2: Int): Int {
+          return arg1 + arg2
         }
-      `,
+      `, 
     ]), [
       sdk.resolve([
-        sdk.resolveParams,
+        sdk.resolveArguments,
       ]),
     ]), { node: "http://localhost:8080" })
     setResult(await sdk.decodeResponse(response))
   }
-
-  console.log('result', result)
 
   return (
     <div>
