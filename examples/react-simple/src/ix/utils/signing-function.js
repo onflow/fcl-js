3fd58d8f1bd51a8aac6412a7b4d3b187127485dc
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-07-22 11:58:25 -0700

    EXP -- Remove ECDSA signer

--- examples/react-simple/src/ix/utils/signing-function.js
+++ examples/react-simple/src/ix/utils/signing-function.js
@@ -1,12 +1,28 @@
-import {signTransactionPayload} from "./ECDSA256Signer"
+import { ec as EC } from "elliptic";
+import { SHA3 } from "sha3";
 import flowJson from "../../../flow.json"
 
+const ec = new EC("p256")
+const hashMsgHex = msgHex => {
+  const sha = new SHA3(256)
+  sha.update(Buffer.from(msgHex, "hex"))
+  return sha.digest()
+};
 const bufferToHexString = buffer =>
   buffer.reduce(
     (output, elem) => output + ("0" + elem.toString(16)).slice(-2),
     ""
   )
 const hashToBuffer = hash => Buffer.from(hash, "hex")
+
+const signWithKey = (privateKey, msgHex) => {
+  const key = ec.keyFromPrivate(Buffer.from(privateKey, "hex"))
+  const sig = key.sign(hashMsgHex(msgHex))
+  const n = 32
+  const r = sig.r.toArrayLike(Buffer, "be", n)
+  const s = sig.s.toArrayLike(Buffer, "be", n)
+  return Buffer.concat([r, s]).toString("hex")
+}
 
 export const signingFunction = ({
   message,
@@ -22,6 +38,6 @@ export const signingFunction = ({
   return {
     addr,
     keyId,
-    signature: bufferToHexString(signTransactionPayload(flowJson.accounts.service.privateKey, hashToBuffer(message)))
+    signature: signWithKey(flowJson.accounts.service.privateKey, hashToBuffer(message))
   }
 }
