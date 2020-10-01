892c976b7be378eab2b645c4d70884b9a735803a
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-10-01 12:25:17 -0700

    PKG -- [six-transfer-tokens] Adds multi-network support for transfer tokens stored interaction

--- packages/six-transfer-tokens/src/six-transfer-tokens.js
+++ packages/six-transfer-tokens/src/six-transfer-tokens.js
@@ -1,12 +1,29 @@
 import * as sdk from "@onflow/sdk"
 import * as t from "@onflow/types"
+import {config} from "@onflow/config"
+
+const Deps = {
+    FungibleToken: "0xFungibleToken"
+}
+
+const Env = {
+    local: {
+        [Deps.FungibleToken]: "0xee82856bf20e2aa6",
+    },
+    testnet: {
+        [Deps.FungibleToken]: "0x9a0766d93b6608b7",
+    },
+    mainnet: {
+        [Deps.FungibleToken]: "0xf233dcee88fe0abe"
+    }
+}
 
 export const TITLE = "Transfer Tokens"
 export const DESCRIPTION = "Transfer tokens from one account to another."
 export const VERSION = "0.0.0"
-export const HASH = "ca80b628d985b358ae1cb136bcd976997c942fa10dbabfeafb4e20fa66a5a5e2"
+export const HASH = "47851586d962335e3f7d9e5d11a4c527ee4b5fd1c3895e3ce1b9c2821f60b166"
 export const CODE = 
-`import FungibleToken from 0xee82856bf20e2aa6
+`import FungibleToken from ${Deps.FungibleToken}
 transaction(amount: UFix64, to: Address) {
 let vault: @FungibleToken.Vault
 prepare(signer: AuthAccount) {
@@ -22,14 +39,19 @@ getAccount(to)
 }
 }`
 
-export const template = ({ proposer, authorization, payer, amount = "", to = "" }) => sdk.pipe([
-    sdk.transaction(CODE),
-    sdk.args([sdk.arg(amount, t.UFix64), sdk.arg(to, t.Address)]),
-    sdk.proposer(proposer),
-    sdk.authorizations([authorization]),
-    sdk.payer(payer),
-    sdk.validator((ix, {Ok, Bad}) => {
-        if (ix.authorizations.length > 1) return Bad(ix, "template only requires one authorization.")
-        return Ok(ix)
-    })
-])
+export const template = async ({ proposer, authorization, payer, amount = "", to = "" }) => {
+    const env = await config().get("env", "mainnet")
+    const code = CODE.replace(Deps.FungibleToken, Env[env][Deps.FungibleToken])
+
+    return await sdk.pipe([
+        sdk.transaction(code),
+        sdk.args([sdk.arg(amount, t.UFix64), sdk.arg(to, t.Address)]),
+        sdk.proposer(proposer),
+        sdk.authorizations([authorization]),
+        sdk.payer(payer),
+        sdk.validator((ix, {Ok, Bad}) => {
+            if (ix.authorizations.length > 1) return Bad(ix, "template only requires one authorization.")
+            return Ok(ix)
+        })
+    ])
+}
