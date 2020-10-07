b9fced701fa81110e0f00bf4fb908680c40af1ae
James Hunter <github@qv.vg>
2020-10-07 15:24:10 +0000

    [SDK] cleanup after pulling packages out

--- packages/sdk/src/resolve/resolve-ref-block-id.js
+++ packages/sdk/src/resolve/resolve-ref-block-id.js
@@ -2,18 +2,19 @@ import {isTransaction, Ok} from "@onflow/interaction"
 import {send} from "@onflow/send"
 import {decodeResponse} from "@onflow/decode"
 import {build} from "../build"
-import {getLatestBlock} from "../build/get-latest-block"
+import {getLatestBlock} from "@onflow/sdk-build-get-latest-block"
 
 export const resolveRefBlockId = ({ node }) => async (ix) => {
-    if (!(isTransaction(ix))) return Ok(ix)
-    if (ix.message.refBlock) return Ok(ix)
+  if (!(isTransaction(ix))) return Ok(ix)
+  if (ix.message.refBlock) return Ok(ix)
 
-    const response = await send(await build([
-        getLatestBlock()
-    ]), { node })
-    const decoded = await decodeResponse(response)
+  const response = await send(await build([
+    getLatestBlock()
+  ]), { node })
+  const decoded = await decodeResponse(response)
 
-    ix.message.refBlock = decoded.id
+  ix.message.refBlock = decoded.id
 
-    return Ok(ix)
+  return Ok(ix)
 }
+
