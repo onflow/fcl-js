fb44f382c6cea1260cee7b4b460b0267a0f3a112
James Hunter <github@qv.vg>
2020-10-07 19:37:41 +0000

    [FCL] remove sdk as dep

--- packages/fcl/src/resolve/index.js
+++ packages/fcl/src/resolve/index.js
@@ -1,5 +1,5 @@
-import {resolveRefBlockId} from "@onflow/sdk"
 import {pipe} from "@onflow/interaction"
+import {resolveRefBlockId} from "@onflow/sdk-resolve-ref-block-id"
 import {resolveCadence} from "@onflow/sdk-resolve-cadence"
 import {resolveArguments} from "@onflow/sdk-resolve-arguments"
 import {resolveAccounts} from "@onflow/sdk-resolve-accounts"
