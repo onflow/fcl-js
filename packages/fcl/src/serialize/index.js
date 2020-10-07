76ca063adbfe557119da65b9bde45df95af3b3ba
James Hunter <github@qv.vg>
2020-10-07 19:58:49 +0000

    [FCL] replace resolvers with @onflow/sdk-resolve

--- packages/fcl/src/serialize/index.js
+++ packages/fcl/src/serialize/index.js
@@ -1,7 +1,7 @@
 import {interaction, pipe} from "@onflow/interaction"
-import {resolve} from "../resolve"
+import {resolve} from "@onflow/sdk-resolve"
 
-export const serialize = async (args = []) => {
+export const serialize = async (args = [], opts= {}) => {
   if (Array.isArray(args)) args = await pipe(interaction(), args)
-  return JSON.stringify(await resolve(args))
+  return JSON.stringify(await resolve(opts)(args))
 }
