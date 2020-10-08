31cdac0a8f71f2943b46251ae53f1a3632db62a9
James Hunter <github@qv.vg>
2020-10-08 17:18:44 +0000

    [sdk-build-get-account] auto cast address input with sansPrefix

--- packages/sdk-build-get-account/src/index.js
+++ packages/sdk-build-get-account/src/index.js
@@ -1,10 +1,11 @@
 import {pipe, makeGetAccount, Ok} from "@onflow/interaction"
+import {sansPrefix} from "@onflow/util-address"
 
 export function getAccount(addr) {
   return pipe([
     makeGetAccount,
     ix => {
-      ix.accountAddr = addr
+      ix.accountAddr = sansPrefix(addr)
       return Ok(ix)
     }
   ])
