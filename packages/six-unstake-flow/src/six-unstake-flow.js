4b875e7cc19ef3f5f1da72a15c3929f7a0d87cee
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-10-14 13:18:34 -0700

    PKG -- [six-unstake-flow] Initial implementation

---
+++ packages/six-unstake-flow/src/six-unstake-flow.js
@@ -0,0 +1,64 @@
+import * as sdk from "@onflow/sdk"
+import * as t from "@onflow/types"
+import {config} from "@onflow/config"
+
+const Deps = {
+    LOCKEDTOKENADDRESS: "0xLOCKEDTOKENADDRESS",
+    STAKINGPROXYADDRESS: "0xSTAKINGPROXYADDRESS"
+}
+
+const Env = {
+    local: {
+        [Deps.LOCKEDTOKENADDRESS]: "0x0",
+        [Deps.STAKINGPROXYADDRESS]: "0x0",
+    },
+    testnet: {
+        [Deps.LOCKEDTOKENADDRESS]: "0xbe9b485f7d5e7787",
+        [Deps.STAKINGPROXYADDRESS]: "0xb5b17ac6b1db09ae",
+    },
+    mainnet: {
+        [Deps.LOCKEDTOKENADDRESS]: "0x31aed847945124fd",
+        [Deps.STAKINGPROXYADDRESS]: "0x3a84eade58d45ad4",
+    }
+}
+
+export const TITLE = "Unstake Flow"
+export const DESCRIPTION = "Unstakes Flow for an account."
+export const VERSION = "0.0.1"
+export const HASH = "494b4668842ce28ca05122587d4b8592ed9055b31f153e273e3de26eb9154999"
+export const CODE = 
+`import LockedTokens from 0xLOCKEDTOKENADDRESS
+import StakingProxy from 0xSTAKINGPROXYADDRESS
+
+transaction() {
+
+    let holderRef: &LockedTokens.TokenHolder
+
+    prepare(acct: AuthAccount) {
+        self.holderRef = acct.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath)
+            ?? panic("Could not borrow reference to TokenHolder")
+    }
+
+    execute {
+        let stakerProxy = self.holderRef.borrowStaker()
+
+        stakerProxy.unstakeAll()
+    }
+}`
+
+export const template = async ({ proposer, authorization, payer }) => {
+    const env = await config().get("env", "mainnet")
+    let code = CODE.replace(Deps.LOCKEDTOKENADDRESS, Env[env][Deps.LOCKEDTOKENADDRESS])
+    code = code.replace(Deps.STAKINGPROXYADDRESS, Env[env][Deps.STAKINGPROXYADDRESS])
+
+    return sdk.pipe([
+        sdk.transaction(code),
+        sdk.proposer(proposer),
+        sdk.authorizations([authorization]),
+        sdk.payer(payer),
+        sdk.validator((ix, {Ok, Bad}) => {
+            if (ix.authorizations.length > 1) return Bad(ix, "template only requires one authorization.")
+            return Ok(ix)
+        })
+    ])
+}
