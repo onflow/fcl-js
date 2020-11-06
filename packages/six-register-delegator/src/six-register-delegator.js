4ec6b0cf6dc79972cd118a5b51cae8e32cac5147
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-11-06 09:44:06 -0800

    PKG -- [six-register-delegator] Updates transaction code

--- packages/six-register-delegator/src/six-register-delegator.js
+++ packages/six-register-delegator/src/six-register-delegator.js
@@ -26,19 +26,19 @@ export const CODE =
 `import LockedTokens from 0xLOCKEDTOKENADDRESS
 
 transaction(id: String, amount: UFix64) {
-
+    
     let holderRef: &LockedTokens.TokenHolder
-
-    prepare(acct: AuthAccount) {
-        self.holderRef = acct.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath) 
+    
+    prepare(account: AuthAccount) {
+        self.holderRef = account.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath) 
             ?? panic("TokenHolder is not saved at specified path")
-    }
-
+        }
+        
     execute {
         self.holderRef.createNodeDelegator(nodeID: id)
-
+        
         let delegatorProxy = self.holderRef.borrowDelegator()
-
+        
         delegatorProxy.delegateNewTokens(amount: amount)
     }
 }
