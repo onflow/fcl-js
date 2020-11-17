5b0bcbf1bbf7a7e599ec6756c8630b7d733d532c
James Hunter <github@qv.vg>
2020-11-17 12:59:31 +0000

    [sdk-latest-block] VSN @onflow/sdk-send

--- packages/sdk-latest-block/src/index.js
+++ packages/sdk-latest-block/src/index.js
@@ -2,6 +2,6 @@ import {send} from "@onflow/sdk-send"
 import {getLatestBlock} from "@onflow/sdk-build-get-latest-block"
 import {decode} from "@onflow/sdk-decode"
 
-export function latestBlock () {
-  return send([getLatestBlock()]).then(decode)
+export function latestBlock(opts = {}) {
+  return send([getLatestBlock()], opts).then(decode)
 }
