33f35de94b04c8fda121dcc39e2b07fa15c06dd1
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-11-18 11:17:08 -0800

    PKG -- [six-restake-rewarded-flow] Updates txn code

--- packages/six-restake-rewarded-flow/src/six-restake-rewarded-flow.js
+++ packages/six-restake-rewarded-flow/src/six-restake-rewarded-flow.js
@@ -34,8 +34,8 @@ transaction(amount: UFix64) {
 
     let holderRef: &LockedTokens.TokenHolder
 
-    prepare(acct: AuthAccount) {
-        self.holderRef = acct.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath)
+    prepare(account: AuthAccount) {
+        self.holderRef = account.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath)
             ?? panic("Could not borrow reference to TokenHolder")
     }
 
