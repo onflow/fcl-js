a2bc23abac09407090fd08151f122bab7b6f3e7f
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-11-18 10:46:14 -0800

    PKG -- [six-stake-new-locked-flow] Updates txn code

--- packages/six-stake-new-locked-flow/src/six-stake-new-locked-flow.js
+++ packages/six-stake-new-locked-flow/src/six-stake-new-locked-flow.js
@@ -47,11 +47,11 @@ transaction(amount: UFix64) {
 
     let vaultRef: &FlowToken.Vault
 
-    prepare(acct: AuthAccount) {
-        self.holderRef = acct.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath)
+    prepare(account: AuthAccount) {
+        self.holderRef = account.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath)
             ?? panic("Could not borrow reference to TokenHolder")
 
-        self.vaultRef = acct.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+        self.vaultRef = account.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
             ?? panic("Could not borrow flow token vault reference")
     }
 
