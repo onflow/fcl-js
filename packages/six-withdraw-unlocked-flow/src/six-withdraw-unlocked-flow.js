e1d78700a9bddeff4644d1c8ac447b8d2c42bfba
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-11-18 10:56:49 -0800

    PKG -- [six-withdraw-unlocked-flow] Updates txn code

--- packages/six-withdraw-unlocked-flow/src/six-withdraw-unlocked-flow.js
+++ packages/six-withdraw-unlocked-flow/src/six-withdraw-unlocked-flow.js
@@ -40,11 +40,11 @@ transaction(amount: UFix64) {
     let holderRef: &LockedTokens.TokenHolder
     let vaultRef: &FlowToken.Vault
 
-    prepare(acct: AuthAccount) {
-        self.holderRef = acct.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath)
+    prepare(account: AuthAccount) {
+        self.holderRef = account.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath)
             ?? panic("Could not borrow a reference to TokenHolder")
 
-        self.vaultRef = acct.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+        self.vaultRef = account.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
             ?? panic("Could not borrow flow token vault ref")
     }
 
