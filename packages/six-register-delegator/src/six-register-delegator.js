4a55ed8f6e38eed7138193d8e22e3ff7a22cd79c
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2020-12-14 11:19:33 -0800

    [six-register-delegator] -- VSN 0.0.6

--- packages/six-register-delegator/src/six-register-delegator.js
+++ packages/six-register-delegator/src/six-register-delegator.js
@@ -3,17 +3,21 @@ import * as t from "@onflow/types"
 import {config} from "@onflow/config"
 
 const Deps = {
+    FLOWTOKENADDRESS: "0xFLOWTOKENADDRESS",
     LOCKEDTOKENADDRESS: "0xLOCKEDTOKENADDRESS",
 }
 
 const Env = {
     local: {
+        [Deps.FLOWTOKENADDRESS]: "0x0",
         [Deps.LOCKEDTOKENADDRESS]: "0x0",
     },
     testnet: {
+        [Deps.FLOWTOKENADDRESS]: "0x7e60df042a9c0868",
         [Deps.LOCKEDTOKENADDRESS]: "0x95e019a17d0e23d7",
     },
     mainnet: {
+        [Deps.FLOWTOKENADDRESS]: "0x1654653399040a61",
         [Deps.LOCKEDTOKENADDRESS]: "0x8d0e87b65159ae63",
     }
 }
@@ -21,17 +25,23 @@ const Env = {
 export const TITLE = "Register Delegator"
 export const DESCRIPTION = "Register a Delegator on Flow."
 export const VERSION = "0.0.1"
-export const HASH = "94a37cfffbd452d6139967282862be747297c60f9f0c78b1bb2c27c59cb9dbf6"
-export const CODE = 
-`import LockedTokens from 0xLOCKEDTOKENADDRESS
+export const HASH = "3cb357a97a57d9abbe5c68f0df342ee96ba97ade2013753fd2ddf47695a8c08a"
+export const CODE =
+`import FlowToken from 0xFLOWTOKENADDRESS
+import LockedTokens from 0xLOCKEDTOKENADDRESS
 
 transaction(id: String, amount: UFix64) {
 
     let holderRef: &LockedTokens.TokenHolder
 
+    let vaultRef: &FlowToken.Vault
+
     prepare(account: AuthAccount) {
         self.holderRef = account.borrow<&LockedTokens.TokenHolder>(from: LockedTokens.TokenHolderStoragePath) 
             ?? panic("TokenHolder is not saved at specified path")
+
+        self.vaultRef = account.borrow<&FlowToken.Vault>(from: /storage/flowTokenVault)
+            ?? panic("Could not borrow flow token vault reference")
     }
 
     execute {
@@ -39,7 +49,21 @@ transaction(id: String, amount: UFix64) {
 
         let delegatorProxy = self.holderRef.borrowDelegator()
 
-        delegatorProxy.delegateNewTokens(amount: amount)
+        let lockedBalance = self.holderRef.getLockedAccountBalance()
+
+        if amount <= lockedBalance {
+
+            delegatorProxy.delegateNewTokens(amount: amount)
+
+        } else if ((amount - lockedBalance) <= self.vaultRef.balance) {
+
+            self.holderRef.deposit(from: <-self.vaultRef.withdraw(amount: amount - lockedBalance))
+
+            delegatorProxy.delegateNewTokens(amount: amount)
+
+        } else {
+            panic("Not enough tokens to stake!")
+        }
     }
 }
 `
