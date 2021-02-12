ace8397e0caf410d32b1b97dc227e8de7a906938
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-02-12 10:44:24 -0800

    [fcl] -- Adds version number

--- packages/fcl/src/fcl.js
+++ packages/fcl/src/fcl.js
@@ -52,4 +52,4 @@ export {limit} from "@onflow/sdk-build-limit"
 export {ref} from "@onflow/sdk-build-ref"
 export {params, param} from "@onflow/sdk-build-params"
 
-export const VERSION = "0.0.67"
+export const VERSION = "0.0.68-alpha.1"
