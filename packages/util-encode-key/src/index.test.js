eaa6bbdd346fc72630de30dbb2e2d1cc3959bb97
qvvg <github@qv.vg>
2021-02-18 18:20:10 -0800

    [util-encode-key] FIX was passing things into rlp incorrectly

--- packages/util-encode-key/src/index.test.js
+++ packages/util-encode-key/src/index.test.js
@@ -1,4 +1,10 @@
-import {encodeKey, ECDSA_P256, ECDSA_secp256k1, SHA2_256, SHA3_256} from "./index.js"
+import {
+  encodeKey,
+  ECDSA_P256,
+  ECDSA_secp256k1,
+  SHA2_256,
+  SHA3_256,
+} from "./index.js"
 
 const PUBLIC_KEYS = [
   "0bfcd8790c3ce88f3fac9d4bd23514f48bf0cdd1f6c3c8bdf87b11489b1bbeca1ef805ec2ee76451e9bdb265284f78febaeacbc8b0827e0a7baafee4e655d0b5",
@@ -6,22 +12,11 @@ const PUBLIC_KEYS = [
   "da19babe1066af1a75139b43c4f7ee2872d12d1ebdb21fd5d471ae74eb26f1912513ef59057086208c1ed0583f8eac714c90f6412f08f1d86f0ddf69189a13aa",
 ]
 
-const CURVES = [
-  ECDSA_P256,
-  ECDSA_secp256k1,
-]
+const CURVES = [ECDSA_P256, ECDSA_secp256k1]
 
-const HASHINGS = [
-  SHA2_256,
-  SHA3_256,
-]
+const HASHINGS = [SHA2_256, SHA3_256]
 
-const WEIGHTS = [
-  0,
-  32,
-  512,
-  1000
-]
+const WEIGHTS = [0, 32, 512, 1000]
 
 for (let [key_index, key] of PUBLIC_KEYS.entries()) {
   for (let [curve_index, curve] of CURVES.entries()) {
