c25f5f01f413ba63cfbb7d6526f3bfa7612c3f6c
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-02-24 09:34:44 -0800

    [fcl] -- VSN 0.0.68-alpha.5

--- packages/sdk/src/send/send.js
+++ packages/sdk/src/send/send.js
@@ -6,11 +6,6 @@ import {config} from "@onflow/config"
 import {resolve as defaultResolve} from "../resolve/resolve.js"
 import {send as defaultSend} from "./sdk-send.js"
 
-// import {interaction, pipe} from "@onflow/interaction"
-// import {send as defaultSend} from "@onflow/send"
-// import {resolve as defaultResolve} from "@onflow/sdk-resolve"
-// import {config} from "@onflow/config"
-
 export const send = async (args = [], opts = {}) => {
   const sendFunction = await config().get("sdk.send", opts.send || defaultSend)
   const resolveFunction = await config().get(
