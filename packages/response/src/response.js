e62c95281fe6ac9396803d88c2694bbc4fe97be9
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-05 17:06:24 -0800

    [response] -- Removes package from repo

--- packages/response/src/response.js
+++
@@ -1,4 +0,0 @@
-const DEFAULT_RESPONSE =
-'{"tag": 0, "transaction":null, "transactionId":null, "encodedData":null, "events": null, "account": null}'
-
-export const response = () => JSON.parse(DEFAULT_RESPONSE)
