96bd24e741c1ca9fd9d166b8d994eb4754cfc7d7
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-05 17:12:03 -0800

    [sdk-account] -- Removes package from repo

--- packages/sdk-account/src/index.js
+++
@@ -1,7 +0,0 @@
-import {getAccount} from "@onflow/sdk-build-get-account"
-import {decode} from "@onflow/sdk-decode"
-import {send} from "@onflow/sdk-send"
-
-export function account(address, opts) {
-  return send([getAccount(address)], opts).then(decode)
-}
