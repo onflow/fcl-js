81db548b1d9dc16cd49a53273d189ab8541dec71
qvvg <github@qv.vg>
2021-03-09 15:13:04 -0800

    [pkg] house cleaning

--- packages/dev-wallet/src/api/approve-authorization.js
+++
@@ -1,7 +0,0 @@
-import {gql} from "../utils/gql.js"
-
-export const query = gql`
-  mutation ApproveAuthorization($input: ApproveAuthorizationInput) {
-    authorizationId: approveAuthorization(input: $input)
-  }
-`
