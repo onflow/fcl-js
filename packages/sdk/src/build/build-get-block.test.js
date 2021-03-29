1ecae4c1fa879e319f1e9f26ccd523aeca64fc6a
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-28 22:44:12 -0700

    [sdk] -- Adds tests for build get block

--- packages/sdk/src/build/build-get-block.test.js
+++ packages/sdk/src/build/build-get-block.test.js
@@ -1,4 +1,22 @@
-test("placeholder", () => {
-    expect(0).toBe(0)
+import {interaction, isGetBlock} from "../interaction/interaction.js"
+import {getBlock} from "./build-get-block.js"
+
+describe("Build Get Block", () => {
+  test("Get Block - isSealed = false", async () => {
+    const isSealed = false
+
+    const ix = await getBlock(isSealed)(interaction())
+
+    expect(isGetBlock(ix)).toBe(true)
+    expect(ix.block.isSealed).toBe(false)
+  })
+
+  test("Get Block - isSealed = false", async () => {
+    const isSealed = true
+
+    const ix = await getBlock(isSealed)(interaction())
+
+    expect(isGetBlock(ix)).toBe(true)
+    expect(ix.block.isSealed).toBe(true)
+  })
 })
-  
