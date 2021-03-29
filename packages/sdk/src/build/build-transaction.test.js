0b28432fdcea6d95bf1ddc2f2cd5c8e841fc3a7c
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-28 22:47:33 -0700

    [sdk] -- Adds tests for build transaction

--- packages/sdk/src/build/build-transaction.test.js
+++ packages/sdk/src/build/build-transaction.test.js
@@ -1,4 +1,13 @@
-test("placeholder", () => {
-    expect(0).toBe(0)
+import {interaction, isTransaction} from "../interaction/interaction.js"
+import {transaction} from "./build-transaction.js"
+
+describe("Build Transaction", () => {
+  test("Build Transaction", async () => {
+    const cadence = "transaction { increaseMyBallerStatus() }"
+
+    let ix = await transaction(cadence)(interaction())
+
+    expect(isTransaction(ix)).toBe(true)
+    expect(ix.assigns["ix.cadence"]).not.toBeUndefined()
+  })
 })
-  
