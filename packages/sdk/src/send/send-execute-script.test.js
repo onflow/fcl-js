bed2938b224cc955bf3ffe1cb66072cb7f892188
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-28 17:18:59 -0700

    [sdk] -- Adds tests for SendExecuteScript

---
+++ packages/sdk/src/send/send-execute-script.test.js
@@ -0,0 +1,127 @@
+import {AccessAPI} from "@onflow/protobuf"
+import {sendExecuteScript} from "./send-execute-script.js"
+import {build} from "../build/build.js"
+import {script} from "../build/build-script.js"
+import {atBlockId} from "../build/build-at-block-id.js"
+import {atBlockHeight} from "../build/build-at-block-height.js"
+import {resolve} from "../resolve/resolve.js"
+
+const jsonToUInt8Array = (json) => {
+    var str = JSON.stringify(json, null, 0);
+    var ret = new Uint8Array(str.length);
+    for (var i = 0; i < str.length; i++) {
+        ret[i] = str.charCodeAt(i);
+    }
+    return ret
+};
+
+describe("Send Execute Script", () => {
+  test("ExecuteScriptAtLatestBlock", async () => {
+    const unaryMock = jest.fn();
+
+    unaryMock.mockReturnValue({
+        getValue_asU8: () => jsonToUInt8Array({type: "Int", value: 123})
+    });
+
+    await sendExecuteScript(
+        await resolve(
+            await build([
+                script`pub fun main(): Int { return 123 }`
+            ])
+        ),
+        {
+            unary: unaryMock
+        }
+    )
+
+    expect(unaryMock.mock.calls.length).toEqual(1)
+
+    const unaryMockArgs = unaryMock.mock.calls[0]
+
+    expect(unaryMockArgs.length).toEqual(3)
+
+    const unaryType = unaryMock.mock.calls[0][1]
+
+    expect(unaryType).toEqual(AccessAPI.ExecuteScriptAtLatestBlock)
+
+    const unaryMockRequest = unaryMock.mock.calls[0][2]
+    const unaryMockScript = unaryMockRequest.getScript()
+
+    expect(unaryMockScript).not.toBeUndefined()
+  })
+
+  test("ExecuteScriptAtBlockID", async () => {
+    const unaryMock = jest.fn();
+
+    unaryMock.mockReturnValue({
+        getValue_asU8: () => jsonToUInt8Array({type: "Int", value: 123})
+    });
+
+    await sendExecuteScript(
+        await resolve(
+            await build([
+                script`pub fun main(): Int { return 123 }`,
+                atBlockId("abc123")
+            ])
+        ),
+        {
+            unary: unaryMock
+        }
+    )
+
+    expect(unaryMock.mock.calls.length).toEqual(1)
+
+    const unaryMockArgs = unaryMock.mock.calls[0]
+
+    expect(unaryMockArgs.length).toEqual(3)
+
+    const unaryType = unaryMock.mock.calls[0][1]
+
+    expect(unaryType).toEqual(AccessAPI.ExecuteScriptAtBlockID)
+
+    const unaryMockRequest = unaryMock.mock.calls[0][2]
+    const unaryMockScript = unaryMockRequest.getScript()
+    const unaryMockBlockID = unaryMockRequest.getBlockId()
+
+    expect(unaryMockScript).not.toBeUndefined()
+    expect(unaryMockBlockID).not.toBeUndefined()
+  })
+
+  test("ExecuteScriptAtBlockHeight", async () => {
+    const unaryMock = jest.fn();
+
+    unaryMock.mockReturnValue({
+        getValue_asU8: () => jsonToUInt8Array({type: "Int", value: 123})
+    });
+
+    await sendExecuteScript(
+        await resolve(
+            await build([
+                script`pub fun main(): Int { return 123 }`,
+                atBlockHeight(123)
+            ])
+        ),
+        {
+            unary: unaryMock
+        }
+    )
+
+    expect(unaryMock.mock.calls.length).toEqual(1)
+
+    const unaryMockArgs = unaryMock.mock.calls[0]
+
+    expect(unaryMockArgs.length).toEqual(3)
+
+    const unaryType = unaryMock.mock.calls[0][1]
+
+    expect(unaryType).toEqual(AccessAPI.ExecuteScriptAtBlockHeight)
+
+    const unaryMockRequest = unaryMock.mock.calls[0][2]
+    const unaryMockScript = unaryMockRequest.getScript()
+    const unaryMockBlockHeight = unaryMockRequest.getBlockHeight()
+
+    expect(unaryMockScript).not.toBeUndefined()
+    expect(unaryMockBlockHeight).not.toBeUndefined()
+  })
+
+})
