40627ca5008b656bed82c349eff351b21dfb292b
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-28 22:45:43 -0700

    [sdk] -- Adds tests for build limit

--- packages/sdk/src/build/build-limit.test.js
+++ packages/sdk/src/build/build-limit.test.js
@@ -1,4 +1,12 @@
-test("placeholder", () => {
-    expect(0).toBe(0)
+import {interaction} from "../interaction/interaction.js"
+import {limit} from "./build-limit.js"
+
+describe("Build Limit", () => {
+  test("Build Limit", async () => {
+    const txLimit = 100
+
+    let ix = await limit(txLimit)(interaction())
+
+    expect(ix.message.computeLimit).toEqual(txLimit)
+  })
 })
-  
