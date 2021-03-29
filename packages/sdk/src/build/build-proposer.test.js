e7d40657681d8d540ffdcd05255de738bcb84177
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-28 22:46:40 -0700

    [sdk] -- Adds tests for build proposer

--- packages/sdk/src/build/build-proposer.test.js
+++ packages/sdk/src/build/build-proposer.test.js
@@ -1,4 +1,20 @@
-test("placeholder", () => {
-    expect(0).toBe(0)
+import {interaction} from "../interaction/interaction.js"
+import {proposer} from "./build-proposer.js"
+
+describe("Build Proposer", () => {
+  test("Build Proposer", async () => {
+    const authz = { addr: "0xabc123" }
+
+    let ix = await (await proposer(authz))(interaction())
+
+    const proposerAccount = ix.accounts[ix.proposer]
+    
+    expect(proposerAccount.addr).toEqual(authz.addr)
+    expect(proposerAccount.role).toEqual({ 
+        authorizer: false,
+        param: false,
+        payer: false,
+        proposer: true
+    })
+  })
 })
-  
