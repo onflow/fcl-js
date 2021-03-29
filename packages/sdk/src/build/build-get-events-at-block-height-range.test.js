a3a1089bb48af9dc675b7de59ed1925dbf2cc1ea
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-28 22:44:33 -0700

    [sdk] -- Adds tests for build get events at block height range

--- packages/sdk/src/build/build-get-events-at-block-height-range.test.js
+++ packages/sdk/src/build/build-get-events-at-block-height-range.test.js
@@ -1,4 +1,17 @@
-test("placeholder", () => {
-    expect(0).toBe(0)
+import {interaction, isGetEvents} from "../interaction/interaction.js"
+import {getEventsAtBlockHeightRange} from "./build-get-events-at-block-height-range.js"
+
+describe("Build Get Events At Block Height Range", () => {
+  test("Get Events At Block Height Range", async () => {
+    const eventName = "MyEvent"
+    const start = 123
+    const end = 456
+
+    let ix = await getEventsAtBlockHeightRange(eventName, start, end)(interaction())
+
+    expect(isGetEvents(ix)).toBe(true)
+    expect(ix.events.eventType).toBe(eventName)
+    expect(ix.events.start).toBe(start)
+    expect(ix.events.end).toBe(end)
+  })
 })
-  
