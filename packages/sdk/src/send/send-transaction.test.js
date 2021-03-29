370c27b8683a29934842740251de022bf620cc92
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-28 17:21:48 -0700

    [sdk] -- Adds tests for SendTransaction

---
+++ packages/sdk/src/send/send-transaction.test.js
@@ -0,0 +1,94 @@
+import {AccessAPI} from "@onflow/protobuf"
+import {sendTransaction} from "./send-transaction.js"
+import {build} from "../build/build.js"
+import {transaction} from "../build/build-transaction.js"
+import {proposer} from "../build/build-proposer.js"
+import {payer} from "../build/build-payer.js"
+import {ref} from "../build/build-ref.js"
+import {authorizations} from "../build/build-authorizations.js"
+
+import {resolve} from "../resolve/resolve.js"
+
+const jsonToUInt8Array = (json) => {
+    var str = JSON.stringify(json, null, 0);
+    var ret = new Uint8Array(str.length);
+    for (var i = 0; i < str.length; i++) {
+        ret[i] = str.charCodeAt(i);
+    }
+    return ret
+};
+
+describe("Transaction", () => {
+  test("SendTransaction", async () => {
+    const unaryMock = jest.fn();
+
+    unaryMock.mockReturnValue({
+        getId_asU8: () => jsonToUInt8Array({type: "String", value: "abc123"})
+    });
+
+    await sendTransaction(
+        await resolve(
+            await build([
+                transaction`cadence transaction`,
+                proposer({
+                    addr: "f8d6e0586b0a20c7",
+                    keyId: 1,
+                    sequenceNum: 123,
+                    signingFunction: () => ({ 
+                        addr: "f8d6e0586b0a20c7",
+                        keyId: 1,
+                        signature: "abc123"
+                    }),
+                    resolve: null,
+                    roles: { proposer: true, authorizer: true, payer: true, param: false },
+                }),
+                payer({
+                    addr: "f8d6e0586b0a20c7",
+                    keyId: 1,
+                    sequenceNum: 123,
+                    signingFunction: () => ({ 
+                        addr: "f8d6e0586b0a20c7",
+                        keyId: 1,
+                        signature: "abc123"
+                    }),
+                    resolve: null,
+                    roles: { proposer: true, authorizer: true, payer: true, param: false },
+                }),
+                authorizations([
+                    {
+                        addr: "f8d6e0586b0a20c7",
+                        keyId: 1,
+                        sequenceNum: 123,
+                        signingFunction: () => ({ 
+                            addr: "f8d6e0586b0a20c7",
+                            keyId: 1,
+                            signature: "abc123"
+                        }),
+                        resolve: null,
+                        roles: { proposer: true, authorizer: true, payer: true, param: false },
+                    }
+                ]),
+                ref("abc123")
+            ])
+        ),
+        {
+            unary: unaryMock
+        }
+    )
+
+    expect(unaryMock.mock.calls.length).toEqual(1)
+
+    const unaryMockArgs = unaryMock.mock.calls[0]
+
+    expect(unaryMockArgs.length).toEqual(3)
+
+    const unaryType = unaryMock.mock.calls[0][1]
+
+    expect(unaryType).toEqual(AccessAPI.SendTransaction)
+
+    const unaryMockRequest = unaryMock.mock.calls[0][2]
+
+    expect(unaryMockRequest).not.toBeUndefined()
+  })
+
+})
