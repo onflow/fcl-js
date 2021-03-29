e2e8903343691c58ee8106a2aed17c39928ba126
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-03-28 22:47:12 -0700

    [sdk] -- Adds tests for build script

--- packages/sdk/src/build/build-script.test.js
+++ packages/sdk/src/build/build-script.test.js
@@ -1,4 +1,13 @@
-test("placeholder", () => {
-    expect(0).toBe(0)
+import {interaction, isScript} from "../interaction/interaction.js"
+import {script} from "./build-script.js"
+
+describe("Build Script", () => {
+  test("Build Script", async () => {
+    const cadence = "pub fun main(): Int { return 123 }"
+
+    let ix = await script(cadence)(interaction())
+
+    expect(isScript(ix)).toBe(true)
+    expect(ix.assigns["ix.cadence"]).not.toBeUndefined()
+  })
 })
-  
