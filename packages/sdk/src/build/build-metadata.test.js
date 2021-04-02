06abd78071cb475daf0ece4a52b3a238525ef5f5
Greg Santos <gregsantos@gmail.com>
2021-04-02 11:56:45 -0700

    PKG -- [build] add metadata builder, test

---
+++ packages/sdk/src/build/build-metadata.test.js
@@ -0,0 +1,21 @@
+import {interaction} from "../interaction/interaction.js"
+import {meta} from "./build-metadata.js"
+
+describe("Build Meta", () => {
+  test("Build Meta", async () => {
+    const metadata = {
+      kind: "METADATA",
+      title: "Kitty Kangol",
+      description: "A cool cat hat",
+      price: "10",
+      image: null,
+    }
+
+    let ix = await meta({
+      title: "Kitty Kangol",
+      description: "A cool cat hat",
+      price: "10",
+    })(interaction())
+    expect(ix.metadata).toEqual(metadata)
+  })
+})
