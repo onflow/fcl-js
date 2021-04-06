6eee925f9d9c9f769c6082d841c715e87f36e29d
Greg Santos <gregsantos@gmail.com>
2021-04-05 22:21:23 -0700

    [sdk] Update meta builder

--- packages/sdk/src/build/build-metadata.js
+++ packages/sdk/src/build/build-metadata.js
@@ -3,14 +3,12 @@ import {pipe} from "../interaction/interaction.js"
 const ALLOWED_META_FIELDS = new Set(["title", "description", "price", "image"])
 
 export function meta(data = {}) {
-  return pipe([
-    ix => {
-      Object.keys(data).forEach(
-        key => ALLOWED_META_FIELDS.has(key) || delete data[key]
-      )
+  return ix => {
+    Object.keys(data).forEach(
+      key => ALLOWED_META_FIELDS.has(key) || delete data[key]
+    )
 
-      ix.metadata = {...ix.metadata, ...data}
-      return ix
-    },
-  ])
+    ix.metadata = {...ix.metadata, ...data}
+    return ix
+  }
 }
