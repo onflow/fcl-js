7165d8ca55b046d9f0c290c081e9169f07a255f4
Greg Santos <gregsantos@gmail.com>
2021-04-06 08:27:41 -0700

    [sdk] Add invalid key warning to meta

--- packages/sdk/src/build/build-metadata.js
+++ packages/sdk/src/build/build-metadata.js
@@ -1,11 +1,12 @@
-import {pipe} from "../interaction/interaction.js"
-
 const ALLOWED_META_FIELDS = new Set(["title", "description", "price", "image"])
 
 export function meta(data = {}) {
   return ix => {
     Object.keys(data).forEach(
-      key => ALLOWED_META_FIELDS.has(key) || delete data[key]
+      key =>
+        ALLOWED_META_FIELDS.has(key) ||
+        (delete data[key] &&
+          console.warn(`Invalid key: ${key} has been removed`))
     )
 
     ix.metadata = {...ix.metadata, ...data}
