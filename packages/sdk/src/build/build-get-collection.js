c313cc1286d8be2c1d9218899d67d9d32e38108e
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-04-08 14:15:16 -0700

    [sdk] -- Removes use of ok in build get collection

--- packages/sdk/src/build/build-get-collection.js
+++ packages/sdk/src/build/build-get-collection.js
@@ -1,11 +1,11 @@
-import {pipe, Ok, makeGetCollection} from "../interaction/interaction.js"
+import {pipe, makeGetCollection} from "../interaction/interaction.js"
 
 export function getCollection(id = null) {
   return pipe([
     makeGetCollection,
     ix => {
       ix.collection.id = id
-      return Ok(ix)
+      return ix
     }
   ])
 }
