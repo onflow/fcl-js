2dbebb28cd8763f9b4be78c7c3738889d4544475
Greg Santos <gregsantos@gmail.com>
2021-04-08 11:26:09 -0700

    [sdk] Add metadata validation

--- packages/sdk/src/build/build-metadata.js
+++ packages/sdk/src/build/build-metadata.js
@@ -1,15 +1,32 @@
+import {pipe} from "../interaction/interaction.js"
 const ALLOWED_META_FIELDS = new Set(["title", "description", "price", "image"])
 
-export function meta(data = {}) {
-  return ix => {
-    Object.keys(data).forEach(
-      key =>
-        ALLOWED_META_FIELDS.has(key) ||
-        (delete data[key] &&
-          console.warn(`Invalid key: ${key} has been removed`))
-    )
+const applyMeta = (data = {}) => ix => {
+  ix.metadata = data
+  return ix
+}
 
-    ix.metadata = {...ix.metadata, ...data}
-    return ix
+const validateMetaAsStrings = ix => {
+  for (let key of Array.from(ALLOWED_META_FIELDS)) {
+    if (typeof ix.metadata[key] !== "string")
+      throw new Error(
+        `Invalid type for meta field, expected string, got ${typeof ix.metadata[
+          key
+        ]}`
+      )
   }
+  return ix
+}
+
+const scrubInvalidMetaFields = ix => {
+  for (let key of Object.keys(ix.metadata)) {
+    if (!ALLOWED_META_FIELDS.has(key)) {
+      delete ix.metadata[key]
+      console.warn(`Invalid metadata field: ${key} has been removed`)
+    }
+  }
+  return ix
+}
+export function meta(data = {}) {
+  return pipe([applyMeta(data), validateMetaAsStrings, scrubInvalidMetaFields])
 }
--- packages/sdk/src/build/build-metadata.test.js
+++ packages/sdk/src/build/build-metadata.test.js
@@ -1,21 +1,48 @@
 import {interaction} from "../interaction/interaction.js"
 import {meta} from "./build-metadata.js"
 
-describe("Build Meta", () => {
-  test("Build Meta", async () => {
-    const metadata = {
-      title: "Kitty Kangol",
-      description: "A cool cat hat",
-      price: "10",
-      image: null,
-    }
+const META = {
+  title: "Kitty Kangol",
+  description: "A cool cat hat",
+  price: "10",
+  image: "https://i.imgur.com/a/JPmBk9R.png",
+}
 
+describe("Build Meta", () => {
+  test("build valid meta", async () => {
     let ix = await meta({
       title: "Kitty Kangol",
       description: "A cool cat hat",
       price: "10",
-      size: "Large",
+      image: "https://i.imgur.com/a/JPmBk9R.png",
     })(interaction())
-    expect(ix.metadata).toEqual(metadata)
+    expect(ix.metadata).toEqual(META)
+  })
+
+  test("throw on invalid meta", async () => {
+    await expect(
+      meta({
+        title: "Kitty Kangol",
+        description: "A cool cat hat",
+        price: "10",
+        image: {
+          url: "https://i.imgur.com/a/JPmBk9R.png",
+          width: "200",
+          height: "200",
+          alt: "An adorable NFT",
+        },
+      })
+    ).rejects.toThrow(Error)
+  })
+
+  test("scrub meta fields", async () => {
+    let ix = await meta({
+      title: "Kitty Kangol",
+      description: "A cool cat hat",
+      price: "10",
+      image: "https://i.imgur.com/a/JPmBk9R.png",
+      foo: "bar",
+    })(interaction())
+    expect(ix.metadata).toEqual(META)
   })
 })
