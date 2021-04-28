151f3b59c3cc5f6f005b33d5811ccd58c24a12f3
Greg Santos <gregsantos@gmail.com>
2021-04-28 13:10:13 -0700

    [sdk] Remove resolve-params, Update exported resolvers

--- packages/sdk/src/contract.test.js
+++ packages/sdk/src/contract.test.js
@@ -6,10 +6,7 @@ import * as send from "./send/send.js"
 import * as template from "@onflow/util-template"
 
 const interfaceContract = (label, wat) => ([template]) => {
-  const keys = template
-    .replace(/\s+/g, "|")
-    .split("|")
-    .filter(Boolean)
+  const keys = template.replace(/\s+/g, "|").split("|").filter(Boolean)
 
   describe(label, () => {
     for (let key of keys)
@@ -30,7 +27,6 @@ interfaceContract("export", root)`
   limit ref
   resolveAccounts
   resolveSignatures
-  resolveParams
 `
 
 describe("consume", () => {
