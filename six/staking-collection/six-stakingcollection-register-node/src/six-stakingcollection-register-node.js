531c5bbec2299c624bd8fc03ec2fba9160bcd778
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-06-08 16:52:19 -0700

    [six] -- Adds dependency checks to StakingCollection stored interactions

--- six/staking-collection/six-stakingcollection-register-node/src/six-stakingcollection-register-node.js
+++ six/staking-collection/six-stakingcollection-register-node/src/six-stakingcollection-register-node.js
@@ -1,6 +1,10 @@
 import * as sdk from "@onflow/sdk"
 import * as t from "@onflow/types"
 
+const DEPS = new Set([
+    "0xSTAKINGCOLLECTIONADDRESS",
+])
+
 export const TITLE = "Register Node"
 export const DESCRIPTION = "Register a node held in a Staking Collection."
 export const VERSION = "0.0.0"
@@ -37,7 +41,21 @@ transaction(id: String,
 }
 `
 
+class UndefinedConfigurationError extends Error {
+    constructor(address) {
+      const msg = `Stored Interaction Error: Missing configuration for ${address}. Please see the following to learn more: https://github.com/onflow/flow-js-sdk/blob/master/six/six-withdraw-unstaked-flow/README.md`.trim()
+      super(msg)
+      this.name = "Stored Interaction Undefined Address Configuration Error"
+    }
+}
+
+const addressCheck = async address => {
+    if (!await config().get(address)) throw new UndefinedConfigurationError(address)
+}
+
 export const template = async ({ proposer, authorization, payer, nodeID = "", nodeRole = "", networkingAddress = "", networkingKey = "", stakingKey = "", amount = "" }) => {
+    for (let addr of DEPS) await addressCheck(addr)
+    
     return sdk.pipe([
         sdk.transaction(CODE),
         sdk.args([sdk.arg(nodeID, t.String), sdk.arg(nodeRole, t.UInt8), sdk.arg(networkingAddress, t.String), sdk.arg(networkingKey, t.String), sdk.arg(stakingKey, t.String), sdk.arg(amount, t.UFix64)]),
