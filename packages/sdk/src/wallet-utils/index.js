8a33a08c829bc9d77e68064a43e4bfef70e447da
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-06-16 12:52:40 -0700

    [sdk] -- Exports encodeMessageFromSignable from sdk

--- packages/sdk/src/wallet-utils/index.js
+++ packages/sdk/src/wallet-utils/index.js
@@ -1,2 +1,2 @@
 export {validateSignableTransaction} from "./validate-tx.js"
-export {encodeMessageFromSignable} from "./encode-signable"
+export {encodeMessageFromSignable} from "./encode-signable.js"
