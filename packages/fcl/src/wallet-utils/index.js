40d896e6fe4c91929be20cebda66e1268b9e5e09
Greg Santos <greg.santos@dapperlabs.com>
2021-07-21 09:15:54 -0700

    PKG -- [fcl] Add pop-rpc strategy, update sendMessage wallet-util

--- packages/fcl/src/wallet-utils/index.js
+++ packages/fcl/src/wallet-utils/index.js
@@ -1,12 +1,9 @@
-export const sendMessage =
-  (type, msg = {}) =>
-  e => {
-    e && e.preventDefault()
-    if (window.location !== window.parent.location) {
-      // The page is in an iFrame
-      window.parent.postMessage({...msg, type}, "*")
-    } else {
-      // The page is not in an iFrame
-      window.opener.postMessage({...msg, type}, "*")
-    }
+export const sendMessage = (type, msg = {}) => {
+  if (window.location !== window.parent.location) {
+    // The page is in an iFrame
+    window.parent.postMessage({...msg, type}, "*")
+  } else {
+    // The page is not in an iFrame
+    window.opener.postMessage({...msg, type}, "*")
   }
+}
