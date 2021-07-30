41c35fd65fba7f34a4e0bff4f920beb7a78e924e
Greg Santos <greg.santos@dapperlabs.com>
2021-07-30 07:57:12 -0700

    PKG -- [fcl] Update event types

--- packages/fcl/src/current-user/exec-service/strategies/utils/frame.js
+++ packages/fcl/src/current-user/exec-service/strategies/utils/frame.js
@@ -1,11 +1,11 @@
 import {renderFrame} from "./render-frame"
 import {serviceEndpoint} from "./service-endpoint"
 
-const CLOSE_EVENT = "FCL:FRAME:CLOSE"
-const READY_EVENT = "FCL:FRAME:READY"
-const RESPONSE_EVENT = "FCL:FRAME:RESPONSE"
+const CLOSE_EVENT = "FCL:VIEW:CLOSE"
+const READY_EVENT = "FCL:VIEW:READY"
+const RESPONSE_EVENT = "FCL:VIEW:RESPONSE"
 
-const noop = () => {}
+export const _ = e => typeof e === "string" && e.toLowerCase()
 
 const IGNORE = new Set([
   "monetizationstart",
@@ -13,6 +13,8 @@ const IGNORE = new Set([
   "monetizationprogress",
   "monetizationstop",
 ])
+
+const noop = () => {}
 
 export function frame(service, opts = {}) {
   if (service == null) return {send: noop, close: noop}
@@ -31,18 +33,23 @@ export function frame(service, opts = {}) {
     try {
       if (typeof e.data !== "object") return
       if (IGNORE.has(e.data.type)) return
-      if (e.data.type === CLOSE_EVENT) close()
-      if (e.data.type === READY_EVENT) onReady(e, {send, close})
-      if (e.data.type === RESPONSE_EVENT) onResponse(e, {send, close})
+      if (_(e.data.type) === _(CLOSE_EVENT)) close()
+      if (_(e.data.type) === _(READY_EVENT)) onReady(e, {send, close})
+      if (_(e.data.type) === _(RESPONSE_EVENT)) onResponse(e, {send, close})
       onMessage(e, {send, close})
 
       // Backwards Compatible
-      if (e.data.type === "FCL::CHALLENGE::RESPONSE") {
+      if (_(e.data.type) === _("FCL:FRAME:READY")) onReady(e, {send, close})
+      if (_(e.data.type) === _("FCL:FRAME:RESPONSE"))
+        onResponse(e, {send, close})
+      if (_(e.data.type) === _("FCL:FRAME:CLOSE")) close()
+      //
+      if (_(e.data.type) === _("FCL::CHALLENGE::RESPONSE")) {
         onResponse(e, {send, close})
       }
-      if (e.data.type === "FCL::AUTHZ_READY") onReady(e, {send, close})
-      if (e.data.type === "FCL::CHALLENGE::CANCEL") close()
-      if (e.data.type === "FCL::CANCEL") close()
+      if (_(e.data.type) === _("FCL::AUTHZ_READY")) onReady(e, {send, close})
+      if (_(e.data.type) === _("FCL::CHALLENGE::CANCEL")) close()
+      if (_(e.data.type) === _("FCL::CANCEL")) close()
     } catch (error) {
       console.error("Frame Callback Error", error)
       close()
