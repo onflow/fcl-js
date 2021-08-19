27b55daaa3f2581c89fb6d517c97dc6d09cff2c3
Greg Santos <greg.santos@dapperlabs.com>
2021-08-19 08:46:05 -0700

    PKG -- [fcl] Add additional wallet utils

--- packages/fcl/src/wallet-utils/send-msg-to-fcl.js
+++ packages/fcl/src/wallet-utils/send-msg-to-fcl.js
@@ -5,3 +5,27 @@ export const sendMsgToFCL = (type, msg = {}) => {
     window.opener.postMessage({...msg, type}, "*")
   }
 }
+
+export const close = () => {
+  sendMsgToFCL("FCL:VIEW:CLOSE")
+}
+
+export const approve = data => {
+  sendMsgToFCL("FCL:VIEW:RESPONSE", {
+    f_type: "PollingResponse",
+    f_vsn: "1.0.0",
+    status: "APPROVED",
+    reason: null,
+    data: data,
+  })
+}
+
+export const decline = reason => {
+  sendMsgToFCL("FCL:VIEW:RESPONSE", {
+    f_type: "PollingResponse",
+    f_vsn: "1.0.0",
+    status: "DECLINED",
+    reason: reason,
+    data: null,
+  })
+}
