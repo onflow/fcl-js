59429fb828e80a623deed03c6775fdf20de882fc
Greg Santos <greg.santos@dapperlabs.com>
2021-08-27 13:31:15 -0700

    PKG -- [fcl] Update coldStorage, inject storage config

--- packages/fcl/src/config-utils.js
+++ packages/fcl/src/config-utils.js
@@ -8,3 +8,27 @@ export async function configLens(regex) {
     ])
   )
 }
+
+export async function getStorageConfig() {
+  return STORAGE_OPTIONS[
+    await config.first(["fcl.storage", "fcl.storage.default"])
+  ]
+}
+
+export const STORAGE_OPTIONS = {
+  SESSION_STORAGE: {
+    can: true,
+    get: async key => JSON.parse(sessionStorage.getItem(key)),
+    put: async (key, value) =>
+      sessionStorage.setItem(key, JSON.stringify(value)),
+  },
+
+  LOCAL_STORAGE: {
+    can: true,
+    get: async key => JSON.parse(localStorage.getItem(key)),
+    put: async (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  },
+  NO_STORAGE: {
+    can: false,
+  },
+}
