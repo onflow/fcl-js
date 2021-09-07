79a16998c136c1e45ba6f36eece87041aa1e4556
Greg Santos <greg.santos@dapperlabs.com>
2021-09-07 15:41:34 -0700

    PKG -- [fcl] Add msg data to fcl.authenticate

--- packages/fcl/src/config-utils.js
+++ packages/fcl/src/config-utils.js
@@ -1,4 +1,25 @@
 import {config} from "@onflow/sdk"
+import {invariant} from "@onflow/util-invariant"
+
+const SESSION_STORAGE = {
+  can: true,
+  get: async key => JSON.parse(sessionStorage.getItem(key)),
+  put: async (key, value) => sessionStorage.setItem(key, JSON.stringify(value)),
+}
+
+const LOCAL_STORAGE = {
+  can: true,
+  get: async key => JSON.parse(localStorage.getItem(key)),
+  put: async (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+}
+
+const NO_STORAGE = {
+  can: false,
+}
+
+export const STORAGE_DEFAULT = SESSION_STORAGE
+export const USER_DOMAIN_TAG = "FLOW-V0.0-user"
+export const DISCOVERY_METHOD = "IFRAME/RPC"
 
 export async function configLens(regex) {
   return Object.fromEntries(
@@ -9,18 +30,29 @@ export async function configLens(regex) {
   )
 }
 
-export const SESSION_STORAGE = {
-  can: true,
-  get: async key => JSON.parse(sessionStorage.getItem(key)),
-  put: async (key, value) => sessionStorage.setItem(key, JSON.stringify(value)),
-}
+export async function buildAuthnConfig() {
+  const discoveryWallet = await config.first([
+    "discovery.wallet",
+    "challenge.handshake",
+  ])
 
-export const LOCAL_STORAGE = {
-  can: true,
-  get: async key => JSON.parse(localStorage.getItem(key)),
-  put: async (key, value) => localStorage.setItem(key, JSON.stringify(value)),
-}
+  const discoveryWalletMethod = await config.first([
+    "discovery.wallet.method",
+    "discovery.wallet.method.default",
+  ])
 
-export const NO_STORAGE = {
-  can: false,
+  const domainTag = await config.first([
+    "fcl.domainTag",
+    "fcl.domainTag.default",
+  ])
+
+  invariant(
+    discoveryWallet != null,
+    `
+      Required value for discovery.wallet not defined in config.
+      See: "https://github.com/onflow/flow-js-sdk/blob/master/packages/fcl/src/exec/query.md#configuration"
+    `
+  )
+
+  return {discoveryWallet, discoveryWalletMethod, domainTag}
 }
