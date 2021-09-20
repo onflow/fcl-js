2fac1a606449529e32fd7717bcc4f415fb00b06e
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-09-20 16:32:46 -0700

    PKG -- [sdk] Adds wallet utility for encoding provable authentication messages.

--- packages/sdk/src/wallet-utils/encode-provable-authn.test.js
+++ packages/sdk/src/wallet-utils/encode-provable-authn.test.js
@@ -1,4 +1,4 @@
-import {encodeMessageFromProvableAuthn} from "./encode-provable-authn.js"
+import {encodeMessageForProvableAuthn} from "./encode-provable-authn.js"
 
 const address = "0xABC123DEF456"
 const timestamp = 1632179933495
@@ -6,13 +6,13 @@ const appTag = "FLOW-JS-SDK"
 
 describe("encode provable authn", () => {
     test("encode provable authn with appTag", () => {
-        const message = encodeMessageFromProvableAuthn(address, timestamp, appTag)
+        const message = encodeMessageForProvableAuthn(address, timestamp, appTag)
 
         expect(message).toMatchSnapshot()
     })
 
     test("encode provable authn without appTag", () => {
-        const message = encodeMessageFromProvableAuthn(address, timestamp)
+        const message = encodeMessageForProvableAuthn(address, timestamp)
 
         expect(message).toMatchSnapshot()
     })
