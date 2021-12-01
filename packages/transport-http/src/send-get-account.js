9e05e23b2b3c32ee172e68e27f38131cd9cdcd1b
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2021-12-01 11:54:51 -0800

    PKG -- [sdk] Adds transform logic to account key hash and sig algo names to and from id

--- packages/transport-http/src/send-get-account.js
+++ packages/transport-http/src/send-get-account.js
@@ -1,6 +1,20 @@
 import {invariant} from "@onflow/util-invariant"
 import {httpRequest as defaultHttpRequest} from "./http-request.js"
 
+const HashAlgorithmIDs = {
+  "SHA2_256": 1,
+  "SHA2_384": 2,
+  "SHA3_256": 3,
+  "SHA3_384": 4,
+  "KMAC128_BLS_BLS12_381": 5
+}
+
+const SignatureAlgorithmIDs = {
+  "ECDSA_P256": 1,
+  "ECDSA_secp256k1": 2,
+  "BLS_BLS12_381": 3
+}
+
 async function sendGetAccountAtBlockHeightRequest(ix, context, opts) {
   const httpRequest = opts.httpRequest || defaultHttpRequest
 
@@ -36,7 +50,17 @@ function constructResponse(ix, context, res) {
     balance: res.balance,
     code: null, // CHECK IF THIS WILL BE RETURNED AS WELL
     contracts: res.contracts,
-    keys: res.keys // VERIFY THAT KEYS WILL CONTAIN NUMBERED REPRESENTATIONS OF SIG / HASH ALGOS FOR BACKWARDS COMPATIBILITY https://docs.onflow.org/cadence/language/crypto/#hashing
+    keys: res.keys.map(key => ({
+      index: key.index,
+      publicKey: key.public_key,
+      signAlgo: SignatureAlgorithmIDs[key.signing_algorithm],
+      signAlgoName: key.signing_algorithm, // New! Verify this field name for correctness.
+      hashAlgo: HashAlgorithmIDs[key.hashing_algorithm],
+      hashAlgoName: key.hashing_algorithm, // New! Verify this field name for correctness.
+      sequenceNumber: key.sequence_number,
+      weight: key.weight,
+      revoked: key.revoked,
+    })) // VERIFY THAT KEYS WILL CONTAIN NUMBERED REPRESENTATIONS OF SIG / HASH ALGOS FOR BACKWARDS COMPATIBILITY https://docs.onflow.org/cadence/language/crypto/#hashing
   }
 
   return ret
