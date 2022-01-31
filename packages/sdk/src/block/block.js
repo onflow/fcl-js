c54838929f5fef4de9ccfd53a829b27a1b056240
Chase Fleming <chasefleming@users.noreply.github.com>
2022-01-31 10:34:03 -0800

    PKG -- [sdk] SDK circular dependency fix (#988)
    * PKG -- [sdk] SDK circular dependency fix
    
    * PKG -- [sdk] Remove old import

--- packages/sdk/src/block/block.js
+++ packages/sdk/src/block/block.js
@@ -1,8 +1,9 @@
 import {send} from "../send/send.js"
 import {getBlock} from "../build/build-get-block"
+import {atBlockHeight} from "../build/build-at-block-height.js"
+import {atBlockId} from "../build/build-at-block-id.js"
 import {decodeResponse as decode} from "../decode/decode.js"
 import {invariant} from "@onflow/util-invariant"
-import {atBlockHeight, atBlockId} from "../sdk"
 
 export function block({sealed = false, id, height} = {}, opts = {}) {
   invariant(
