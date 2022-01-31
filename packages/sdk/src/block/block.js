7c93aadfeb3341ef0f0e713289bd84c0c20e1ab5
Chase Fleming <chaseefleming@gmail.com>
2022-01-31 10:11:01 -0800

    PKG -- [sdk] Remove old import

--- packages/sdk/src/block/block.js
+++ packages/sdk/src/block/block.js
@@ -4,7 +4,6 @@ import {atBlockHeight} from "../build/build-at-block-height.js"
 import {atBlockId} from "../build/build-at-block-id.js"
 import {decodeResponse as decode} from "../decode/decode.js"
 import {invariant} from "@onflow/util-invariant"
-import {atBlockHeight, atBlockId} from "../sdk"
 
 export function block({sealed = false, id, height} = {}, opts = {}) {
   invariant(
