ef8c6f3cb2d1c5e0637b30785e9765553e2c1450
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2022-01-31 18:07:41 -0800

    PKG -- [util-transform-string] Corrects usage of invariant

--- packages/util-transform-string/src/util-string.js
+++ packages/util-transform-string/src/util-string.js
@@ -12,14 +12,14 @@ export function utf8_to_hex(inputString) {
     return uInt8Array_to_hex(new window.TextEncoder().encode(inputString))
   if (Buffer) return Buffer.from(inputString).toString("hex")
 
-  invariant(true, "Util-String Error: No supported encoding strategy found.")
+  invariant(false, "Util-String Error: No supported encoding strategy found.")
 }
 
 export function utf8_to_base64(inputString) {
   if (window?.btoa) return window.btoa(inputString)
   if (Buffer) return Buffer.from(inputString).toString("base64")
 
-  invariant(true, "Util-String Error: No supported encoding strategy found.")
+  invariant(false, "Util-String Error: No supported encoding strategy found.")
 } 
 
 // hex
@@ -28,7 +28,7 @@ export function hex_to_utf8(inputString) {
     return window?.decodeURIComponent(inputString.replace(/../g, '%$&'))
   if (Buffer) return Buffer.from(inputString, "hex").toString()
 
-  invariant(true, "Util-String Error: No supported encoding strategy found.")
+  invariant(false, "Util-String Error: No supported encoding strategy found.")
 }
 
 export function hex_to_base64(inputString) {
@@ -36,7 +36,7 @@ export function hex_to_base64(inputString) {
     return window?.btoa(window.decodeURIComponent(inputString.replace(/../g, '%$&')))
   if (Buffer) return Buffer.from(inputString, "hex").toString("base64")
 
-  invariant(true, "Util-String Error: No supported encoding strategy found.")
+  invariant(false, "Util-String Error: No supported encoding strategy found.")
 }
 
 // base64
@@ -44,12 +44,12 @@ export function base64_to_utf8(inputString) {
   if (window?.atob) return window?.atob(inputString)
   if (Buffer) return Buffer.from(inputString, "base64").toString("utf-8")
 
-  invariant(true, "Util-String Error: No supported encoding strategy found.")
+  invariant(false, "Util-String Error: No supported encoding strategy found.")
 }
 
 export function base64_to_hex(inputString) {
   if (window?.atob && window?.TextEncoder) return uInt8Array_to_hex(new window.TextEncoder().encode(window?.atob(inputString)))
   if (Buffer) return Buffer.from(inputString, "base64").toString("hex")
 
-  invariant(true, "Util-String Error: No supported encoding strategy found.")
+  invariant(false, "Util-String Error: No supported encoding strategy found.")
 }
