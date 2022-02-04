e76f861e53134ab756f3138c7f93ef5568e1b374
Chase Fleming <chasefleming@users.noreply.github.com>
2022-02-04 14:37:27 -0800

    PKG -- [sdk] Get account at height (#1002)
    * PKG -- [sdk] Get account at height
    
    * PKG -- [sdk] Fix date format
    
    * PKG -- [sdk] Add id as an option

--- packages/sdk/src/account/account.js
+++ packages/sdk/src/account/account.js
@@ -1,7 +1,21 @@
+import {atBlockHeight} from "../build/build-at-block-height.js"
+import {atBlockId} from "../build/build-at-block-id.js"
 import {getAccount} from "../build/build-get-account.js"
+import {invariant} from "@onflow/util-invariant"
 import {decodeResponse as decode} from "../decode/decode.js"
 import {send} from "../send/send.js"
 
-export function account(address, opts) {
+export function account(address, {height, id} = {}, opts) {
+  invariant(
+    !(id && height),
+    `Method: account -- Cannot pass "id" and "height" simultaneously`
+  )
+
+  // Get account by ID
+  if (id) return send([getAccount(address), atBlockId(id)], opts).then(decode)
+
+  // Get account by height
+  if (height) return send([getAccount(address), atBlockHeight(height)], opts).then(decode)
+
   return send([getAccount(address)], opts).then(decode)
 }
