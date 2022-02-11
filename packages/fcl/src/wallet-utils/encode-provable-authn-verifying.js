e9153cae42a2337162b9f03df67a7033617860af
Greg Santos <greg.santos@dapperlabs.com>
2022-02-11 14:53:08 -0800

    PKG -- [fcl] update encode-pauthn-verifying and snapshot

--- packages/fcl/src/wallet-utils/encode-provable-authn-verifying.js
+++ packages/fcl/src/wallet-utils/encode-provable-authn-verifying.js
@@ -1,19 +1,36 @@
-import {withPrefix} from "@onflow/util-address"
+import {sansPrefix} from "@onflow/util-address"
 import {invariant} from "@onflow/util-invariant"
-import {encode} from '@onflow/rlp'
+import {encode} from "@onflow/rlp"
 
 const rightPaddedHexBuffer = (value, pad) =>
   Buffer.from(value.padEnd(pad * 2, 0), "hex")
 
-export const encodeMessageForProvableAuthnVerifying = (address, timestamp, appDomainTag = "") => {
-    invariant(address, "Encode Message From Provable Authn Error: address must be defined")
-    invariant(timestamp, "Encode Message From Provable Authn Error: timestamp must be defined")
+const leftPaddedHexBuffer = (value, pad) =>
+  Buffer.from(value.padStart(pad * 2, 0), "hex")
 
-    const APP_DOMAIN_TAG = appDomainTag ? rightPaddedHexBuffer(Buffer.from(appDomainTag).toString("hex"), 32).toString("hex") : null
+const addressBuffer = addr => leftPaddedHexBuffer(addr, 8)
 
-    return (appDomainTag ? 
-      encode([APP_DOMAIN_TAG, withPrefix(address), timestamp]).toString("hex")
-      :
-      encode([withPrefix(address), timestamp]).toString("hex")
-    )
+export const encodeMessageForProvableAuthnVerifying = (
+  address,
+  timestamp,
+  appDomainTag = ""
+) => {
+  invariant(
+    address,
+    "Encode Message From Provable Authn Error: address must be defined"
+  )
+  invariant(
+    timestamp,
+    "Encode Message From Provable Authn Error: timestamp must be defined"
+  )
+
+  const APP_DOMAIN_TAG = appDomainTag
+    ? rightPaddedHexBuffer(Buffer.from(appDomainTag).toString("hex"), 32)
+    : null
+
+  return Buffer.concat([
+    appDomainTag
+      ? encode([APP_DOMAIN_TAG, addressBuffer(sansPrefix(address)), timestamp])
+      : encode([addressBuffer(sansPrefix(address)), timestamp]),
+  ]).toString("hex")
 }
