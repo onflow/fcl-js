12dbc20fcf10fb32f3ce8479c68ed5b2d3812898
Greg Santos <greg.santos@dapperlabs.com>
2022-02-15 12:25:36 -0800

    Remove redundant Buffer and concat

--- packages/fcl/src/wallet-utils/encode-provable-authn-signing.js
+++ packages/fcl/src/wallet-utils/encode-provable-authn-signing.js
@@ -1,6 +1,6 @@
 import {sansPrefix} from "@onflow/util-address"
 import {invariant} from "@onflow/util-invariant"
-import {encode} from "@onflow/rlp"
+import {encode as rlpEncode} from "@onflow/rlp"
 
 const rightPaddedHexBuffer = (value, pad) =>
   Buffer.from(value.padEnd(pad * 2, 0), "hex")
@@ -17,32 +17,30 @@ export const encodeMessageForProvableAuthnSigning = (
 ) => {
   invariant(
     address,
-    "Encode Message From Provable Authn Error: address must be defined"
+    "Encode Message For Provable Authn Error: address must be defined"
   )
   invariant(
     timestamp,
-    "Encode Message From Provable Authn Error: timestamp must be defined"
+    "Encode Message For Provable Authn Error: timestamp must be defined"
   )
 
   const USER_DOMAIN_TAG = rightPaddedHexBuffer(
     Buffer.from("FLOW-V0.0-user").toString("hex"),
     32
-  ).toString("hex")
+  )
 
   const APP_DOMAIN_TAG = appDomainTag
     ? rightPaddedHexBuffer(Buffer.from(appDomainTag).toString("hex"), 32)
     : null
 
-  return (
-    USER_DOMAIN_TAG +
-    Buffer.concat([
-      appDomainTag
-        ? encode([
-            APP_DOMAIN_TAG,
-            addressBuffer(sansPrefix(address)),
-            timestamp,
-          ])
-        : encode([addressBuffer(sansPrefix(address)), timestamp]),
-    ]).toString("hex")
-  )
+  return Buffer.concat([
+    USER_DOMAIN_TAG,
+    appDomainTag
+      ? rlpEncode([
+          APP_DOMAIN_TAG,
+          addressBuffer(sansPrefix(address)),
+          timestamp,
+        ])
+      : rlpEncode([addressBuffer(sansPrefix(address)), timestamp]),
+  ]).toString("hex")
 }
--- packages/fcl/src/wallet-utils/encode-provable-authn-verifying.js
+++ packages/fcl/src/wallet-utils/encode-provable-authn-verifying.js
@@ -1,6 +1,6 @@
 import {sansPrefix} from "@onflow/util-address"
 import {invariant} from "@onflow/util-invariant"
-import {encode} from "@onflow/rlp"
+import {encode as rlpEncode} from "@onflow/rlp"
 
 const rightPaddedHexBuffer = (value, pad) =>
   Buffer.from(value.padEnd(pad * 2, 0), "hex")
@@ -17,20 +17,24 @@ export const encodeMessageForProvableAuthnVerifying = (
 ) => {
   invariant(
     address,
-    "Encode Message From Provable Authn Error: address must be defined"
+    "Encode Message For Provable Authn Error: address must be defined"
   )
   invariant(
     timestamp,
-    "Encode Message From Provable Authn Error: timestamp must be defined"
+    "Encode Message For Provable Authn Error: timestamp must be defined"
   )
 
   const APP_DOMAIN_TAG = appDomainTag
     ? rightPaddedHexBuffer(Buffer.from(appDomainTag).toString("hex"), 32)
     : null
 
-  return Buffer.concat([
+  return (
     appDomainTag
-      ? encode([APP_DOMAIN_TAG, addressBuffer(sansPrefix(address)), timestamp])
-      : encode([addressBuffer(sansPrefix(address)), timestamp]),
-  ]).toString("hex")
+      ? rlpEncode([
+          APP_DOMAIN_TAG,
+          addressBuffer(sansPrefix(address)),
+          timestamp,
+        ])
+      : rlpEncode([addressBuffer(sansPrefix(address)), timestamp])
+  ).toString("hex")
 }
