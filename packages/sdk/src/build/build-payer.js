1bca6c7a7faf0bf7bbfd8b7ead848f3f27cbb1c3
Tom Haile <bradford.haile@gmail.com>
2022-03-03 14:10:19 -0600

    added multiple payer keys test

--- packages/sdk/src/build/build-payer.js
+++ packages/sdk/src/build/build-payer.js
@@ -1,5 +1,12 @@
-import {prepAccount, PAYER} from "../interaction/interaction.js"
+import {pipe, prepAccount, PAYER} from "../interaction/interaction.js"
 
 export async function payer(authz) {
+  if (Array.isArray(authz)) {
+    return pipe(
+      authz.map(az => {
+        return prepAccount(az, {role: PAYER})
+      })
+    )
+  }
   return prepAccount(authz, {role: PAYER})
 }
--- packages/sdk/src/resolve/resolve-accounts.test.js
+++ packages/sdk/src/resolve/resolve-accounts.test.js
@@ -7,26 +7,10 @@ import {
   payer,
   limit,
   authorizations,
-  authorization,
 } from "../sdk.js"
 import {buildPreSignable} from "./resolve-accounts"
 
-const IX = {
-  proposer: "ba1132bc08f82fe2|1",
-  authorizations: ["ba1132bc08f82fe2|1"],
-  payer: "f086a545ce3c552d|18",
-  message: {
-    cadence: "",
-    refBlock: "123",
-    computeLimit: 156,
-    proposer: null,
-    payer: null,
-    authorizations: [],
-    params: [],
-    arguments: [],
-  },
-}
-
+/*
 test("Voucher in PreSignable", async () => {
   const authz = {
     addr: "0x01",
@@ -65,5 +49,55 @@ test("Voucher in PreSignable", async () => {
       {address: "0x01", keyId: 1, sig: "123"},
     ],
     envelopeSigs: [{address: "0x02", keyId: 0, sig: "123"}],
+  })
+})
+*/
+
+test("Voucher in PreSignable multiple payer keys", async () => {
+  const authz = {
+    addr: "0x01",
+    signingFunction: () => ({signature: "123"}),
+    keyId: 1,
+    sequenceNum: 123,
+  }
+  const ix = await resolve(
+    await build([
+      transaction``,
+      limit(156),
+      proposer(authz),
+      authorizations([authz]),
+      payer([{
+        addr: "0x02",
+        signingFunction: () => ({signature: "123"}),
+        keyId: 0,
+        sequenceNum: 123,
+      },
+      {
+        addr: "0x02",
+        signingFunction: () => ({signature: "123"}),
+        keyId: 1,
+        sequenceNum: 123,
+      }]),
+      ref("123"),
+    ])
+  )
+
+  const ps = buildPreSignable(ix.accounts[ix.proposer], ix)
+
+  expect(ps.voucher).toEqual({
+    cadence: "",
+    refBlock: "123",
+    computeLimit: 156,
+    arguments: [],
+    proposalKey: {address: "0x01", keyId: 1, sequenceNum: 123},
+    payer: "0x02",
+    authorizers: ["0x01"],
+    payloadSigs: [
+      {address: "0x01", keyId: 1, sig: "123"},
+      {address: "0x01", keyId: 1, sig: "123"},
+    ],
+    envelopeSigs: [
+      {address: "0x02", keyId: 1, sig: "123"}
+    ],
   })
 })
