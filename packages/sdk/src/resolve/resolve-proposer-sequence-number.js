05b42fbed54397981967b34d340d3e9fd5ba525f
Jeffrey Doyle <jeffrey.doyle@dapperlabs.com>
2022-03-09 13:39:26 -0800

    PKG -- [sdk] Uses Buffer from @onflow/rlp

--- packages/sdk/src/resolve/resolve-proposer-sequence-number.js
+++ packages/sdk/src/resolve/resolve-proposer-sequence-number.js
@@ -6,6 +6,7 @@ import {decodeResponse} from "../decode/decode.js"
 import {getAccount} from "../build/build-get-account.js"
 import {build} from "../build/build.js"
 import {invariant} from "@onflow/util-invariant"
+import {Buffer} from "@onflow/rlp"
 
 export const resolveProposerSequenceNumber = ({ node }) => async (ix) => {
   if (!(isTransaction(ix))) return Ok(ix)
@@ -24,7 +25,7 @@ export const resolveProposerSequenceNumber = ({ node }) => async (ix) => {
     await build([
       getAccount(ix.accounts[ix.proposer].addr)
     ]),
-    {config, response: responseModule, ix: ixModule},
+    {config, response: responseModule, Buffer, ix: ixModule},
     {node}
   )
   const decoded = await decodeResponse(response)
