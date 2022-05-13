2153b667bf21a4922280b3e75915c2e84447d21b
Greg Santos <greg.santos@dapperlabs.com>
2022-05-13 09:18:39 -0700

    PKG -- [fcl-wc] Create pkg, add WC deps

---
+++ packages/fcl-wc/src/fcl-wc.js
@@ -0,0 +1,9 @@
+import Client, {CLIENT_EVENTS} from "@walletconnect/client"
+import QRCodeModal from "@walletconnect/legacy-modal"
+
+const fclWC = {
+  Client,
+  CLIENT_EVENTS,
+  QRCodeModal,
+}
+export default fclWC
