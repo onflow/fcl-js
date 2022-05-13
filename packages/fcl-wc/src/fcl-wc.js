0096bd85b380694cef9edc5d77901ee53971aa61
Greg Santos <greg.santos@dapperlabs.com>
2022-05-13 16:31:59 -0700

    PKG -- [fcl-wc] Add init, wc utils

--- packages/fcl-wc/src/fcl-wc.js
+++ packages/fcl-wc/src/fcl-wc.js
@@ -1,9 +1,33 @@
 import Client, {CLIENT_EVENTS} from "@walletconnect/client"
 import QRCodeModal from "@walletconnect/legacy-modal"
+import {ERROR, getAppMetadata} from "@walletconnect/utils"
+
+const INIT_DATA = {
+  RELAY_URL: "wss://relay.walletconnect.com",
+  LOGGER: "debug",
+}
+const data = {
+  client: null,
+}
 
 const fclWC = {
+  init: async projectId => {
+    if (data.client) {
+      return data.client
+    }
+    data.client = await Client.init({
+      logger: INIT_DATA.LOGGER,
+      relayUrl: INIT_DATA.RELAY_URL,
+      projectId: projectId,
+    })
+
+    return {client: data.client, CLIENT_EVENTS, QRCodeModal}
+  },
   Client,
   CLIENT_EVENTS,
   QRCodeModal,
+  ERROR,
+  getAppMetadata,
 }
+
 export default fclWC
