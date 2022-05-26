8837698fe47cc10b022b7b3803595cbf0144d9db
Jordan Ribbink <jribbink@telus.net>
2022-05-26 16:44:16 -0700

    fix whitespace

--- packages/sdk/src/build/build-get-events.js
+++ packages/sdk/src/build/build-get-events.js
@@ -6,7 +6,7 @@ export function getEvents(eventType, start, end) {
     logger.log({
       title: "FCL/SDK Deprecation Notice",
       message: `Passing a start and end into getEnvents has been deprecated and will not be supported in future versions of the Flow JS-SDK/FCL.
-      You can learn more (including a guide on common transition paths) here: https://github.com/onflow/flow-js-sdk/blob/master/packages/sdk/TRANSITIONS.md#0005-deprecate-start-end-get-events-builder`,
+You can learn more (including a guide on common transition paths) here: https://github.com/onflow/flow-js-sdk/blob/master/packages/sdk/TRANSITIONS.md#0005-deprecate-start-end-get-events-builder`,
       level: logger.LEVELS.warn,
     })
   }
