48b55545f9f45f6ee1a6bb56be5bfffdf1655473
gregsantos <greg.santos@dapperlabs.com>
2022-08-15 17:12:18 -0700

    PKG -- [fcl] refactor getDiscoveryService

---
+++ packages/fcl/src/discovery/utils.js
@@ -0,0 +1,35 @@
+import {config} from "@onflow/config"
+import {invariant} from "@onflow/util-invariant"
+
+// Certain method types cannot be overridden to use other methods like POP/RCP
+const isServiceMethodUnchangable = method =>
+  ["EXT/RPC", "WC/RPC"].includes(method)
+
+export async function getDiscoveryService(service) {
+  const discoveryAuthnInclude = await config.get("discovery.authn.include", [])
+  const discoveryWalletMethod = await config.first([
+    "discovery.wallet.method",
+    "discovery.wallet.method.default",
+  ])
+  const method = isServiceMethodUnchangable(service?.method)
+    ? service.method
+    : discoveryWalletMethod || service.method
+  const endpoint =
+    service?.endpoint ??
+    (await config.first(["discovery.wallet", "challenge.handshake"]))
+
+  invariant(
+    endpoint,
+    `
+      If no service is passed to "authenticate," then "discovery.wallet" must be defined in fcl config.
+      See: "https://docs.onflow.org/fcl/reference/api/#setting-configuration-values"
+    `
+  )
+
+  return {
+    type: "authn",
+    endpoint,
+    method,
+    discoveryAuthnInclude,
+  }
+}
