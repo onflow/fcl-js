2b25fbae1d4eed777b91f57d323a5e620e49c87c
gregsantos <greg.santos@dapperlabs.com>
2022-08-22 13:28:54 -0700

    PKG -- [fcl] add validation

--- packages/fcl/src/current-user/exec-service/plugins.js
+++ packages/fcl/src/current-user/exec-service/plugins.js
@@ -5,6 +5,7 @@ import {execTabRPC} from "./strategies/tab-rpc"
 import {execExtRPC} from "./strategies/ext-rpc"
 import {invariant} from "@onflow/util-invariant"
 import {log} from "@onflow/util-logger"
+import {isRequired, isString, isObject, isFunc} from "../../exec/utils/is"
 
 const CORE_STRATEGIES = {
   "HTTP/RPC": execHttpPost,
@@ -17,25 +18,51 @@ const CORE_STRATEGIES = {
 
 const supportedPlugins = ["discovery-service"]
 
+const validateDiscoveryServices = discoveryServices => {
+  let discoveryServiceArray
+  invariant(discoveryServices.length, "Discovery services are required")
+
+  if (!Array.isArray(discoveryServices)) {
+    discoveryServiceArray = [discoveryServices]
+  } else {
+    discoveryServiceArray = [...discoveryServices]
+  }
+  for (const ds of discoveryServiceArray) {
+    invariant(
+      isRequired(ds.definition) &&
+        isObject(ds.definition) &&
+        ds.definition.f_type === "Service",
+      "Service definition is required"
+    )
+    invariant(
+      isRequired(ds.strategy) && isFunc(ds.strategy),
+      "Service strategy is required"
+    )
+  }
+
+  return discoveryServiceArray
+}
+
 const ServiceRegistry = () => {
   let services = new Set()
   let strategies = new Map(Object.entries(CORE_STRATEGIES))
 
-  const setServices = pluginServices =>
-    (services = new Set([...pluginServices]))
+  const setServices = discoveryServicePlugins =>
+    (services = new Set([...discoveryServicePlugins]))
 
   const getServices = () => [...services].map(service => service.definition)
 
-  const add = servicePlugins => {
-    invariant(servicePlugins.length, "Discovery services are required")
-    setServices(servicePlugins)
-    for (const s of servicePlugins) {
-      if (!strategies.has(s.definition?.method)) {
-        strategies.set(s.definition?.method, s.strategy)
+  const add = discoveryServices => {
+    const discoveryServicesArray = validateDiscoveryServices(discoveryServices)
+
+    setServices(discoveryServicesArray)
+    for (const dsp of discoveryServicesArray) {
+      if (!strategies.has(dsp.definition?.method)) {
+        strategies.set(dsp.definition?.method, dsp.strategy)
       } else {
         log({
           title: `Add Service Plugin`,
-          message: `Service strategy for ${s.definition.method} already exists`,
+          message: `Service strategy for ${dsp.definition.method} already exists`,
           level: 2,
         })
       }
@@ -59,7 +86,7 @@ const validatePlugins = plugins => {
     pluginsArray = [...plugins]
   }
   for (const p of pluginsArray) {
-    invariant(p.name, "Plugin name is required")
+    invariant(isRequired(p.name), "Plugin name is required")
     invariant(
       supportedPlugins.includes(p.type),
       `Plugin type ${p.type} is not supported`
@@ -79,7 +106,7 @@ const PluginRegistry = () => {
     for (const p of pluginsArray) {
       pluginsMap.set(p.name, p)
       if (p.type === "discovery-service") {
-        serviceRegistry.add(p.services)
+        serviceRegistry.add(p.discoveryServices)
       }
     }
   }
