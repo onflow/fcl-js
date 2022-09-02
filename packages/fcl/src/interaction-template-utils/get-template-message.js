f14b730c52bec664bda7bf222e3f0c0ab9c70f40
Jeff Doyle <jeffrey.doyle@dapperlabs.com>
2022-09-02 14:45:02 -0700

    PKG -- [fcl] Adds interaction template message getter utils (#1397)
    * PKG -- [fcl] Adds interaction template message getter utils
    
    * PKG -- [fcl] Adds changeset

---
+++ packages/fcl/src/interaction-template-utils/get-template-message.js
@@ -0,0 +1,42 @@
+import {invariant} from "@onflow/sdk"
+
+export function getTemplateMessage({
+  localization = "en-US",
+  messageKey,
+  template,
+}) {
+  invariant(
+    messageKey,
+    "getMessage({ messageKey }) -- messageKey must be defined"
+  )
+  invariant(
+    typeof messageKey === "stirng",
+    "getMessage({ messageKey }) -- messageKey must be a string"
+  )
+
+  invariant(
+    localization,
+    "getMessage({ localization }) -- localization must be defined"
+  )
+  invariant(
+    typeof localization === "stirng",
+    "getMessage({ localization }) -- localization must be a string"
+  )
+
+  invariant(
+    template != undefined,
+    "generateTemplateId({ template }) -- template must be defined"
+  )
+  invariant(
+    typeof template === "object",
+    "generateTemplateId({ template }) -- template must be an object"
+  )
+  invariant(
+    typeof template.f_type === "InteractionTemplate",
+    "generateTemplateId({ template }) -- template object must be an InteractionTemplate"
+  )
+
+  const messages = template?.data?.messages
+
+  return messages?.[messageKey]?.i18n?.[localization]
+}
