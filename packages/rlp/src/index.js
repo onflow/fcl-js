fb4af2a6ce054fc5abd806dbb7234522b78dcfc4
Tom Haile <bradford.haile@gmail.com>
2022-10-28 16:43:15 -0500

    export rlp decode (#1471)
    * export rlp decode
    
    * add doc header to method
    
    * adding back in decode method as pass through
    
    * add changeset file

--- packages/rlp/src/index.js
+++ packages/rlp/src/index.js
@@ -54,7 +54,22 @@ function encodeLength(len, offset) {
   }
 }
 
-function decode(input, stream) {
+/**
+ * Built on top of rlp library, removing the BN dependency for the flow.
+ * Package : https://github.com/ethereumjs/rlp
+ * RLP License : https://github.com/ethereumjs/rlp/blob/master/LICENSE
+ *
+ * ethereumjs/rlp is licensed under the
+ * Mozilla Public License 2.0
+ * Permissions of this weak copyleft license are conditioned on making available source code of licensed files and modifications of those files under the same license (or in certain cases, one of the GNU licenses). Copyright and license notices must be preserved. Contributors provide an express grant of patent rights. However, a larger work using the licensed work may be distributed under different terms and without source code for files added in the larger work.
+ **/
+
+/**
+ * @param input - will be converted to buffer
+ * @param stream Is the input a stream (false by default)
+ * @returns returns buffer of encoded data
+ **/
+export function decode(input, stream) {
   if (stream === void 0) {
     stream = false
   }
