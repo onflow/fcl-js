ca9afa5a0034ea182531ec6687354545c5d1a328
huyndo <huy.nguyen@dapperlabs.com>
2022-11-01 16:16:46 -0700

    Update fcl-wc test to work with getChainId

--- packages/fcl-wc/src/fcl-wc.test.js
+++ packages/fcl-wc/src/fcl-wc.test.js
@@ -1,10 +1,21 @@
-import { config } from "@onflow/config"
-import * as fclWC from "./fcl-wc"
+import {config} from "@onflow/config"
 
 describe("Init Client", () => {
   it("should throw without projectId", async () => {
+    async function initWC() {
+      await config.overload(
+        {
+          "flow.network.default": "testnet",
+        },
+        fclWC.init
+      )
+    }
+
+    // Mock transport then import fcl-wc because startup of fcl will call getChainId util which hits the chain
+    config.put("sdk.transport", async ix => ix)
+    const fclWC = require("./fcl-wc")
+
     expect.assertions(1)
-    config.put("accessNode.api", "https://rest-testnet.onflow.org")
-    await expect(fclWC.init()).rejects.toThrow(Error)
+    await expect(initWC).rejects.toThrow(Error)
   })
 })
