fda3829955678c9d4c14279457b9a75f8bfa945b
Leon Li <leon.li@dapperlabs.com>
2022-11-08 18:11:35 -0500

    chore: replace fcl-bundle with tsc

--- packages/fcl-bundle/src/cli.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-const program = require("./program")
-const getPackageJSON = require("./get-package-json")
-const parsePackageJSON = require("./package-config")
-
-const package = getPackageJSON()
-const config = parsePackageJSON(package)
-
-program(config)(package)
