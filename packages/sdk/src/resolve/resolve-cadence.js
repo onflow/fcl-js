cb26a2468d4435d46b9dcc0ffb527be367970f3d
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-11-10 13:56:17 -0800

    Revert "PKG -- [sdk] Add Cadence handler for load placeholders"
    This reverts commit 72f2a83d4fdcc2a5d0ea7215af25608ebc9708c1.

--- packages/sdk/src/resolve/resolve-cadence.js
+++ packages/sdk/src/resolve/resolve-cadence.js
@@ -1,7 +1,6 @@
 import {isTransaction, isScript, get} from "../interaction/interaction.js"
 import {invariant} from "@onflow/util-invariant"
 import {config} from "@onflow/config"
-import {sansPrefix} from "@onflow/util-address"
 
 const isFn = v => typeof v === "function"
 const isString = v => typeof v === "string"
@@ -15,19 +14,12 @@ export async function resolveCadence(ix) {
     )
     if (isFn(cadence)) cadence = await cadence({})
     invariant(isString(cadence), "Cadence needs to be a string at this point.")
-
     ix.message.cadence = await config()
       .where(/^0x/)
       .then(d =>
         Object.entries(d).reduce((cadence, [key, value]) => {
-          // config.load let's you write 'import HelloWorld from HelloWorld' but under the hood stores them with 0x to identify contracts
-          const regexWithoutPrefix = new RegExp("(?<=\\bfrom\\s)" + sansPrefix(key), "g")
-          // Manual placeholder config also exists for 'import HelloWorld from 0xHelloWorldTwo'
-          const regexWithPrefix = new RegExp("(\\b" + key + "\\b)", "g")
-
-          return cadence
-            .replace(regexWithoutPrefix, value)
-            .replace(regexWithPrefix, value)
+          const regex = new RegExp("(\\b" + key + "\\b)", "g")
+          return cadence.replace(regex, value)
         }, cadence)
       )
   }
