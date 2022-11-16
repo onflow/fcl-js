504e19e8b9238e0dfda0a2eab9acdd15f81ae66c
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2022-11-16 15:53:54 -0800

    PKG -- [util-address] Add types to util-address

---
+++ packages/util-address/types/index.d.ts
@@ -0,0 +1,18 @@
+/**
+ * Removes 0x from address if present
+ * @param {string} address
+ * @returns {string}
+ */
+export function sansPrefix(address: string): string;
+/**
+ * Adds 0x to address if not already present
+ * @param {string} address
+ * @returns {string}
+ */
+export function withPrefix(address: string): string;
+/**
+ * Adds 0x to address if not already present
+ * @param {string} address
+ * @returns {string}
+ */
+export function display(address: string): string;
