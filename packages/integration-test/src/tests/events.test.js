61f924f098961fd72d59a60270af788051150bc6
huyndo <huy.nguyen@dapperlabs.com>
2022-12-12 11:58:11 -0800

    Add events test

--- packages/integration-test/src/tests/events.test.js
+++ packages/integration-test/src/tests/events.test.js
@@ -1,5 +1,5 @@
 import * as fcl from "@onflow/fcl"
-const execSync = require("child_process").execSync
+import { createAccountCLI } from "../../test-utils/test-utils"
 
 describe("events test", () => {
   beforeAll(() => {
@@ -14,14 +14,8 @@ describe("events test", () => {
     let eventList = []
 
     // Create two accounts to make a block (events need at least one block in the chain to work)
-    let output = execSync(
-      "flow accounts create --key e570c319765cf506af22234c5839d98e5a4f11cc199429133aa012e4745e454453efa698f1b05c34520a13ce8636d990c35fd45b6ac3681b4bc3a534ddc3545e",
-      {encoding: "utf-8"}
-    )
-    output = execSync(
-      "flow accounts create --key 8c1a50fd1f84561c1c439c74ae863feb36a4803a1645e70d4d39919abb15c638fca5ebfd1e71145ff5eaac8c241d1fd9fe9741d7d089b14ffeea3e307ed5df2e",
-      {encoding: "utf-8"}
-    )
+    createAccountCLI("e570c319765cf506af22234c5839d98e5a4f11cc199429133aa012e4745e454453efa698f1b05c34520a13ce8636d990c35fd45b6ac3681b4bc3a534ddc3545e")
+    createAccountCLI("8c1a50fd1f84561c1c439c74ae863feb36a4803a1645e70d4d39919abb15c638fca5ebfd1e71145ff5eaac8c241d1fd9fe9741d7d089b14ffeea3e307ed5df2e")
 
     let unsubscribeFn = fcl.events("flow.AccountCreated").subscribe(event => {
       eventList.push(event)
@@ -31,14 +25,8 @@ describe("events test", () => {
     await new Promise((res, rej) => setTimeout(res, 500))
 
     // Create two accounts to emit flow.AccountCreated event
-    output = execSync(
-      "flow accounts create --key b09b8cf7421df57411aec82c2b94df0e593212e02449421977f6972e53aaaf1be49493f08950ee52dd115d52eb8523e7653a0d83ac9c8617d857eda96285497d",
-      {encoding: "utf-8"}
-    )
-    output = execSync(
-      "flow accounts create --key dfa5402be121667bb3987e7afe1d4ab2d1526afc1516b2fa7bde3e36eb99dc0c23c576c8caebc56a98d4c61591e372f01363ccd6d55f5f1eb121bbc8b8f3b70a",
-      {encoding: "utf-8"}
-    )
+    createAccountCLI("b09b8cf7421df57411aec82c2b94df0e593212e02449421977f6972e53aaaf1be49493f08950ee52dd115d52eb8523e7653a0d83ac9c8617d857eda96285497d")
+    createAccountCLI("dfa5402be121667bb3987e7afe1d4ab2d1526afc1516b2fa7bde3e36eb99dc0c23c576c8caebc56a98d4c61591e372f01363ccd6d55f5f1eb121bbc8b8f3b70a")
 
     // wait for previous transactions to be sealed
     await new Promise((res, rej) => setTimeout(res, 500))
@@ -51,14 +39,8 @@ describe("events test", () => {
     let eventList = []
 
     // Create two accounts to make a block (events need at least one block in the chain to work)
-    let output = execSync(
-      "flow accounts create --key bc109999c133a4de49e699f7ec597872afc1f5e8c6f2a98b88cc9165ee047074fede4622ee8c49c9c74995c73f855877aa571a46035df3e2222548129ce76f96",
-      {encoding: "utf-8"}
-    )
-    output = execSync(
-      "flow accounts create --key 7b5e223c0efaffc120f4137c5d178ec3ec55438bc065111986745969f6749822afac6b7a23136bce9b66fce1103884e5bc7764b1ed6d08f1eee5918d862b8ab6",
-      {encoding: "utf-8"}
-    )
+    createAccountCLI("bc109999c133a4de49e699f7ec597872afc1f5e8c6f2a98b88cc9165ee047074fede4622ee8c49c9c74995c73f855877aa571a46035df3e2222548129ce76f96")
+    createAccountCLI("7b5e223c0efaffc120f4137c5d178ec3ec55438bc065111986745969f6749822afac6b7a23136bce9b66fce1103884e5bc7764b1ed6d08f1eee5918d862b8ab6")
 
     let unsubscribeFn = fcl.events("flow.AccountKeyAdded").subscribe(event => {
       eventList.push(event)
@@ -74,13 +56,9 @@ describe("events test", () => {
     let publicKeys = [publicKey1, publicKey2]
 
     // Create two accounts to add keys to emit flow.AccountKeyAdded event
-    output = execSync(`flow accounts create --key ${publicKeys[0]}`, {
-      encoding: "utf-8",
-    })
-    output = execSync(`flow accounts create --key ${publicKeys[1]}`, {
-      encoding: "utf-8",
-    })
-
+    createAccountCLI(publicKeys[0])
+    createAccountCLI(publicKeys[1])
+    
     // wait for previous transactions to be sealed
     await new Promise((res, rej) => setTimeout(res, 500))
     await unsubscribeFn()
