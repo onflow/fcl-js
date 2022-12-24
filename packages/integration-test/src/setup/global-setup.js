dba0cddc5e0211c90c6c5f5153e7201fad9528a5
gpavlov <guy.pavlov@dapperlabs.com>
2022-12-24 16:09:52 -0500

    fixed flow process termination

--- packages/integration-test/src/setup/global-setup.js
+++ packages/integration-test/src/setup/global-setup.js
@@ -1,10 +1,13 @@
 module.exports = async function () {
   var exec = require("child_process").exec
+  var waitOn = require('wait-on');
 
   const restPort = 8888
   const adminPort = 8080
   const grpcPort = 3569
-  const cmd = `flow emulator --rest-port=${restPort} --admin-port=${adminPort} --port=${grpcPort}`
+  const cmd = `flow emulator --rest-port=${restPort} --admin-port=${adminPort} --port=${grpcPort} &`
+
+  console.log('Launching emulator');
 
   globalThis.__emulator__ = exec(cmd, function (error, stdout, stderr) {
     // This will log emulator output at the end
@@ -14,4 +17,18 @@ module.exports = async function () {
       console.log("Emulator error: " + error)
     }
   })
+
+  try {
+    await waitOn({
+      resources: [
+        `tcp:127.0.0.1:${restPort}`,
+        `tcp:127.0.0.1:${adminPort}`,
+        `tcp:127.0.0.1:${grpcPort}`,
+      ],
+      log: true
+    })
+  } catch (err) {
+    console.log('Error waiting for port');
+  }
+
 }
