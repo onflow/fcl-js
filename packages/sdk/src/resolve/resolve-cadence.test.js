48ff43303c30bab86274bd281f6af28affdb2f25
justin <328965+justinbarry@users.noreply.github.com>
2023-02-06 09:40:53 -0800

    PKG -- [sdk] Add support for new import format

--- packages/sdk/src/resolve/resolve-cadence.test.js
+++ packages/sdk/src/resolve/resolve-cadence.test.js
@@ -2,70 +2,71 @@ import {interaction, pipe, put, makeScript} from "../interaction/interaction.js"
 import {resolveCadence} from "./resolve-cadence.js"
 import {config} from "@onflow/config"
 
-const log = msg => ix => (console.log(msg, ix), ix)
+const idle = () => new Promise(resolve => setTimeout(resolve), 0)
 
 describe("resolveCadence", () => {
-  test("cadence is a string", async () => {
-    const CADENCE = "CADENCE_STRING"
+  describe("0xHelloWorld-style account identifier syntax", () => {
+    test("cadence is a string", async () => {
+      const CADENCE = "CADENCE_STRING"
 
-    const ix = await pipe([
-      makeScript,
-      put("ix.cadence", CADENCE),
-      resolveCadence,
-    ])(interaction())
+      const ix = await pipe([
+        makeScript,
+        put("ix.cadence", CADENCE),
+        resolveCadence,
+      ])(interaction())
 
-    expect(ix.message.cadence).toBe(CADENCE)
-  })
+      expect(ix.message.cadence).toBe(CADENCE)
+    })
 
-  test("cadence is a function", async () => {
-    const CADENCE = async function () {
-      return "CADENCE_ASYNC_FUNCTION"
-    }
+    test("cadence is a function", async () => {
+      const CADENCE = async function () {
+        return "CADENCE_ASYNC_FUNCTION"
+      }
 
-    const ix = await pipe([
-      makeScript,
-      put("ix.cadence", CADENCE),
-      resolveCadence,
-    ])(interaction())
+      const ix = await pipe([
+        makeScript,
+        put("ix.cadence", CADENCE),
+        resolveCadence,
+      ])(interaction())
 
-    expect(ix.message.cadence).toBe(await CADENCE())
-  })
+      expect(ix.message.cadence).toBe(await CADENCE())
+    })
 
-  test("replaces all addresses from config", async () => {
-    const CADENCE = async function () {
-      return `
+    test("replaces all addresses from config", async () => {
+      const CADENCE = async function () {
+        return `
         import MyContract from 0xMY_CONTRACT_ADDRESS
   
         pub fun main(): Address {
           return 0xMY_CONTRACT_ADDRESS
         }
       `
-    }
+      }
 
-    const RESULT = async function () {
-      return `
+      const RESULT = async function () {
+        return `
         import MyContract from 0x123abc
   
         pub fun main(): Address {
           return 0x123abc
         }
       `
-    }
+      }
 
-    config.put("0xMY_CONTRACT_ADDRESS", "0x123abc")
+      await config.put("0xMY_CONTRACT_ADDRESS", "0x123abc")
 
-    const ix = await pipe([
-      makeScript,
-      put("ix.cadence", CADENCE),
-      resolveCadence,
-    ])(interaction())
+      const ix = await pipe([
+        makeScript,
+        put("ix.cadence", CADENCE),
+        resolveCadence,
+      ])(interaction())
 
-    expect(ix.message.cadence).toEqual(await RESULT())
-  })
+      expect(ix.message.cadence).toEqual(await RESULT())
+    })
 
-  test("similar config names do not replace each other", async () => {
-    const CADENCE = async function () {
-      return `
+    test("similar config names do not replace each other", async () => {
+      const CADENCE = async function () {
+        return `
         import FooBar from 0xFoo
         import FooBar from 0xFooBar
 
@@ -82,10 +83,10 @@ describe("resolveCadence", () => {
         pub fun otherTwo(): Address {return 0xFoo}
         pub fun otherThree(): Address {return 0xFooBar}
       `
-    }
+      }
 
-    const RESULT = async function () {
-      return `
+      const RESULT = async function () {
+        return `
         import FooBar from 0x123
         import FooBar from 0x456
 
@@ -102,16 +103,99 @@ describe("resolveCadence", () => {
         pub fun otherTwo(): Address {return 0x123}
         pub fun otherThree(): Address {return 0x456}
       `
-    }
+      }
 
-    config.put("0xFoo", "0x123").put("0xFooBar", "0x456")
+      config.put("0xFoo", "0x123").put("0xFooBar", "0x456")
 
-    const ix = await pipe([
-      makeScript,
-      put("ix.cadence", CADENCE),
-      resolveCadence,
-    ])(interaction())
+      const ix = await pipe([
+        makeScript,
+        put("ix.cadence", CADENCE),
+        resolveCadence,
+      ])(interaction())
 
-    expect(ix.message.cadence).toEqual(await RESULT())
+      expect(ix.message.cadence).toEqual(await RESULT())
+    })
+  })
+
+  describe("new Identifier syntax", () => {
+    test("single import statement", async () => {
+      const CADENCE = `import "Foo"
+
+pub fun main(): Address {
+  return "Foo"
+}`
+
+      const expected = `import Foo from 0x1
+
+pub fun main(): Address {
+  return "Foo"
+}`
+
+      await config().put("system.contracts.Foo", "0x1")
+      await idle()
+
+      const ix = await pipe([
+        makeScript,
+        put("ix.cadence", CADENCE),
+        resolveCadence,
+      ])(interaction())
+
+      expect(ix.message.cadence).toEqual(expected)
+    })
+
+    test("multiple import statements with only one defined", async () => {
+      const CADENCE = `import "Foo"
+import "Bar"
+
+pub fun main(): Address {
+  return "Foo"
+}`
+
+      const expected = `import Foo from 0x1
+import "Bar"
+
+pub fun main(): Address {
+  return "Foo"
+}`
+
+      await config().put("system.contracts.Foo", "0x1")
+      await idle()
+
+      const ix = await pipe([
+        makeScript,
+        put("ix.cadence", CADENCE),
+        resolveCadence,
+      ])(interaction())
+
+      expect(ix.message.cadence).toEqual(expected)
+    })
+
+    test("multiple import statements", async () => {
+      const CADENCE = `import "Foo"
+import "Bar"
+
+pub fun main(): Address {
+  return "Foo"
+}`
+
+      const expected = `import Foo from 0x1
+import Bar from 0x2
+
+pub fun main(): Address {
+  return "Foo"
+}`
+
+      await config().put("system.contracts.Foo", "0x1")
+      await config().put("system.contracts.Bar", "0x2")
+      await idle()
+
+      const ix = await pipe([
+        makeScript,
+        put("ix.cadence", CADENCE),
+        resolveCadence,
+      ])(interaction())
+
+      expect(ix.message.cadence).toEqual(expected)
+    })
   })
 })
