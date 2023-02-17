bd34c38cecf5b640593afcdd07d5d3709489dd37
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-02-17 10:34:16 -0800

    FCL -- [fcl] Add JSDoc for Wallet Utils send msg

--- packages/fcl/src/wallet-utils/send-msg-to-fcl.js
+++ packages/fcl/src/wallet-utils/send-msg-to-fcl.js
@@ -1,5 +1,22 @@
 import {onMessageFromFCL} from "./on-message-from-fcl"
 
+/**
+ * @description
+ * Sends message to FCL window
+ * 
+ * @param {string} type - Message type
+ * @param {Object} msg - Message object
+ * @returns {void}
+ * 
+ * @example
+ * sendMsgToFCL("FCL:VIEW:RESPONSE", {
+ *    f_type: "PollingResponse",
+ *    f_vsn: "1.0.0",
+ *    status: "APPROVED",
+ *    reason: null,
+ *    data: data,
+ *  })
+ */
 export const sendMsgToFCL = (type, msg = {}) => {
   if (window.location !== window.parent.location) {
     window.parent.postMessage({...msg, type}, "*")
@@ -10,15 +27,36 @@ export const sendMsgToFCL = (type, msg = {}) => {
   }
 }
 
+/**
+ * @description
+ * Listens for "FCL:VIEW:READY:RESPONSE" and sends "FCL:VIEW:READY"
+ * 
+ * @param {Function} cb - Callback function
+ * @param {Object} msg - Message object
+ * @returns {void}
+ */
 export const ready = (cb, msg = {}) => {
   onMessageFromFCL("FCL:VIEW:READY:RESPONSE", cb)
   sendMsgToFCL("FCL:VIEW:READY")
 }
 
+/**
+ * @description
+ * Sends "FCL:VIEW:CLOSE"
+ * 
+ * @returns {void}
+ */
 export const close = () => {
   sendMsgToFCL("FCL:VIEW:CLOSE")
 }
 
+/**
+ * @description
+ * Sends "FCL:VIEW:RESPONSE" with status "APPROVED"
+ * 
+ * @param {Object} data - Data object
+ * @returns {void}
+ */
 export const approve = data => {
   sendMsgToFCL("FCL:VIEW:RESPONSE", {
     f_type: "PollingResponse",
@@ -29,6 +67,13 @@ export const approve = data => {
   })
 }
 
+/**
+ * @description
+ * Sends "FCL:VIEW:RESPONSE" with status "DECLINED"
+ * 
+ * @param {string} reason - Reason for declining
+ * @returns {void}
+ */
 export const decline = reason => {
   sendMsgToFCL("FCL:VIEW:RESPONSE", {
     f_type: "PollingResponse",
@@ -39,6 +84,13 @@ export const decline = reason => {
   })
 }
 
+/**
+ * @description
+ * Sends "FCL:VIEW:RESPONSE" with status "REDIRECT"
+ * 
+ * @param {Object} data - Data object
+ * @returns {void}
+ */
 export const redirect = data => {
   sendMsgToFCL("FCL:VIEW:RESPONSE", {
     f_type: "PollingResponse",
