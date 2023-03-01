c6315a050b34d0d917833cf283c2a2a011503477
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-03-01 14:33:16 -0800

    PKG -- [sdk] Add jsdoc to builder functions

--- packages/sdk/src/build/build-get-collection.js
+++ packages/sdk/src/build/build-get-collection.js
@@ -1,5 +1,12 @@
 import {pipe, makeGetCollection} from "../interaction/interaction.js"
 
+/**
+ * @description - A builder function that returns all a collection containing a list of transaction ids by its collection id
+ * NOTE:
+ * - The block range provided must be from the current spork. All events emitted during past sporks is current unavailable.
+ * @param {string} id - The id of the collection to get
+ * @returns {object} - An interaction object
+ */
 export function getCollection(id = null) {
   return pipe([
     makeGetCollection,
