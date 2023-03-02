318720ab991c88d2273f18a9b20afa7505a669c8
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2023-03-02 15:48:09 -0800

    PKG -- [sdk] Change to optionals

--- packages/sdk/src/build/build-get-block-header.js
+++ packages/sdk/src/build/build-get-block-header.js
@@ -2,7 +2,7 @@ import {pipe, Ok, makeGetBlockHeader} from "../interaction/interaction.js"
 
 /**
  * @description - A builder function that returns the interaction to get a block header
- * @param {boolean} isSealed - Whether or not the block should be sealed
+ * @param {boolean} [isSealed] - Whether or not the block should be sealed
  * @returns {object} - An interaction object
  */
 export function getBlockHeader(isSealed = null) {
--- packages/sdk/src/build/build-get-block.js
+++ packages/sdk/src/build/build-get-block.js
@@ -2,7 +2,7 @@ import {pipe, Ok, makeGetBlock} from "../interaction/interaction.js"
 
 /**
  * @description - A builder function that returns the interaction to get the latest block
- * @param {boolean} isSealed - Whether or not the block should be sealed
+ * @param {boolean} [isSealed] - Whether or not the block should be sealed
  * @returns {object} - An interaction object
  */
 export function getBlock(isSealed = null) {
