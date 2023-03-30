a2e257b7c64dcf64dc46a010233511257d2a9ed5
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-03-30 13:37:04 -0400

    PKG -- [transport-grpc] additional check for CustomEvent

--- packages/transport-grpc/src/send-transaction.js
+++ packages/transport-grpc/src/send-transaction.js
@@ -106,7 +106,7 @@ export async function sendTransaction(ix, context = {}, opts = {}) {
   ret.tag = ix.tag
   ret.transactionId = u8ToHex(res.getId_asU8(), context)
 
-  if (typeof window !== "undefined") {
+  if (typeof window !== "undefined" && typeof CustomEvent !== "undefined") {
     window.dispatchEvent(
       new CustomEvent("FLOW::TX", {
         detail: {txId: ret.transactionId, delta: t2 - t1},
