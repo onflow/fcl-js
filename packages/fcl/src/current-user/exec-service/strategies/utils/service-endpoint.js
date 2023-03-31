0eaadceb7643996ad7f9be0929de7f305b03fbdb
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-03-31 09:46:23 -0400

    PKG -- [fcl] check for window.location.origin

--- packages/fcl/src/current-user/exec-service/strategies/utils/service-endpoint.js
+++ packages/fcl/src/current-user/exec-service/strategies/utils/service-endpoint.js
@@ -1,6 +1,8 @@
 export function serviceEndpoint(service) {
   const url = new URL(service.endpoint)
-  url.searchParams.append("l6n", window.location.origin)
+  if (window?.location?.origin) {
+    url.searchParams.append("l6n", window.location.origin)
+  }
   if (service.params != null) {
     for (let [key, value] of Object.entries(service.params || {})) {
       url.searchParams.append(key, value)
