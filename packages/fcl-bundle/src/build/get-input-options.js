257a78c73dce9319f7e6db9c6fc0430bf8d21968
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-06-05 13:10:19 -0400

    revert unnecessary changes

--- packages/fcl-bundle/src/build/get-input-options.js
+++ packages/fcl-bundle/src/build/get-input-options.js
@@ -59,7 +59,6 @@ module.exports = function getInputOptions(package, build) {
       nodeResolve({
         browser: true,
         preferBuiltins: build.type !== "umd",
-        resolveOnly: [/^(?!react)$/], // don't resolve react and react-native libraries
       }),
       babel({
         babelHelpers: "runtime",
