18b264a14400277a542411c204d52bc1edf2b78c
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-06-07 12:38:50 -0400

    DOC -- [fcl] generate jsdocs

--- packages/fcl/src/utils/react-native/ServiceDiscovery.js
+++ packages/fcl/src/utils/react-native/ServiceDiscovery.js
@@ -1,6 +1,13 @@
 import { useState, useEffect, createElement } from 'react'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
+/**
+ * Fetches data from a URL using the POST method and returns the parsed JSON response.
+ *
+ * @param {string} url - The URL to fetch.
+ * @param {object} opts - Additional options for the fetch request.
+ * @returns {Promise<object>} - A promise that resolves to the parsed JSON response.
+ */
 const fetcher = (url, opts) => {
   return fetch(url, {
     method: 'POST',
@@ -10,9 +17,29 @@ const fetcher = (url, opts) => {
     body: JSON.stringify(opts),
   }).then(d => d.json())
 }
-const DefaultLoadingComponent = () => createElement(Text, null, "Loading...");
-const DefaultEmptyComponent = () => createElement(Text, null, "No Wallets Found");
 
+/**
+ * Default loading component that renders the "Loading..." text.
+ *
+ * @returns {JSX.Element} - The loading component.
+ */
+const DefaultLoadingComponent = () => createElement(Text, null, "Loading...")
+
+/**
+ * Default empty component that renders the "No Wallets Found" text.
+ *
+ * @returns {JSX.Element} - The empty component.
+ */
+const DefaultEmptyComponent = () => createElement(Text, null, "No Wallets Found")
+
+/**
+ * Default service card component that renders a TouchableOpacity with the service provider's name as text.
+ *
+ * @param {object} props - The component props.
+ * @param {object} props.service - The service object.
+ * @param {Function} props.onPress - The onPress event handler.
+ * @returns {JSX.Element} - The service card component.
+ */
 const DefaultServiceCard = ({
   service,
   onPress
@@ -24,39 +51,59 @@ const DefaultServiceCard = ({
   )
 }
 
-const DefaultWrapper = ({ children }) => createElement(View, { style: styles.container}, ...children)
+/**
+ * Default wrapper component that renders a View with the specified style and children components.
+ *
+ * @param {object} props - The component props.
+ * @param {JSX.Element[]} props.children - The children components.
+ * @returns {JSX.Element} - The wrapper component.
+ */
+const DefaultWrapper = ({ children }) => createElement(View, { style: styles.container }, ...children)
 
+/**
+ * Custom hook for service discovery.
+ *
+ * @param {object} params - The hook parameters.
+ * @param {object} params.fcl - The fcl instance.
+ * @returns {object} - The service discovery result object.
+ * @property {object[]} services - The discovered services.
+ * @property {boolean} isLoading - A flag indicating whether the services are being loaded.
+ * @property {Function} authenticateService - A function to authenticate a service.
+ */
 export const useServiceDiscovery = ({ fcl }) => {
   const [services, setServices] = useState([])
   const [isLoading, setIsLoading] = useState(false)
-  
+
   const getServices = async () => {
-    setIsLoading(true);
+    setIsLoading(true)
     const endpoint = await fcl.config.get("discovery.authn.endpoint")
     try {
       const response = await fetcher(endpoint, {
         fclVersion: fcl.VERSION,
         userAgent: 'ReactNative',
-        supportedStrategies: [
-          'HTTP/POST',
-        ],
+        supportedStrategies: ['HTTP/POST'],
       })
       setServices(response)
       setIsLoading(false)
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   useEffect(() => {
     getServices()
-  }, []);
+  }, [])
 
+  /**
+   * Authenticates the provided service using the fcl instance.
+   *
+   * @param {object} service - The service object to authenticate.
+   */
   const authenticateService = (service) => {
     if (services.includes(service)) {
-      fcl.authenticate({service})
+      fcl.authenticate({ service })
     }
-  } 
+  }
 
   return {
     services,
@@ -65,26 +112,29 @@ export const useServiceDiscovery = ({ fcl }) => {
   }
 }
 
-// interface IWebBrowserProps {
-//   fcl: FCL,
-//   Loading: () => JSX.Element,
-//   Empty: () => JSX.Element,
-//   Wrapper: () => JSX.Element,
-//   ServiceCard: ({service, onPress, key}) => JSX.Element,
-// }
-
+/**
+ * Component for service discovery.
+ *
+ * @param {object} props - The component props.
+ * @param {object} props.fcl - The fcl instance.
+ * @param {Function} [props.Loading=DefaultLoadingComponent] - The loading component.
+ * @param {Function} [props.Empty=DefaultEmptyComponent] - The empty component.
+ * @param {Function} [props.ServiceCard=DefaultServiceCard] - The service card component.
+ * @param {Function} [props.Wrapper=DefaultWrapper] - The wrapper component.
+ * @returns {JSX.Element} - The service discovery component.
+ */
 export const ServiceDiscovery = ({
-  fcl, 
-  Loading = DefaultLoadingComponent, 
-  Empty = DefaultEmptyComponent, 
-  ServiceCard = DefaultServiceCard, 
+  fcl,
+  Loading = DefaultLoadingComponent,
+  Empty = DefaultEmptyComponent,
+  ServiceCard = DefaultServiceCard,
   Wrapper = DefaultWrapper
 }) => {
   const { services, isLoading, authenticateService } = useServiceDiscovery({ fcl })
 
   return createElement(
     Wrapper,
-    null, 
+    null,
     isLoading && createElement(Loading),
     !isLoading && services.length === 0 && createElement(Empty),
     !isLoading && services.map((service, index) => {
@@ -96,8 +146,9 @@ export const ServiceDiscovery = ({
             authenticateService(service)
           }
         })
-      )})
-  );
+      )
+    })
+  )
 }
 
 const styles = StyleSheet.create({
@@ -107,4 +158,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
