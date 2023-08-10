6f2de996a138f5b4460078be27f857d420d5a4c0
Jordan Ribbink <jribbink@telus.net>
2023-08-10 15:50:06 -0700

    stash

--- packages/util-uid/src/util-uid.ts
+++ packages/util-uid/src/util-uid.ts
@@ -1,11 +1,10 @@
-/**
- * Generates a unique identifier
- * @returns {string}
- */
 var HEX = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 var T = HEX.length
 
-export function uid() {
+/**
+ * Generates a unique 32 character alphanumeric string
+ */
+export function uid(): string {
   var str = "",
     num = 32
   while (num--) str += HEX[(Math.random() * T) | 0]
