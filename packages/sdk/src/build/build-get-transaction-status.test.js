cefddbf79300c97d604c655fc7e9f810bf1ae515
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2023-08-25 16:53:40 -0400

    fixed tests

--- packages/sdk/src/build/build-get-transaction-status.test.js
+++ packages/sdk/src/build/build-get-transaction-status.test.js
@@ -1,5 +1,5 @@
 import {
-  interaction,
+  initInteraction,
   isGetTransactionStatus,
 } from "../interaction/interaction"
 import {getTransactionStatus} from "./build-get-transaction-status.js"
@@ -8,7 +8,7 @@ describe("Build Get Transaction Status", () => {
   test("Get Transaction Status", async () => {
     const transactionId = "abc123"
 
-    let ix = await getTransactionStatus(transactionId)(interaction())
+    let ix = await getTransactionStatus(transactionId)(initInteraction())
 
     expect(isGetTransactionStatus(ix)).toBe(true)
     expect(ix.transaction.id).toBe(transactionId)
