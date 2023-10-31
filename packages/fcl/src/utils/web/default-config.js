5d5aee4f0945d75e91068c83ec876eff8fde4335
Alex <12097569+nialexsan@users.noreply.github.com>
2023-10-31 14:14:49 -0400

    Revert "Switch to localStorage (#1782)"
    This reverts commit 3d037e8470d56935bbc07ad7177ac40907def660.

--- packages/fcl/src/utils/web/default-config.js
+++ packages/fcl/src/utils/web/default-config.js
@@ -1,8 +1,21 @@
-import {LOCAL_STORAGE} from "./storage"
+const isServerSide = () => typeof window === "undefined"
+
+const getSessionStorage = () => {
+  try {
+    const SESSION_STORAGE = {
+      can: !isServerSide(),
+      get: async key => JSON.parse(sessionStorage.getItem(key)),
+      put: async (key, value) => sessionStorage.setItem(key, JSON.stringify(value)),
+    }
+    return SESSION_STORAGE
+  } catch (error) {
+    return null
+  }
+}
 
 export const getDefaultConfig = () => {
   return {
     "discovery.wallet.method.default": "IFRAME/RPC",
-    "fcl.storage.default": LOCAL_STORAGE,
+    "fcl.storage.default": getSessionStorage(),
   }
 }
