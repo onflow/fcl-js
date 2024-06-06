b1da8807b7acbf8f219c7a26e413df768b4374f7
Jordan Ribbink <jribbink@telus.net>
2024-06-06 09:24:05 -0700

    update wc check

--- packages/fcl-core/src/current-user/exec-service/wc-check.ts
+++ packages/fcl-core/src/current-user/exec-service/wc-check.ts
@@ -3,18 +3,21 @@ import {getServiceRegistry} from "./plugins"
 
 const FC_WC_SERVICE_METHOD = "EXT/WC"
 
+const isServerSide = typeof window === "undefined"
+
 // Utility to notify the user if the Walletconnect service plugin has not been loaded
 export function checkWalletConnectEnabled() {
+  if (isServerSide) return
+
   const serviceRegistry = getServiceRegistry()
   const strategies = serviceRegistry.getStrategies()
 
   if (!strategies.includes(FC_WC_SERVICE_METHOD)) {
-    // TODO: Add link to documentation
     logger.log({
       title: "FCL WalletConnect Service Plugin",
       level: logger.LEVELS.error,
       message:
-        "All dApps are expected to register for a WalletConnect projectId & add this to their FCL configuration.  If you do not do so, users will be unable to use certain wallets to interact with your dApp.  See <<DOC LINK>> for more information.",
+        "All dApps are expected to register for a WalletConnect projectId & add this to their FCL configuration.  If you do not do so, users will be unable to use certain wallets to interact with your dApp.  See https://developers.flow.com/tools/clients/fcl-js/configure-fcl for more information.",
     })
   }
 }
