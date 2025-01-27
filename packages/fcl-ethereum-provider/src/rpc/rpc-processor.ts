1452dc074373177fe73c71c298758ed4ece867c8
Chase Fleming <chasefleming@users.noreply.github.com>
2025-01-27 15:04:47 -0800

    Setup eth accounts handler (#2079)
    * Setup eth accounts handler
    
    * Run prettier
    
    ---------
    
    Co-authored-by: Chase Fleming <1666730+chasefleming@users.noreply.github.com>

--- packages/fcl-ethereum-provider/src/rpc/rpc-processor.ts
+++ packages/fcl-ethereum-provider/src/rpc/rpc-processor.ts
@@ -1,15 +1,14 @@
 import {ProviderRequest} from "../types/provider"
-import {eth_accounts} from "./handlers/eth-accounts"
-
-const handlers = {
-  eth_accounts,
-}
+import {ethAccounts} from "./handlers/eth-accounts"
+import {AccountManager} from "../accounts/account-manager"
 
 export class RpcProcessor {
-  constructor() {}
+  constructor(private accountManager: AccountManager) {}
 
   async handleRequest({method, params}: ProviderRequest): Promise<any> {
     switch (method) {
+      case "eth_accounts":
+        return ethAccounts(this.accountManager)
       case "eth_requestAccounts":
         throw new Error("Not implemented")
       default:
