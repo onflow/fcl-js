b1b9e3e14f1e511a7385aa4dd0298ba5dc16be29
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-01-27 16:17:49 -0800

    Implement eth_requestAccounts

--- packages/fcl-ethereum-provider/src/accounts/account-manager.ts
+++ packages/fcl-ethereum-provider/src/accounts/account-manager.ts
@@ -1,14 +1,26 @@
 import * as fcl from "@onflow/fcl"
 
 export class AccountManager {
+  private accounts: string[] = [];
+
   constructor(private user: typeof fcl.currentUser) {}
 
   getAccounts(): string[] {
-    // ... get coa address
-    return []
+    return this.accounts;
+  }
+
+  setAccounts(accounts: string[]): void {
+    this.accounts = accounts;
   }
 
   subscribe(callback: (accounts: string[]) => void) {
-    // ...
+    this.user.subscribe(async () => {
+      const snapshot = await fcl.currentUser().snapshot();
+
+      const updatedAccounts = snapshot.addr ? [snapshot.addr] : [];
+      this.setAccounts(updatedAccounts);
+
+      callback(this.getAccounts());
+    });
   }
 }
