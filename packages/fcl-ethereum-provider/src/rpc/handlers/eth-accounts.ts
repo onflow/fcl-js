99fb09e41815b6ecd2e2353d884fb885398b0d90
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-01-28 10:24:35 -0800

    Remove

--- packages/fcl-ethereum-provider/src/rpc/handlers/eth-accounts.ts
+++ packages/fcl-ethereum-provider/src/rpc/handlers/eth-accounts.ts
@@ -1,24 +1,11 @@
 import {AccountManager} from "../../accounts/account-manager";
 
-async function getCOAAddress(accountManager: AccountManager): Promise<string> {
-  const accounts = accountManager.getAccounts();
-
-  if (accounts.length === 0) {
-    throw new Error("COA account not found for the authenticated user");
-  }
-
-  return accounts[0];
-}
-
 export function ethAccounts(accountManager: AccountManager): string[] {
   return accountManager.getAccounts();
 }
 
 export async function ethRequestAccounts(accountManager: AccountManager): Promise<string[]> {
   await accountManager.user.authenticate();
-
-  const coaAddress = await getCOAAddress(accountManager);
-  accountManager.setAccounts(coaAddress);
 
   return accountManager.getAccounts();
 }
