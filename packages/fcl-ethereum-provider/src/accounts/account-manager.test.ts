1a012a17e908cea2044e3408eb51a87e6493dc9e
Jordan Ribbink <jribbink@telus.net>
2025-01-29 12:46:16 -0800

    fix test

--- packages/fcl-ethereum-provider/src/accounts/account-manager.test.ts
+++ packages/fcl-ethereum-provider/src/accounts/account-manager.test.ts
@@ -14,139 +14,14 @@ jest.mock("@onflow/fcl", () => {
   }
 })
 
-const mockFcl = fcl as jest.Mocked<typeof fcl>
-
-describe("send transaction", () => {
-  beforeEach(() => {
-    jest.clearAllMocks()
-  })
-
-  test("send transaction mainnet", async () => {
-    const user = mockUser()
-    const accountManager = new AccountManager(user)
-
-    const mockTxResult = {
-      onceExecuted: jest.fn().mockResolvedValue({
-        events: [
-          {
-            type: "A.e467b9dd11fa00df.EVM.TransactionExecuted",
-            data: {
-              hash: ["12", "34"],
-            },
-          },
-        ],
-      }),
-    } as any as jest.Mocked<ReturnType<typeof fcl.tx>>
-
-    jest.mocked(fcl.tx).mockReturnValue(mockTxResult)
-    jest.mocked(fcl.mutate).mockResolvedValue("1111")
-
-    const tx = {
-      to: "0x1234",
-      value: "0",
-      data: "0x1234",
-      nonce: "0",
-      gasLimit: "0",
-      chainId: "747",
-    }
-
-    const result = await accountManager.sendTransaction(tx)
-
-    expect(result).toEqual("1234")
-    expect(fcl.mutate).toHaveBeenCalled()
-    expect(mockFcl.mutate.mock.calls[0][0]).toMatchObject({
-      cadence: expect.any(String),
-      args: expect.any(Function),
-      authz: user,
-      limit: 9999,
-    })
-
-    expect(mockFcl.tx).toHaveBeenCalledWith("1111")
-    expect(mockFcl.tx).toHaveBeenCalledTimes(1)
-    expect(mockTxResult.onceExecuted).toHaveBeenCalledTimes(1)
-  })
-
-  test("send transaction testnet", async () => {
-    const user = mockUser()
-    const accountManager = new AccountManager(user)
-
-    const mockTxResult = {
-      onceExecuted: jest.fn().mockResolvedValue({
-        events: [
-          {
-            type: "A.8c5303eaa26202d6.EVM.TransactionExecuted",
-            data: {
-              hash: ["12", "34"],
-            },
-          },
-        ],
-      }),
-    } as any as jest.Mocked<ReturnType<typeof fcl.tx>>
-
-    jest.mocked(fcl.tx).mockReturnValue(mockTxResult)
-    jest.mocked(fcl.mutate).mockResolvedValue("1111")
-
-    const tx = {
-      to: "0x1234",
-      value: "0",
-      data: "0x1234",
-      nonce: "0",
-      gasLimit: "0",
-      chainId: "646",
-    }
-
-    const result = await accountManager.sendTransaction(tx)
-
-    expect(result).toEqual("1234")
-    expect(fcl.mutate).toHaveBeenCalled()
-    expect(mockFcl.mutate.mock.calls[0][0]).toMatchObject({
-      cadence: expect.any(String),
-      args: expect.any(Function),
-      authz: user,
-      limit: 9999,
-    })
-
-    expect(mockFcl.tx).toHaveBeenCalledWith("1111")
-    expect(mockFcl.tx).toHaveBeenCalledTimes(1)
-    expect(mockTxResult.onceExecuted).toHaveBeenCalledTimes(1)
-  })
-
-  test("throws error if no executed event not found", async () => {
-    const user = mockUser()
-    const accountManager = new AccountManager(user)
-
-    const mockTxResult = {
-      onceExecuted: jest.fn().mockResolvedValue({
-        events: [],
-      }),
-    } as any as jest.Mocked<ReturnType<typeof fcl.tx>>
-
-    jest.mocked(fcl.tx).mockReturnValue(mockTxResult)
-    jest.mocked(fcl.mutate).mockResolvedValue("1111")
-
-    const tx = {
-      to: "0x1234",
-      value: "0",
-      data: "0x1234",
-      nonce: "0",
-      gasLimit: "0",
-      chainId: "646",
-    }
-
-    await expect(accountManager.sendTransaction(tx)).rejects.toThrow(
-      "EVM transaction hash not found"
-    )
-  })
-})
+const mockFcl = jest.mocked(fcl)
+const mockQuery = jest.mocked(fcl.query)
 
 describe("AccountManager", () => {
   let accountManager: AccountManager
   let user: jest.Mocked<typeof fcl.currentUser>
-  let mockQuery: jest.MockedFn<typeof fcl.query>
 
   beforeEach(() => {
-    mockQuery = jest.mocked(fcl.query)
-
     user = mockUser()
     accountManager = new AccountManager(user)
   })
@@ -330,5 +205,128 @@ describe("AccountManager", () => {
 
     await accountManager.updateCOAAddress()
     expect(accountManager.getAccounts()).toEqual(["0x123"])
+  })
+})
+
+describe("send transaction", () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test("send transaction mainnet", async () => {
+    const user = mockUser()
+    const accountManager = new AccountManager(user)
+
+    const mockTxResult = {
+      onceExecuted: jest.fn().mockResolvedValue({
+        events: [
+          {
+            type: "A.e467b9dd11fa00df.EVM.TransactionExecuted",
+            data: {
+              hash: ["12", "34"],
+            },
+          },
+        ],
+      }),
+    } as any as jest.Mocked<ReturnType<typeof fcl.tx>>
+
+    jest.mocked(fcl.tx).mockReturnValue(mockTxResult)
+    jest.mocked(fcl.mutate).mockResolvedValue("1111")
+
+    const tx = {
+      to: "0x1234",
+      value: "0",
+      data: "0x1234",
+      nonce: "0",
+      gasLimit: "0",
+      chainId: "747",
+    }
+
+    const result = await accountManager.sendTransaction(tx)
+
+    expect(result).toEqual("1234")
+    expect(fcl.mutate).toHaveBeenCalled()
+    expect(mockFcl.mutate.mock.calls[0][0]).toMatchObject({
+      cadence: expect.any(String),
+      args: expect.any(Function),
+      authz: user,
+      limit: 9999,
+    })
+
+    expect(mockFcl.tx).toHaveBeenCalledWith("1111")
+    expect(mockFcl.tx).toHaveBeenCalledTimes(1)
+    expect(mockTxResult.onceExecuted).toHaveBeenCalledTimes(1)
+  })
+
+  test("send transaction testnet", async () => {
+    const user = mockUser()
+    const accountManager = new AccountManager(user)
+
+    const mockTxResult = {
+      onceExecuted: jest.fn().mockResolvedValue({
+        events: [
+          {
+            type: "A.8c5303eaa26202d6.EVM.TransactionExecuted",
+            data: {
+              hash: ["12", "34"],
+            },
+          },
+        ],
+      }),
+    } as any as jest.Mocked<ReturnType<typeof fcl.tx>>
+
+    jest.mocked(fcl.tx).mockReturnValue(mockTxResult)
+    jest.mocked(fcl.mutate).mockResolvedValue("1111")
+
+    const tx = {
+      to: "0x1234",
+      value: "0",
+      data: "0x1234",
+      nonce: "0",
+      gasLimit: "0",
+      chainId: "646",
+    }
+
+    const result = await accountManager.sendTransaction(tx)
+
+    expect(result).toEqual("1234")
+    expect(fcl.mutate).toHaveBeenCalled()
+    expect(mockFcl.mutate.mock.calls[0][0]).toMatchObject({
+      cadence: expect.any(String),
+      args: expect.any(Function),
+      authz: user,
+      limit: 9999,
+    })
+
+    expect(mockFcl.tx).toHaveBeenCalledWith("1111")
+    expect(mockFcl.tx).toHaveBeenCalledTimes(1)
+    expect(mockTxResult.onceExecuted).toHaveBeenCalledTimes(1)
+  })
+
+  test("throws error if no executed event not found", async () => {
+    const user = mockUser()
+    const accountManager = new AccountManager(user)
+
+    const mockTxResult = {
+      onceExecuted: jest.fn().mockResolvedValue({
+        events: [],
+      }),
+    } as any as jest.Mocked<ReturnType<typeof fcl.tx>>
+
+    jest.mocked(fcl.tx).mockReturnValue(mockTxResult)
+    jest.mocked(fcl.mutate).mockResolvedValue("1111")
+
+    const tx = {
+      to: "0x1234",
+      value: "0",
+      data: "0x1234",
+      nonce: "0",
+      gasLimit: "0",
+      chainId: "646",
+    }
+
+    await expect(accountManager.sendTransaction(tx)).rejects.toThrow(
+      "EVM transaction hash not found"
+    )
   })
 })
