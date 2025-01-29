5a0cc3b8f7bbed0d64ce3cd23f33458374fa6851
Jordan Ribbink <jribbink@telus.net>
2025-01-29 03:01:52 -0800

    fix test

--- packages/fcl-ethereum-provider/src/create-provider.ts
+++ packages/fcl-ethereum-provider/src/create-provider.ts
@@ -46,7 +46,7 @@ export function createProvider(config: {
     ...(config.rpcUrls || {}),
   })
   const rpcProcessor = new RpcProcessor(gateway, accountManager)
-  const eventProcessor = new EventDispatcher()
+  const eventProcessor = new EventDispatcher(accountManager)
   const provider = new FclEthereumProvider(rpcProcessor, eventProcessor)
   return provider
 }
