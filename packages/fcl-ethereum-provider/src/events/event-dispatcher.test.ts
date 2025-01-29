a126607604a6bc60e97eb8ab4a7d69d09baca2f1
Jordan Ribbink <jribbink@telus.net>
2025-01-29 15:31:59 -0800

    fix

--- packages/fcl-ethereum-provider/src/events/event-dispatcher.test.ts
+++ packages/fcl-ethereum-provider/src/events/event-dispatcher.test.ts
@@ -1,3 +1,90 @@
-test("notifications service", () => {
-  expect(true).toBe(true)
+import {AccountManager} from "../accounts/account-manager"
+import {EventDispatcher} from "./event-dispatcher"
+
+jest.mock("../accounts/account-manager")
+
+describe("event dispatcher", () => {
+  test("unsubscribe should remove listener", () => {
+    const accountManager: jest.Mocked<AccountManager> =
+      new (AccountManager as any)()
+
+    let mockSubscribeCallback: (accounts: string[]) => void
+    accountManager.subscribe.mockImplementation(cb => {
+      mockSubscribeCallback = cb
+    })
+    const listener = jest.fn()
+
+    const eventDispatcher = new EventDispatcher(accountManager)
+    eventDispatcher.on("accountsChanged", listener)
+
+    expect(accountManager.subscribe).toHaveBeenCalled()
+    expect(accountManager.subscribe).toHaveBeenCalledTimes(1)
+    expect(accountManager.subscribe).toHaveBeenCalledWith(expect.any(Function))
+
+    // Simulate account change from account manager
+    mockSubscribeCallback!(["0x1234"])
+
+    expect(listener).toHaveBeenCalled()
+    expect(listener).toHaveBeenCalledTimes(1)
+    expect(listener).toHaveBeenCalledWith(["0x1234"])
+
+    eventDispatcher.off("accountsChanged", listener)
+
+    // Simulate account change from account manager
+    mockSubscribeCallback!(["0x5678"])
+
+    expect(listener).toHaveBeenCalledTimes(1)
+  })
+
+  test("should emit accountsChanged", () => {
+    const accountManager: jest.Mocked<AccountManager> =
+      new (AccountManager as any)()
+
+    let mockSubscribeCallback: (accounts: string[]) => void
+    accountManager.subscribe.mockImplementation(cb => {
+      mockSubscribeCallback = cb
+    })
+    const listener = jest.fn()
+
+    const eventDispatcher = new EventDispatcher(accountManager)
+    eventDispatcher.on("accountsChanged", listener)
+
+    expect(accountManager.subscribe).toHaveBeenCalled()
+    expect(accountManager.subscribe).toHaveBeenCalledTimes(1)
+    expect(accountManager.subscribe).toHaveBeenCalledWith(expect.any(Function))
+
+    // Simulate account change from account manager
+    mockSubscribeCallback!(["0x1234"])
+
+    expect(listener).toHaveBeenCalled()
+    expect(listener).toHaveBeenCalledTimes(1)
+    expect(listener).toHaveBeenCalledWith(["0x1234"])
+  })
+
+  test("should emit accountsChanged multiple times", () => {
+    const accountManager: jest.Mocked<AccountManager> =
+      new (AccountManager as any)()
+
+    let mockSubscribeCallback: (accounts: string[]) => void
+    accountManager.subscribe.mockImplementation(cb => {
+      mockSubscribeCallback = cb
+    })
+    const listener = jest.fn()
+
+    const eventDispatcher = new EventDispatcher(accountManager)
+    eventDispatcher.on("accountsChanged", listener)
+
+    expect(accountManager.subscribe).toHaveBeenCalled()
+    expect(accountManager.subscribe).toHaveBeenCalledTimes(1)
+    expect(accountManager.subscribe).toHaveBeenCalledWith(expect.any(Function))
+
+    // Simulate account change from account manager
+    mockSubscribeCallback!(["0x1234"])
+    mockSubscribeCallback!(["0x5678"])
+
+    expect(listener).toHaveBeenCalled()
+    expect(listener).toHaveBeenCalledTimes(2)
+    expect(listener).toHaveBeenNthCalledWith(1, ["0x1234"])
+    expect(listener).toHaveBeenNthCalledWith(2, ["0x5678"])
+  })
 })
--- packages/fcl-ethereum-provider/src/events/event-dispatcher.ts
+++ packages/fcl-ethereum-provider/src/events/event-dispatcher.ts
@@ -2,11 +2,15 @@ import {EventCallback, ProviderEvents} from "../types/provider"
 import EventEmitter from "events"
 import * as fcl from "@onflow/fcl"
 import {CurrentUser} from "@onflow/typedefs"
+import {AccountManager} from "../accounts/account-manager"
 
 export class EventDispatcher {
   private eventEmitter = new EventEmitter()
 
-  constructor(config: typeof fcl.config) {
+  constructor(config: typeof fcl.config, accountManager: AccountManager) {
+    accountManager.subscribe(accounts => {
+      this.emit("accountsChanged", accounts)
+    })
     config.subscribe(async cfg => {
       const accessNode = cfg?.["accessNode.api"]
       const flowNetwork = await fcl.getChainId({
