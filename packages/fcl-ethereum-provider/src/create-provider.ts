e585a9d5961d4e6a43a57c8893f772ce0c70966c
Jordan Ribbink <jribbink@telus.net>
2025-01-29 16:14:58 -0800

    Fix create

--- packages/fcl-ethereum-provider/src/create-provider.ts
+++ packages/fcl-ethereum-provider/src/create-provider.ts
@@ -29,6 +29,7 @@ import {Gateway} from "./gateway/gateway"
  */
 export function createProvider(config: {
   user: typeof fcl.currentUser
+  config: typeof fcl.config
   service?: Service
   rpcUrls?: {[chainId: string]: number}
 }): Eip1193Provider {
@@ -46,7 +47,7 @@ export function createProvider(config: {
     ...(config.rpcUrls || {}),
   })
   const rpcProcessor = new RpcProcessor(gateway, accountManager)
-  const eventProcessor = new EventDispatcher()
+  const eventProcessor = new EventDispatcher(config.config, accountManager)
   const provider = new FclEthereumProvider(rpcProcessor, eventProcessor)
   return provider
 }
