d8c04ad396465eb4c3dc5b9ce1fbea8887e19451
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-02-12 14:55:17 -0800

    Cleanup

--- packages/fcl-ethereum-provider/src/util/chain.ts
+++ packages/fcl-ethereum-provider/src/util/chain.ts
@@ -1,9 +1,7 @@
 import {FLOW_CHAINS, FlowNetwork} from "../constants"
 
-export function getFlowNetwork(chainId: number): FlowNetwork {
-  const flowNetwork = Object.entries(FLOW_CHAINS).find(
+export function getFlowNetwork(chainId: number): FlowNetwork | undefined {
+  return Object.entries(FLOW_CHAINS).find(
     ([, chain]) => chain.eip155ChainId === chainId
   )?.[0] as FlowNetwork | undefined
-
-  return flowNetwork
 }
