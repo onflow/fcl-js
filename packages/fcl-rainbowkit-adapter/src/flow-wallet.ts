ae76e07f4de4107b057d9a46e8cdb7006e65dab6
Jordan Ribbink <jribbink@telus.net>
2025-02-19 14:52:36 -0800

    fix UID mismatch

--- packages/fcl-rainbowkit-adapter/src/flow-wallet.ts
+++ packages/fcl-rainbowkit-adapter/src/flow-wallet.ts
@@ -18,7 +18,7 @@ export const flowWallet = (params: {
       f_type: "Service",
       f_vsn: "1.0.0",
       type: "authn",
-      uid: "Flow Wallet",
+      uid: "fcw#authn",
       endpoint:
         "chrome-extension://hpclkefagolihohboafpheddmmgdffjm/popup.html",
       method: "EXT/RPC",
