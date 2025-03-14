2df3280df7fb68ea1fda0fb2123825d92f62749f
Chase Fleming <chasefleming@users.noreply.github.com>
2025-03-14 14:32:30 -0700

    Add `useBlock` hook (#2214)
    * Add useBlock hook
    
    * Change params
    
    * Add test
    
    * Add tests
    
    ---------
    
    Co-authored-by: Chase Fleming <1666730+chasefleming@users.noreply.github.com>

---
+++ packages/kit/src/hooks/useBlock.ts
@@ -0,0 +1,57 @@
+import * as fcl from "@onflow/fcl"
+import {Block} from "@onflow/typedefs"
+import {useQuery, UseQueryResult} from "@tanstack/react-query"
+import {useFlowQueryClient} from "../provider/FlowQueryClient"
+import {useCallback} from "react"
+
+interface BlockByLatest {
+  sealed?: never
+  id?: never
+  height?: never
+}
+
+interface BlockByLatestSealed {
+  sealed?: boolean
+  id?: never
+  height?: never
+}
+
+interface BlockById {
+  id: string
+  sealed?: never
+  height?: never
+}
+
+interface BlockByHeight {
+  height: number
+  sealed?: never
+  id?: never
+}
+
+type UseBlockParams =
+  | BlockByLatest
+  | BlockByLatestSealed
+  | BlockById
+  | BlockByHeight
+
+export function useBlock(
+  params: UseBlockParams = {}
+): UseQueryResult<Block | null, Error> {
+  const queryClient = useFlowQueryClient()
+
+  const fetchBlock = useCallback(async () => {
+    const block = await fcl.block(params)
+    return block as Block
+  }, [params])
+
+  return useQuery<Block | null, Error>(
+    {
+      queryKey: ["flowBlock", params],
+      queryFn: fetchBlock,
+      enabled: true,
+      initialData: null,
+      retry: false,
+    },
+    queryClient
+  )
+}
