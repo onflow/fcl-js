9c246d46a90b1fd4a96789636b9358afe0c0d844
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-03-19 13:40:28 -0700

    Switch to useFlowAccount

--- packages/kit/src/hooks/index.ts
+++ packages/kit/src/hooks/index.ts
@@ -1,4 +1,4 @@
-export {useAccount} from "./useAccount"
+export {useFlowAccount} from "./useFlowAccount"
 export {useFlowBlock} from "./useFlowBlock"
 export {useConfig} from "./useConfig"
 export {useFlowEvents} from "./useFlowEvents"
