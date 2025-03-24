65eb43bf5bf8191f702233f490a77b9dd04a71e8
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-03-24 14:28:39 -0700

    Add query client

--- packages/kit/src/hooks/useFlowMutate.ts
+++ packages/kit/src/hooks/useFlowMutate.ts
@@ -57,6 +57,8 @@ export function useFlowMutate({
   authorizations,
   limit = 9999,
 }: FlowMutateArgs): UseMutationResult<string, Error> {
+  const queryClient = useFlowQueryClient()
+
   const mutationFn = useCallback(async () => {
     if (!cadence) {
       throw new Error("Cadence transaction code is required.")
@@ -74,8 +76,11 @@ export function useFlowMutate({
     return txId
   }, [cadence, args, payer, proposer, authorizations, limit])
 
-  return useMutation({
-    mutationFn,
-    retry: false,
-  })
+  return useMutation(
+    {
+      mutationFn,
+      retry: false,
+    },
+    queryClient
+  )
 }
