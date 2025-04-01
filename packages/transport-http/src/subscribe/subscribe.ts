ca708a8ce3167585631bae1acaa57abe4cec01c2
Jordan Ribbink <jribbink@telus.net>
2025-04-01 12:27:19 -0700

    Fix endpoint for WS subscriptions

--- packages/transport-http/src/subscribe/subscribe.ts
+++ packages/transport-http/src/subscribe/subscribe.ts
@@ -11,6 +11,7 @@ import {blockDigestsHandler} from "./handlers/block-digests"
 import {accountStatusesHandler} from "./handlers/account-statuses"
 import {transactionStatusesHandler} from "./handlers/transaction-statuses"
 import {eventsHandler} from "./handlers/events"
+import {combineURLs} from "../utils/combine-urls"
 
 const SUBSCRIPTION_HANDLERS = [
   blocksHandler,
@@ -42,7 +43,7 @@ export function subscribe<T extends SubscriptionTopic>(
   opts: {node: string}
 ): Subscription {
   // Get the SubscriptionManager instance for the access node, or create a new one
-  const node = opts.node
+  const node = getWsUrl(opts.node)
   const manager =
     subscriptionManagerMap.get(node) ||
     new SubscriptionManager(SUBSCRIPTION_HANDLERS, {node})
@@ -54,4 +55,14 @@ export function subscribe<T extends SubscriptionTopic>(
     onData: onData as any,
     onError,
   })
+}
+
+function getWsUrl(node: string) {
+  const url = new URL(combineURLs(node, "/v1/ws"))
+  if (url.protocol === "https:") {
+    url.protocol = "wss:"
+  } else if (url.protocol === "http:") {
+    url.protocol = "ws:"
+  }
+  return url.toString()
 }
