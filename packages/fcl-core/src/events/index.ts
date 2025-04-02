db3abb366c6a4582e963d458e07510346e5e1015
Jordan Ribbink <jribbink@telus.net>
2025-04-01 18:54:15 -0700

    Add legacy `fcl.events` fallback for emulator

--- packages/fcl-core/src/events/index.ts
+++ packages/fcl-core/src/events/index.ts
@@ -2,6 +2,9 @@ import {subscribe} from "@onflow/sdk"
 import {Event, EventFilter, SubscriptionTopic} from "@onflow/typedefs"
 import {events as legacyEvents} from "./legacy-events"
 import {SubscriptionsNotSupportedError} from "@onflow/sdk"
+import {getChainId} from "../utils"
+
+const FLOW_EMULATOR = "emulator"
 
 /**
  * @description - Subscribe to events
@@ -24,28 +27,34 @@ export function events(filterOrType?: EventFilter | string) {
     subscribe: (
       callback: (events: Event | null, error: Error | null) => void
     ) => {
+      let unsubscribeFn = () => {}
       let unsubscribeFnLegacy = () => {}
-      const {unsubscribe: unsubscribeFn} = subscribe({
-        topic: SubscriptionTopic.EVENTS,
-        args: filter,
-        onData: data => {
-          callback(data, null)
-        },
-        onError: (error: Error) => {
-          // If subscriptions are not supported, fallback to legacy polling, otherwise return the error
-          if (error instanceof SubscriptionsNotSupportedError) {
-            fallbackLegacyPolling()
-          } else {
-            callback(null, error)
-          }
-        },
-      })
 
+      // Subscribe to the event stream
+      function subscribeEventStream() {
+        const {unsubscribe} = subscribe({
+          topic: SubscriptionTopic.EVENTS,
+          args: filter,
+          onData: data => {
+            callback(data, null)
+          },
+          onError: (error: Error) => {
+            // If subscriptions are not supported, fallback to legacy polling, otherwise return the error
+            if (error instanceof SubscriptionsNotSupportedError) {
+              console.warn(
+                "Failed to subscribe to events using real-time streaming (are you using the deprecated GRPC transport?), falling back to legacy polling."
+              )
+              fallbackLegacyPolling()
+            } else {
+              callback(null, error)
+            }
+          },
+        })
+        unsubscribeFn = unsubscribe
+      }
+
+      // Fallback to legacy polling if real-time streaming is not supported
       function fallbackLegacyPolling() {
-        console.warn(
-          "Failed to subscribe to events using real-time streaming (are you using the deprecated GRPC transport?), falling back to legacy polling."
-        )
-
         if (typeof filterOrType !== "string") {
           throw new Error(
             "Legacy fcl.events fallback only supports string filters (single event type)"
@@ -55,10 +64,32 @@ export function events(filterOrType?: EventFilter | string) {
         unsubscribeFnLegacy = unsubscribe
       }
 
+      async function subscribeToEvents() {
+        const network = await getChainId()
+
+        // As of Flow CLI v2.2.8, WebSocket subscriptions are not supported on the Flow emulator
+        // This conditional will be removed when WebSocket subscriptions are supported in this environment
+        if (network === FLOW_EMULATOR) {
+          console.warn(
+            "Events are not supported on the Flow emulator, falling back to legacy polling."
+          )
+          fallbackLegacyPolling()
+        } else {
+          subscribeEventStream()
+        }
+      }
+
+      // Subscribe to events
+      const initPromise = subscribeToEvents().catch(error => {
+        callback(null, error)
+      })
+
       // Return an unsubscribe function
       return () => {
-        unsubscribeFn()
-        unsubscribeFnLegacy()
+        initPromise.finally(() => {
+          unsubscribeFn()
+          unsubscribeFnLegacy()
+        })
       }
     },
   }
