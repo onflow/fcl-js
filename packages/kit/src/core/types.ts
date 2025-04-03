2702b614e241e012204f53c376301b4f74d5a969
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-04-03 13:49:37 -0700

    Add type export

---
+++ packages/kit/src/core/types.ts
@@ -0,0 +1,1 @@
+export type FlowNetwork = "local" | "testnet" | "mainnet"
--- packages/kit/src/index.ts
+++ packages/kit/src/index.ts
@@ -1,3 +1,4 @@
+export * from "./core/types"
 export * from "./hooks"
 export * from "./components"
 export * from "./provider"
