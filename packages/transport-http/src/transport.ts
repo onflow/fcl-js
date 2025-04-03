93a6b13fe49acad9042b3b6b69a2ddcf324a7f92
Jordan Ribbink <17958158+jribbink@users.noreply.github.com>
2025-04-03 13:06:45 -0600

    Reorganize subscriptions types (#2301)

--- packages/transport-http/src/transport.ts
+++ packages/transport-http/src/transport.ts
@@ -2,7 +2,7 @@ import {SdkTransport} from "@onflow/typedefs"
 import {send} from "./send/send-http"
 import {subscribe} from "./subscribe/subscribe"
 
-export const httpTransport: SdkTransport.Transport = {
+export const httpTransport: SdkTransport = {
   send,
   subscribe,
 }
