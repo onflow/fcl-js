4af452c24a4e4965de36890490c540e2e093b754
Chase Fleming <chasefleming@users.noreply.github.com>
2025-05-20 15:39:19 -0700

    Add `onConnect` and `onDisconnect` props to `Connect` (#2438)
    Co-authored-by: Chase Fleming <1666730+chasefleming@users.noreply.github.com>

--- packages/kit/src/components/Connect.tsx
+++ packages/kit/src/components/Connect.tsx
@@ -4,15 +4,31 @@ import {Button, ButtonProps} from "./internal/Button"
 
 interface ConnectProps {
   variant?: ButtonProps["variant"]
+  onConnect?: () => void
+  onDisconnect?: () => void
 }
 
-export const Connect: React.FC<ConnectProps> = ({variant = "primary"}) => {
+export const Connect: React.FC<ConnectProps> = ({
+  variant = "primary",
+  onConnect,
+  onDisconnect,
+}) => {
   const {user, authenticate, unauthenticate} = useCurrentFlowUser()
+
+  const handleConnect = async () => {
+    await authenticate()
+    onConnect?.()
+  }
+
+  const handleDisconnect = () => {
+    unauthenticate()
+    onDisconnect?.()
+  }
 
   return (
     <Button
       variant={variant}
-      onClick={user.loggedIn ? unauthenticate : authenticate}
+      onClick={user.loggedIn ? handleDisconnect : handleConnect}
     >
       {user.loggedIn ? "Disconnect Wallet" : "Connect Wallet"}
     </Button>
