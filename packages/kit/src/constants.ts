99510059485ffb2d741407a573f3be076c77e044
Jordan Ribbink <17958158+jribbink@users.noreply.github.com>
2025-05-23 19:11:59 +0000

    Add `useCrossVmTokenBalance` hook (#2398)

---
+++ packages/kit/src/constants.ts
@@ -0,0 +1,18 @@
+export const CONTRACT_ADDRESSES = {
+  testnet: {
+    EVM: "0x8c5303eaa26202d6",
+    FungibleToken: "0x9a0766d93b6608b7",
+    FlowEVMBridgeUtils: "0xdfc20aee650fcbdf",
+    FlowEVMBridgeConfig: "0xdfc20aee650fcbdf",
+    FungibleTokenMetadataViews: "0x9a0766d93b6608b7",
+  },
+  mainnet: {
+    EVM: "0xe467b9dd11fa00df",
+    FungibleToken: "0xf233dcee88fe0abe",
+    FlowEVMBridgeUtils: "0x1e4aa0b87d10b141",
+    FlowEVMBridgeConfig: "0x1e4aa0b87d10b141",
+    FungibleTokenMetadataViews: "0xf233dcee88fe0abe",
+  },
+}
+
+export const CADENCE_UFIX64_PRECISION = 8
