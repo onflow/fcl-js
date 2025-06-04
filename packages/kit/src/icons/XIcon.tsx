a1de5844062b2f7b7b35dd3c9319383cef62a638
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-06-04 09:29:01 -0700

    Add xicon

---
+++ packages/kit/src/icons/XIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+export const XIcon: React.FC<{className?: string}> = ({className}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+  >
+    <path d="M18 6 6 18"/>
+    <path d="m6 6 12 12"/>
+  </svg>
+) 
