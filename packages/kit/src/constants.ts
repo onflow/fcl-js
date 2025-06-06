ff07e0ea38845f188f0bbbcb9a365cad96cfb8b7
Jordan Ribbink <17958158+jribbink@users.noreply.github.com>
2025-06-06 08:41:14 -0700

    Export `useCrossVmTokenBalance` hook (#2491)

--- packages/kit/src/constants.ts
+++ packages/kit/src/constants.ts
@@ -2,6 +2,7 @@ export const CONTRACT_ADDRESSES = {
   testnet: {
     EVM: "0x8c5303eaa26202d6",
     FungibleToken: "0x9a0766d93b6608b7",
+    FlowToken: "0x7e60df042a9c0868",
     FlowEVMBridgeUtils: "0xdfc20aee650fcbdf",
     FlowEVMBridgeConfig: "0xdfc20aee650fcbdf",
     FungibleTokenMetadataViews: "0x9a0766d93b6608b7",
@@ -9,6 +10,7 @@ export const CONTRACT_ADDRESSES = {
   mainnet: {
     EVM: "0xe467b9dd11fa00df",
     FungibleToken: "0xf233dcee88fe0abe",
+    FlowToken: "0x1654653399040a61",
     FlowEVMBridgeUtils: "0x1e4aa0b87d10b141",
     FlowEVMBridgeConfig: "0x1e4aa0b87d10b141",
     FungibleTokenMetadataViews: "0xf233dcee88fe0abe",
