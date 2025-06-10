4bc15e4550fffa8ed1b7918fdf7ff87a3f34fbd0
Michael Fabozzi <39808567+mfbz@users.noreply.github.com>
2025-06-11 00:18:00 +0200

    Refactored fcl-core utils folder files to TypeScript (#2477)
    * Refactored utils folder
    
    * Update packages/fcl-core/src/utils/chain-id/chain-id-watcher.ts
    
    Co-authored-by: Jordan Ribbink <17958158+jribbink@users.noreply.github.com>
    
    * Update packages/fcl-core/src/utils/chain-id/chain-id-watcher.ts
    
    Co-authored-by: Jordan Ribbink <17958158+jribbink@users.noreply.github.com>
    
    * Fixed minor typo
    
    ---------
    
    Co-authored-by: mfbz <mfbz@users.noreply.github.com>
    Co-authored-by: Jordan Ribbink <17958158+jribbink@users.noreply.github.com>

--- packages/fcl-core/src/utils/constants.ts
+++ packages/fcl-core/src/utils/constants.ts
@@ -1,7 +1,7 @@
-export const FCL_REDIRECT_URL_PARAM_NAME = "fcl_redirect_url"
-export const FCL_RESPONSE_PARAM_NAME = "fclResponseJson"
+export const FCL_REDIRECT_URL_PARAM_NAME: string = "fcl_redirect_url"
+export const FCL_RESPONSE_PARAM_NAME: string = "fclResponseJson"
 
-export const CORE_STRATEGIES = {
+export const CORE_STRATEGIES: Record<string, string> = {
   "HTTP/RPC": "HTTP/RPC",
   "HTTP/POST": "HTTP/POST",
   "IFRAME/RPC": "IFRAME/RPC",
--- packages/fcl-core/src/utils/is.js
+++
@@ -1,7 +0,0 @@
-const is = type => d => typeof d === type
-
-export const isRequired = d => d != null
-export const isObject = is("object")
-export const isString = is("string")
-export const isFunc = is("function")
-export const isNumber = is("number")
