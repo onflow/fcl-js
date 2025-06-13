9731c62e78a3744e0a80b48533983eda1d7c9e89
Michael Fabozzi <39808567+mfbz@users.noreply.github.com>
2025-06-13 15:42:26 +0200

    Update packages/fcl-core/src/interaction-template-utils/generate-template-id/generate-template-id-1.1.0.ts
    Co-authored-by: Jordan Ribbink <17958158+jribbink@users.noreply.github.com>

--- packages/fcl-core/src/interaction-template-utils/generate-template-id/generate-template-id-1.1.0.ts
+++ packages/fcl-core/src/interaction-template-utils/generate-template-id/generate-template-id-1.1.0.ts
@@ -67,7 +67,7 @@ export async function generateTemplateId({
   template: InteractionTemplate110
 }): Promise<string> {
   invariant(
-    template as any,
+    !!template,
     "generateTemplateId({ template }) -- template must be defined"
   )
   invariant(
