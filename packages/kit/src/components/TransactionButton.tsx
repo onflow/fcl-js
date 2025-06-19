655867f1e34a067813dced7e813499450db7be42
Jordan Ribbink <jribbink@telus.net>
2025-06-19 15:38:24 -0700

    remove custom variant prop

--- packages/kit/src/components/TransactionButton.tsx
+++ packages/kit/src/components/TransactionButton.tsx
@@ -5,18 +5,14 @@ import {UseMutationOptions} from "@tanstack/react-query"
 import {mutate} from "@onflow/fcl"
 import {useGlobalTransaction} from "../provider/GlobalTransactionProvider"
 
-interface TransactionButtonProps {
+interface TransactionButtonProps
+  extends Omit<ButtonProps, "onClick" | "children"> {
   /**
    * Transaction to execute when the button is clicked.
    * This should be a valid Flow transaction object that can be passed to `fcl.mutate`.
    * It can include Cadence code, arguments, and other transaction parameters.
    */
   transaction: Parameters<typeof mutate>[0]
-  /**
-   * Variant of the button, defaults to "primary".
-   * Can be set to "outline" or other variants supported by the Button component.
-   */
-  variant?: ButtonProps["variant"]
   /**
    * Optional label for the button.
    * If not provided, the button will display a default label based on the transaction type.
@@ -28,14 +24,6 @@ interface TransactionButtonProps {
    * error handling, and more.
    */
   mutation?: UseMutationOptions<string, Error, Parameters<typeof mutate>[0]>
-  /**
-   * Whether to disable the button. This will be combined with global transaction state.
-   */
-  disabled?: boolean
-  /**
-   * Additional CSS classes to apply to the button.
-   */
-  className?: string
 }
 
 export const TransactionButton: React.FC<TransactionButtonProps> = ({
@@ -43,8 +31,7 @@ export const TransactionButton: React.FC<TransactionButtonProps> = ({
   label,
   transaction,
   mutation,
-  disabled = false,
-  className,
+  ...buttonProps
 }) => {
   // Get global transaction state
   const {setGlobalTxId, globalTxId} = useGlobalTransaction()
@@ -71,15 +58,10 @@ export const TransactionButton: React.FC<TransactionButtonProps> = ({
   const isLoading = isPending
 
   // Button is disabled if: explicitly disabled, currently loading, or any global transaction is running
-  const isDisabled = disabled || isLoading || !!globalTxId
+  const isDisabled = buttonProps.disabled || isLoading || !!globalTxId
 
   return (
-    <Button
-      onClick={handleButtonClick}
-      variant={variant}
-      className={className}
-      disabled={isDisabled}
-    >
+    <Button onClick={handleButtonClick} disabled={isDisabled} {...buttonProps}>
       <span>
         {!isLoading ? label || "Execute Transaction" : "Processing..."}
       </span>
