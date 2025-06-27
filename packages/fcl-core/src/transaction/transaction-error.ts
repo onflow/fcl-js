cf7d08c0ead30ccd275fd3490f5aa4ef883862b9
mfbz <mfbz@users.noreply.github.com>
2025-06-28 00:44:11 +0200

    Implemented improvement with transaction error reverse mapping

--- packages/fcl-core/src/transaction/transaction-error.ts
+++ packages/fcl-core/src/transaction/transaction-error.ts
@@ -2,6 +2,17 @@ import {FvmErrorCode} from "@onflow/typedefs"
 
 const ERROR_CODE_REGEX = /\[Error Code: (\d+)\]/
 
+// Reverse mapping from error codes to error type names for better lookup
+const ERROR_CODE_TO_TYPE: Record<number, string> = Object.entries(
+  FvmErrorCode
+).reduce(
+  (acc, [key, value]) => {
+    acc[value as number] = key
+    return acc
+  },
+  {} as Record<number, string>
+)
+
 export class TransactionError extends Error {
   public code: FvmErrorCode
   public type: string
@@ -9,10 +20,7 @@ export class TransactionError extends Error {
   private constructor(message: string, code: FvmErrorCode) {
     super(message)
     this.code = code
-    this.type =
-      Object.keys(FvmErrorCode).find(
-        key => FvmErrorCode[key as keyof typeof FvmErrorCode] === code
-      ) || "UNKNOWN_ERROR"
+    this.type = ERROR_CODE_TO_TYPE[code] || "UNKNOWN_ERROR"
   }
 
   static fromErrorMessage(errorMessage: string): TransactionError {
