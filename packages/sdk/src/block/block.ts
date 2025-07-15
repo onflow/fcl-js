f16d1169f4842fd530a27904d21da949b667e3a2
Jordan Ribbink <jribbink@telus.net>
2025-07-15 08:45:34 -0700

    Fix comments

--- packages/sdk/src/block/block.ts
+++ packages/sdk/src/block/block.ts
@@ -15,33 +15,6 @@ export interface BlockQueryOptions {
 }
 
 export function createBlock(context: SdkContext) {
-  /**
-   * Query the network for block by id, height or get the latest block.
-   *
-   * Block ID is SHA3-256 hash of the entire block payload. This hash is stored as an ID field on any block response object (ie. response from `GetLatestBlock`).
-   *
-   * Block height expresses the height of the block on the chain. The latest block height increases by one for every valid block produced.
-   *
-   * @param queryOptions Query parameters
-   * @param queryOptions.sealed Whether to query for a sealed block
-   * @param queryOptions.height Block height to query
-   * @param queryOptions.id Block ID to query
-   * @param opts Optional parameters
-   * @returns A promise that resolves to a Block object
-   *
-   * @example
-   * import * as fcl from "@onflow/fcl";
-   *
-   * // Get latest block
-   * const latestBlock = await fcl.block(); // Get the latest finalized block
-   * const latestSealedBlock = await fcl.block({sealed: true}); // Get the latest sealed block
-   *
-   * // Get block by ID (uses builder function)
-   * await fcl.send([fcl.getBlock(), fcl.atBlockId("23232323232")]).then(fcl.decode);
-   *
-   * // Get block at height (uses builder function)
-   * await fcl.send([fcl.getBlock(), fcl.atBlockHeight(123)]).then(fcl.decode)
-   */
   async function block(
     {sealed = false, id, height}: BlockQueryOptions = {},
     opts: object = {}
@@ -76,4 +49,31 @@ export function createBlock(context: SdkContext) {
   return block
 }
 
+/**
+ * Query the network for block by id, height or get the latest block.
+ *
+ * Block ID is SHA3-256 hash of the entire block payload. This hash is stored as an ID field on any block response object (ie. response from `GetLatestBlock`).
+ *
+ * Block height expresses the height of the block on the chain. The latest block height increases by one for every valid block produced.
+ *
+ * @param queryOptions Query parameters
+ * @param queryOptions.sealed Whether to query for a sealed block
+ * @param queryOptions.height Block height to query
+ * @param queryOptions.id Block ID to query
+ * @param opts Optional parameters
+ * @returns A promise that resolves to a Block object
+ *
+ * @example
+ * import * as fcl from "@onflow/fcl";
+ *
+ * // Get latest block
+ * const latestBlock = await fcl.block(); // Get the latest finalized block
+ * const latestSealedBlock = await fcl.block({sealed: true}); // Get the latest sealed block
+ *
+ * // Get block by ID (uses builder function)
+ * await fcl.send([fcl.getBlock(), fcl.atBlockId("23232323232")]).then(fcl.decode);
+ *
+ * // Get block at height (uses builder function)
+ * await fcl.send([fcl.getBlock(), fcl.atBlockHeight(123)]).then(fcl.decode)
+ */
 export const block = /* @__PURE__ */ withGlobalContext(createBlock)
