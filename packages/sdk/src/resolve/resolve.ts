ef30c35d9e930608e9acae9aeabb852a1120162e
Jordan Ribbink <jribbink@telus.net>
2025-07-15 16:11:09 -0700

    unify resolver syntax

--- packages/sdk/src/resolve/resolve.ts
+++ packages/sdk/src/resolve/resolve.ts
@@ -10,14 +10,14 @@ import {getAccount} from "../build/build-get-account"
 import {decodeResponse as decode} from "../decode/decode"
 import {Interaction} from "@onflow/typedefs"
 
-import {resolveCadence} from "./resolve-cadence"
+import {createResolveCadence} from "./resolve-cadence"
 import {resolveArguments} from "./resolve-arguments"
 import {resolveAccounts} from "./resolve-accounts"
 import {resolveSignatures} from "./resolve-signatures"
 import {resolveValidators} from "./resolve-validators"
 import {resolveFinalNormalization} from "./resolve-final-normalization"
 import {resolveVoucherIntercept} from "./resolve-voucher-intercept"
-import {resolveComputeLimit} from "./resolve-compute-limit"
+import {createResolveComputeLimit} from "./resolve-compute-limit"
 import {SdkContext} from "../context/context"
 import {withGlobalContext} from "../context/global"
 
@@ -52,49 +52,34 @@ const debug =
     return ix
   }
 
-/**
- * Binds a given context to a two-argument handler,
- * returning a unary function that only needs an Interaction.
- */
-export function applyContext(
-  handler: (ix: Interaction, context: SdkContext) => Promise<Interaction>,
-  context: SdkContext
-): (ix: Interaction) => Promise<Interaction> {
-  return ix => handler(ix, context)
-}
-
 export function createResolve(context: SdkContext) {
   const resolve = async (ix: Interaction) =>
-    pipe(
-      [
-        resolveCadence,
-        debug("cadence", (ix: Interaction, log: any) =>
-          log(ix.message.cadence)
-        ),
-        resolveComputeLimit,
-        debug("compute limit", (ix: Interaction, log: any) =>
-          log(ix.message.computeLimit)
-        ),
-        resolveArguments,
-        debug("arguments", (ix: Interaction, log: any) =>
-          log(ix.message.arguments, ix.message)
-        ),
-        resolveAccounts,
-        debug("accounts", (ix: Interaction, log: any, accts: any) =>
-          log(...accts(ix))
-        ),
-        /* special */ execFetchRef,
-        /* special */ execFetchSequenceNumber,
-        resolveSignatures,
-        debug("signatures", (ix: Interaction, log: any, accts: any) =>
-          log(...accts(ix))
-        ),
-        resolveFinalNormalization,
-        resolveValidators,
-        resolveVoucherIntercept,
-        debug("resolved", (ix: Interaction, log: any) => log(ix)),
-      ].map(handler => applyContext(handler, context))
-    )(ix)
+    pipe([
+      createResolveCadence(context),
+      debug("cadence", (ix: Interaction, log: any) => log(ix.message.cadence)),
+      createResolveComputeLimit(context),
+      debug("compute limit", (ix: Interaction, log: any) =>
+        log(ix.message.computeLimit)
+      ),
+      resolveArguments,
+      debug("arguments", (ix: Interaction, log: any) =>
+        log(ix.message.arguments, ix.message)
+      ),
+      resolveAccounts,
+      debug("accounts", (ix: Interaction, log: any, accts: any) =>
+        log(...accts(ix))
+      ),
+      /* special */ createExecFetchRef(context),
+      /* special */ createExecFetchSequenceNumber(context),
+      resolveSignatures,
+      debug("signatures", (ix: Interaction, log: any, accts: any) =>
+        log(...accts(ix))
+      ),
+      resolveFinalNormalization,
+      resolveValidators,
+      resolveVoucherIntercept,
+      debug("resolved", (ix: Interaction, log: any) => log(ix)),
+    ])(ix)
 
   return resolve
 }
@@ -126,52 +111,54 @@ export function createResolve(context: SdkContext) {
  */
 export const resolve = /* @__PURE__ */ withGlobalContext(createResolve)
 
-async function execFetchRef(
-  ix: Interaction,
-  context: SdkContext
-): Promise<Interaction> {
-  if (isTransaction(ix) && ix.message.refBlock == null) {
-    const sendFn = context.transport.send
-
-    ix.message.refBlock = (
-      await sendFn(
-        build([getBlock()]),
-        {response, Buffer, ix: ixModule} as any,
-        {
-          get node() {
-            return context.accessNodeUrl
-          },
-        }
-      ).then(decode)
-    ).id
-  }
-  return ix
-}
-
-async function execFetchSequenceNumber(
-  ix: Interaction,
-  context: SdkContext
-): Promise<Interaction> {
-  if (isTransaction(ix)) {
-    var acct = Object.values(ix.accounts).find((a: any) => a.role.proposer)
-    invariant(acct !== undefined, `Transactions require a proposer`)
-    if (acct && acct.sequenceNum == null) {
+function createExecFetchRef(context: SdkContext) {
+  return async function execFetchRef(ix: Interaction): Promise<Interaction> {
+    if (isTransaction(ix) && ix.message.refBlock == null) {
       const sendFn = context.transport.send
 
-      ix.accounts[acct.tempId].sequenceNum = await sendFn(
-        await build([getAccount(acct.addr!)]),
-        {response, Buffer, ix: ixModule} as any,
-        {
-          get node() {
-            return context.accessNodeUrl
-          },
-        }
-      )
-        .then(decode)
-        .then((acctResponse: any) => acctResponse.keys)
-        .then((keys: any) => keys.find((key: any) => key.index === acct!.keyId))
-        .then((key: any) => key.sequenceNumber)
+      ix.message.refBlock = (
+        await sendFn(
+          build([getBlock()]),
+          {response, Buffer, ix: ixModule} as any,
+          {
+            get node() {
+              return context.accessNodeUrl
+            },
+          }
+        ).then(decode)
+      ).id
     }
+    return ix
   }
-  return ix
+}
+
+function createExecFetchSequenceNumber(context: SdkContext) {
+  return async function execFetchSequenceNumber(
+    ix: Interaction
+  ): Promise<Interaction> {
+    if (isTransaction(ix)) {
+      var acct = Object.values(ix.accounts).find((a: any) => a.role.proposer)
+      invariant(acct !== undefined, `Transactions require a proposer`)
+      if (acct && acct.sequenceNum == null) {
+        const sendFn = context.transport.send
+
+        ix.accounts[acct.tempId].sequenceNum = await sendFn(
+          await build([getAccount(acct.addr!)]),
+          {response, Buffer, ix: ixModule} as any,
+          {
+            get node() {
+              return context.accessNodeUrl
+            },
+          }
+        )
+          .then(decode)
+          .then((acctResponse: any) => acctResponse.keys)
+          .then((keys: any) =>
+            keys.find((key: any) => key.index === acct!.keyId)
+          )
+          .then((key: any) => key.sequenceNumber)
+      }
+    }
+    return ix
+  }
 }
