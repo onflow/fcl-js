226a8a6462566af0a66b321e3546f6bf7ad17596
Jordan Ribbink <jribbink@telus.net>
2025-07-17 11:42:57 -0700

    fix demo

--- packages/demo/src/components/flow-provider-wrapper.tsx
+++ packages/demo/src/components/flow-provider-wrapper.tsx
@@ -7,9 +7,23 @@ import flowJSON from "../../flow.json"
 const flowNetwork =
   (import.meta.env.VITE_FLOW_NETWORK as FlowNetwork) || "emulator"
 const flowConfig = {
-  emulator: flowEmulator,
-  testnet: flowTestnet,
-  mainnet: flowMainnet,
+  emulator: {
+    accessNodeUrl: "http://localhost:8888",
+    discoveryAuthnEndpoint: "http://localhost:8701/fcl/authn",
+    flowNetwork: "local" as any,
+  },
+  testnet: {
+    accessNodeUrl: "https://rest-testnet.onflow.org",
+    discoveryAuthnEndpoint:
+      "https://fcl-discovery.onflow.org/api/testnet/authn",
+    flowNetwork: "testnet" as any,
+  },
+  mainnet: {
+    accessNodeUrl: "https://rest-mainnet.onflow.org",
+    discoveryAuthnEndpoint:
+      "https://fcl-discovery.onflow.org/api/mainnet/authn",
+    flowNetwork: "mainnet" as any,
+  },
 }
 
 export default function FlowProviderWrapper({
@@ -17,17 +31,6 @@ export default function FlowProviderWrapper({
 }: {
   children: React.ReactNode
 }) {
-  // TODO: Remove once fixed
-  useEffect(() => {
-    fcl.config({
-      ...flowConfig[flowNetwork],
-      appDetailTitle: "Demo App",
-      appDetailIcon: "https://avatars.githubusercontent.com/u/62387156?v=4",
-      appDetailUrl: "https://yourapp.com",
-      appDetailDescription: "Your app description",
-    })
-  }, [flowNetwork])
-
   return (
     <FlowProvider
       config={{
