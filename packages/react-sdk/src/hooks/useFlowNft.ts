2eefc5e74989dcb5419a305679c130068762bd6a
Chase Fleming <1666730+chasefleming@users.noreply.github.com>
2025-08-22 11:42:01 -0700

    Add nft metadata hook

---
+++ packages/react-sdk/src/hooks/useFlowNft.ts
@@ -0,0 +1,268 @@
+import {UseQueryOptions, UseQueryResult} from "@tanstack/react-query"
+import {useFlowQuery} from "./useFlowQuery"
+import {CONTRACT_ADDRESSES} from "../constants"
+import {useFlowChainId} from "./useFlowChainId"
+import {useFlowClient} from "./useFlowClient"
+
+interface ViewInfo {
+  name: string
+  description: string
+  thumbnail: {
+    url?: string
+    cid?: string
+    path?: string
+  }
+  rarity?: string
+  transferrable: boolean
+  collectionDisplay?: {
+    name: string
+    description: string
+    externalURL?: {
+      url: string
+    }
+    squareImage?: {
+      file: {
+        url?: string
+        cid?: string
+      }
+    }
+    bannerImage?: {
+      file: {
+        url?: string
+        cid?: string
+      }
+    }
+    socials?: Record<string, {url: string}>
+  }
+  externalURL?: string
+  traits?: Record<string, string>
+  royalties?: Record<string, string>
+  serialNumber?: string
+}
+
+export interface NftViewResult {
+  name: string
+  description: string
+  thumbnailUrl: string
+  externalUrl?: string
+  collectionName?: string
+  collectionExternalUrl?: string
+  tokenID: string
+  traits?: Record<string, string>
+  rarity?: string
+  serialNumber?: string
+}
+
+interface UseFlowNftArgs {
+  accountAddress?: string
+  tokenId?: string | number
+  publicPathIdentifier: string
+  query?: Omit<UseQueryOptions<unknown, Error>, "queryKey" | "queryFn">
+  flowClient?: ReturnType<typeof useFlowClient>
+}
+
+const getNftCadence = (network: "testnet" | "mainnet") => `
+  import NonFungibleToken from ${CONTRACT_ADDRESSES[network].NonFungibleToken}
+  import MetadataViews from ${CONTRACT_ADDRESSES[network].MetadataViews}
+  import ViewResolver from ${CONTRACT_ADDRESSES[network].ViewResolver}
+
+  access(all) struct ViewInfo {
+    access(all) let name: String
+    access(all) let description: String
+    access(all) let thumbnail: {MetadataViews.File}
+    access(all) let rarity: String?
+    access(all) let transferrable: Bool
+    access(all) let collectionDisplay: MetadataViews.NFTCollectionDisplay?
+    access(all) let externalURL: String?
+    access(all) let traits: {String: String}?
+    access(all) let royalties: {String: UFix64}?
+    access(all) let serialNumber: UInt64?
+
+    init(name: String, description: String, thumbnail: {MetadataViews.File}, rarity: String?, transferrable: Bool, collectionDisplay: MetadataViews.NFTCollectionDisplay?, externalURL: String?, traits: {String: String}?, royalties: {String: UFix64}?, serialNumber: UInt64?) {
+      self.name = name
+      self.description = description
+      self.thumbnail = thumbnail
+      self.rarity = rarity
+      self.transferrable = transferrable
+      self.collectionDisplay = collectionDisplay
+      self.externalURL = externalURL
+      self.traits = traits
+      self.royalties = royalties
+      self.serialNumber = serialNumber
+    }
+  }
+
+  access(all) fun main(address: Address, publicPathID: String, tokenID: UInt64): ViewInfo? {
+    let account = getAccount(address)
+
+    // First try to get the capability as a standard ViewResolver collection
+    let capability = account.capabilities.get<&{ViewResolver.ResolverCollection}>(PublicPath(identifier: publicPathID)!)
+    if capability != nil {
+      let collection = capability!.borrow()
+      if collection != nil {
+        let resolver = collection!.borrowViewResolver(id: tokenID)
+        if resolver != nil {
+          // Get display information
+          let display = MetadataViews.getDisplay(resolver!)
+          if display != nil {
+            // Get rarity information
+            var rarityDesc: String? = nil
+            let rarityView = MetadataViews.getRarity(resolver!)
+            if rarityView != nil {
+              rarityDesc = rarityView!.description
+            }
+
+            // Get collection display information
+            var collectionDisplay: MetadataViews.NFTCollectionDisplay? = nil
+            let cDisplay = MetadataViews.getNFTCollectionDisplay(resolver!)
+            if cDisplay != nil {
+              collectionDisplay = cDisplay
+            }
+
+            // Get external URL
+            var externalURL: String? = nil
+            let externalView = MetadataViews.getExternalURL(resolver!)
+            if externalView != nil {
+              externalURL = externalView!.url
+            }
+
+            // Get traits
+            var traits: {String: String}? = nil
+            let traitsView = MetadataViews.getTraits(resolver!)
+            if traitsView != nil {
+              var traitsDict: {String: String} = {}
+              for trait in traitsView!.traits {
+                // Convert trait value to string
+                var valueStr: String = ""
+                if let s = trait.value as? String { valueStr = s }
+                else if let i = trait.value as? Int { valueStr = i.toString() }
+                else if let f = trait.value as? UFix64 { valueStr = f.toString() }
+                else { valueStr = "Unknown" }
+                traitsDict[trait.name] = valueStr
+              }
+              traits = traitsDict
+            }
+
+            // Get royalties - simplified for now
+            var royalties: {String: UFix64}? = nil
+
+            // Get serial number
+            var serialNumber: UInt64? = nil
+            let serialView = MetadataViews.getSerial(resolver!)
+            if serialView != nil {
+              serialNumber = serialView!.number
+            }
+
+            return ViewInfo(
+              name: display!.name,
+              description: display!.description,
+              thumbnail: display!.thumbnail,
+              rarity: rarityDesc,
+              transferrable: true,
+              collectionDisplay: collectionDisplay,
+              externalURL: externalURL,
+              traits: traits,
+              royalties: royalties,
+              serialNumber: serialNumber
+            )
+          }
+        }
+      }
+    }
+
+    // If ViewResolver approach fails, try as a basic NFT collection
+    let basicCapability = account.capabilities.get<&{NonFungibleToken.CollectionPublic}>(PublicPath(identifier: publicPathID)!)
+    if basicCapability != nil {
+      let collection = basicCapability!.borrow()
+      if collection != nil {
+        // Check if the NFT exists
+        if collection!.getIDs().contains(tokenID) {
+          // Return basic NFT info
+          return ViewInfo(
+            name: "NFT #".concat(tokenID.toString()),
+            description: "An NFT from ".concat(publicPathID),
+            thumbnail: MetadataViews.HTTPFile(url: ""),
+            rarity: nil,
+            transferrable: true,
+            collectionDisplay: nil,
+            externalURL: nil,
+            traits: nil,
+            royalties: nil,
+            serialNumber: tokenID
+          )
+        }
+      }
+    }
+
+    return nil
+  }
+`
+
+/**
+ * Query a single NFT's basic metadata views from an account's ResolverCollection capability.
+ *
+ * Requirements: the account must expose a `MetadataViews.ResolverCollection` at the provided public path.
+ */
+export function useFlowNft(params: UseFlowNftArgs) {
+  const chainIdResult = useFlowChainId()
+
+  const queryResult = useFlowQuery({
+    cadence: chainIdResult.data
+      ? getNftCadence(chainIdResult.data as "testnet" | "mainnet")
+      : "",
+    args: (arg, t) => [
+      arg(params.accountAddress!, t.Address),
+      arg(params.publicPathIdentifier, t.String),
+      arg(String(params.tokenId!), t.UInt64),
+    ],
+    flowClient: params.flowClient,
+    query: {
+      ...params.query,
+      enabled:
+        (params.query?.enabled ?? true) &&
+        !!chainIdResult.data &&
+        !!params.accountAddress &&
+        params.publicPathIdentifier.length > 0 &&
+        (params.tokenId !== undefined && params.tokenId !== null && String(params.tokenId).length > 0),
+      staleTime: params.query?.staleTime ?? 60_000,
+    },
+  })
+
+  if (chainIdResult.isError) {
+    return chainIdResult
+  }
+
+  const getThumbnailUrl = (thumbnail: any): string => {
+    if (!thumbnail) return ""
+    if (thumbnail.url) return thumbnail.url
+    if (thumbnail.cid) return `https://ipfs.io/ipfs/${thumbnail.cid}${thumbnail.path || ""}`
+    return ""
+  }
+
+  const processedData: NftViewResult | null = queryResult.data
+    ? {
+        name: (queryResult.data as any).name || "Unknown NFT",
+        description: (queryResult.data as any).description || "",
+        thumbnailUrl: getThumbnailUrl((queryResult.data as any).thumbnail),
+        externalUrl:
+          (queryResult.data as any).externalURL ||
+          (queryResult.data as any).collectionDisplay?.externalURL?.url,
+        collectionName: (queryResult.data as any).collectionDisplay?.name,
+        collectionExternalUrl:
+          (queryResult.data as any).collectionDisplay?.externalURL?.url,
+        tokenID: String(params.tokenId ?? ""),
+        traits: (queryResult.data as any).traits || {},
+        rarity: (queryResult.data as any).rarity,
+        serialNumber: (queryResult.data as any).serialNumber
+          ? String((queryResult.data as any).serialNumber)
+          : undefined,
+      }
+    : null
+
+  return {
+    ...queryResult,
+    data: processedData,
+  } as UseQueryResult<NftViewResult | null, Error>
+}
+
+
