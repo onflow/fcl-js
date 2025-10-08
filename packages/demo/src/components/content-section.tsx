e651d625af8e516d935e74d885524eca741dd9e6
Michael Fabozzi <39808567+mfbz@users.noreply.github.com>
2025-10-08 21:16:06 +0200

    Add useFlowNftMetadata hook (#2661)
    * Add nft metadata hook
    
    * Remove comments
    
    * Make optional
    
    * Remove enabled
    
    * Remove ViewInfo
    
    * Remove
    
    * Add emulator support
    
    * Add emulator support
    
    * Clean up processed data
    
    * Clean up
    
    * Add test
    
    * Add nft card
    
    * Add fix
    
    * Fix card colors
    
    * Fix color
    
    * Rename
    
    * Run prettier
    
    * Add missing changeset for #2625
    
    Co-authored-by: Chase Fleming <1666730+chasefleming@users.noreply.github.com>
    
    * Updated card with new playground design
    
    * Improved changeset description
    
    ---------
    
    Co-authored-by: Chase Fleming <1666730+chasefleming@users.noreply.github.com>
    Co-authored-by: Chase Fleming <chasefleming@users.noreply.github.com>
    Co-authored-by: mfbz <mfbz@users.noreply.github.com>

--- packages/demo/src/components/content-section.tsx
+++ packages/demo/src/components/content-section.tsx
@@ -12,6 +12,7 @@ import {UseFlowMutateCard} from "./hook-cards/use-flow-mutate-card"
 import {UseFlowEventsCard} from "./hook-cards/use-flow-events-card"
 import {UseFlowTransactionStatusCard} from "./hook-cards/use-flow-transaction-status-card"
 import {UseFlowRevertibleRandomCard} from "./hook-cards/use-flow-revertible-random-card"
+import {UseFlowNftMetadataCard} from "./hook-cards/use-flow-nft-metadata-card"
 
 // Import setup cards
 import {InstallationCard} from "./setup-cards/installation-card"
@@ -85,6 +86,7 @@ export function ContentSection() {
             <UseFlowEventsCard />
             <UseFlowRevertibleRandomCard />
             <UseFlowTransactionStatusCard />
+            <UseFlowNftMetadataCard />
           </div>
 
           <div className="mb-12 md:mb-24">
