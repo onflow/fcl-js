c8b0b880d147840c66b8913894a8fe1e9804d557
Jordan Ribbink <17958158+jribbink@users.noreply.github.com>
2025-11-04 08:41:00 -0800

    Fix `flow.json` import resolution across multiple different deployment accounts (#2683)

--- packages/config/src/utils/utils.test.ts
+++ packages/config/src/utils/utils.test.ts
@@ -77,6 +77,99 @@ describe("getContracts", () => {
     expect(getContracts([flowJSON], "testnet")).toEqual(testnetMappings)
     expect(getContracts([flowJSON], "mainnet")).toEqual(mainnetMappings)
   })
+
+  test("it should handle multiple contracts in deployments section", () => {
+    const flowJSON: FlowJson = {
+      accounts: {
+        "emulator-account": {
+          address: "f8d6e0586b0a20c7",
+        },
+        "testnet-account": {
+          address: "a470920a30c770a5",
+        },
+      },
+      deployments: {
+        emulator: {
+          "emulator-account": ["ContractA", "ContractB", "ContractC"],
+        },
+        testnet: {
+          "testnet-account": ["ContractA", "ContractB"],
+        },
+      },
+    }
+
+    const emulatorMappings = {
+      ContractA: "f8d6e0586b0a20c7",
+      ContractB: "f8d6e0586b0a20c7",
+      ContractC: "f8d6e0586b0a20c7",
+    }
+
+    const testnetMappings = {
+      ContractA: "a470920a30c770a5",
+      ContractB: "a470920a30c770a5",
+    }
+
+    expect(getContracts(flowJSON, "emulator")).toEqual(emulatorMappings)
+    expect(getContracts(flowJSON, "testnet")).toEqual(testnetMappings)
+  })
+
+  test("it should merge deployments with contract aliases", () => {
+    const flowJSON: FlowJson = {
+      contracts: {
+        HelloWorld: {
+          source: "./cadence/contracts/HelloWorld.cdc",
+          aliases: {
+            emulator: "0x123",
+          },
+        },
+      },
+      accounts: {
+        "emulator-account": {
+          address: "0xf8d6e0586b0a20c7",
+        },
+      },
+      deployments: {
+        emulator: {
+          "emulator-account": ["DeployedContract"],
+        },
+      },
+    }
+
+    const emulatorMappings = {
+      HelloWorld: "0x123",
+      DeployedContract: "0xf8d6e0586b0a20c7",
+    }
+
+    expect(getContracts(flowJSON, "emulator")).toEqual(emulatorMappings)
+  })
+
+  test("it should handle multiple accounts with multiple contracts each", () => {
+    const flowJSON: FlowJson = {
+      accounts: {
+        "account-1": {
+          address: "0x01",
+        },
+        "account-2": {
+          address: "0x02",
+        },
+      },
+      deployments: {
+        emulator: {
+          "account-1": ["Contract1A", "Contract1B"],
+          "account-2": ["Contract2A", "Contract2B"],
+        },
+      },
+    }
+
+    const emulatorMappings = {
+      Contract1A: "0x01",
+      Contract1B: "0x01",
+      Contract2A: "0x02",
+      Contract2B: "0x02",
+    }
+
+    expect(getContracts(flowJSON, "emulator")).toEqual(emulatorMappings)
+  })
 })
 
 describe("anyHasPrivateKeys", () => {
