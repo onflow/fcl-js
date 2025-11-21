5870fec3069477059d2adcf4c6399439978e8c10
mfbz <mfbz@users.noreply.github.com>
2025-11-21 17:23:52 +0100

    Other cleanups

--- packages/sdk/src/VERSION.ts
+++ packages/sdk/src/VERSION.ts
@@ -1,7 +1,3 @@
 declare const PACKAGE_CURRENT_VERSION: string | undefined
 
-// Use PACKAGE_CURRENT_VERSION if available (from build), otherwise use a default for development
-export const VERSION: string =
-  typeof PACKAGE_CURRENT_VERSION !== "undefined"
-    ? PACKAGE_CURRENT_VERSION
-    : "TESTVERSION"
+export const VERSION: string = PACKAGE_CURRENT_VERSION || "TESTVERSION"
