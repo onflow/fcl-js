82b75ceada5664b4bc42a1f9664f6a60f439e8c4
Michael Fabozzi <39808567+mfbz@users.noreply.github.com>
2025-12-09 21:10:51 +0100

    Fixed metro interop issues with peer deps (#2739)
    Co-authored-by: mfbz <mfbz@users.noreply.github.com>

--- packages/fcl-react-native/src/utils/react-native/storage.ts
+++ packages/fcl-react-native/src/utils/react-native/storage.ts
@@ -1,4 +1,7 @@
-import AsyncStorage from "@react-native-async-storage/async-storage"
+// Use require() to avoid Rollup/Metro interop issues with peer dependencies
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const AsyncStorage =
+  require("@react-native-async-storage/async-storage").default
 
 const safeParseJSON = (str?: string | null) => {
   if (str == null) return null
